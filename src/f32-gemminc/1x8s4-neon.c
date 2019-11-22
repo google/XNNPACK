574319328a73d3a254def6bb434edb81f98d5888
Marat Dukhan <maratek@google.com>
2019-11-22 07:50:42 -0800

    Fix typos in END_FUNCTION arguments in ARM64 assembly kernels
    Fix compilation errors with GNU ARM64 toolchain
    
    PiperOrigin-RevId: 281967270

--- src/f32-gemminc/1x8s4-neon.c
+++ src/f32-gemminc/1x8s4-neon.c
@@ -7,6 +7,7 @@
 // This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree.
 
+
 #include <assert.h>
 
 #include <arm_neon.h>
