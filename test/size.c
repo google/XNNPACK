0812e0b889318595cdaa707f5d7c28d70b1b99cf
Marat Dukhan <maratek@google.com>
2020-03-09 03:59:07 -0700

    Update size_test with newly added operators
    - Android/ARM64: 177KB
    - Android/ARMv7: 191KB
    - Android/x86: 147KB
    - Android/x86-64: 160KB
    - WebAssembly/MVP: 60KB
    - WebAssembly/SIMD: 80KB
    
    PiperOrigin-RevId: 299806364

--- test/size.c
+++ test/size.c
@@ -27,166 +27,252 @@ int main(int argc, char** argv) {
       xnn_run_operator(op, NULL);
       break;
     case 1:
-      xnn_create_convolution2d_nhwc_f32(
-        0, 0, 0, 0,
-        0, 0,
-        0, 0,
-        0, 0,
-        0, 0, 0,
-        0, 0,
-        NULL, NULL,
-        -INFINITY, INFINITY,
-        0,
-        &op);
+      xnn_create_add_nd_f32(
+        0.0f, 0.0f,
+        0, &op);
       break;
     case 2:
-      xnn_setup_convolution2d_nhwc_f32(
-        op,
-        0, 0, 0,
-        NULL, NULL,
-        NULL);
+      xnn_setup_add_nd_f32(
+        op, 0, NULL, 0, NULL,
+        NULL, NULL, NULL, NULL);
       break;
     case 3:
-      xnn_create_deconvolution2d_nhwc_f32(
+      xnn_create_argmax_pooling2d_nhwc_f32(
         0, 0, 0, 0,
         0, 0,
-        0, 0,
-        0, 0,
         0, 0, 0,
-        0, 0,
-        NULL, NULL,
-        -INFINITY, INFINITY,
-        0,
-        &op);
+        0.0f, 0.0f,
+        0, &op);
       break;
     case 4:
-      xnn_setup_deconvolution2d_nhwc_f32(
-        op,
-        0, 0, 0,
-        0, 0,
-        NULL, NULL,
-        NULL);
+      xnn_setup_argmax_pooling2d_nhwc_f32(
+        op, 0, 0, 0,
+        NULL, NULL, NULL, NULL);
       break;
     case 5:
-      xnn_create_fully_connected_nc_f32(
-        0, 0, 0, 0, NULL, NULL,
-        -INFINITY, INFINITY, 0, &op);
-      break;
-    case 6:
-      xnn_setup_fully_connected_nc_f32(
-        op, 0, NULL, NULL, NULL);
-      break;
-    case 7:
-      xnn_create_global_average_pooling_nwc_f32(
-        0, 0, 0,
-        -INFINITY, INFINITY, 0, &op);
-      break;
-    case 8:
-      xnn_setup_global_average_pooling_nwc_f32(
-        op, 0, 0, NULL, NULL, NULL);
-      break;
-    case 9:
       xnn_create_average_pooling2d_nhwc_f32(
         0, 0, 0, 0,
         0, 0,
         0, 0,
         0, 0, 0,
-        -INFINITY, INFINITY,
+        0.0f, 0.0f,
+        0, &op);
+      break;
+    case 6:
+      xnn_setup_average_pooling2d_nhwc_f32(
+        op, 0, 0, 0,
+        NULL, NULL, NULL);
+      break;
+    case 7:
+      xnn_create_clamp_nc_f32(
+        0, 0, 0,
+        0.0f, 0.0f,
+        0, &op);
+      break;
+    case 8:
+      xnn_setup_clamp_nc_f32(
+        op, 0,
+        NULL, NULL, NULL);
+      break;
+    case 9:
+      xnn_create_convolution2d_nhwc_f32(
+        0, 0, 0, 0,
+        0, 0,
+        0, 0,
+        0, 0,
+        0, 0, 0, 0, 0,
+        NULL, NULL,
+        0.0f, 0.0f,
         0, &op);
       break;
     case 10:
-      xnn_setup_average_pooling2d_nhwc_f32(
-        op, 0, 0, 0, NULL, NULL, NULL);
+      xnn_setup_convolution2d_nhwc_f32(
+        op, 0, 0, 0,
+        NULL, NULL, NULL);
       break;
     case 11:
+      xnn_create_deconvolution2d_nhwc_f32(
+        0, 0, 0, 0,
+        0, 0,
+        0, 0,
+        0, 0,
+        0, 0, 0, 0, 0,
+        NULL, NULL,
+        0.0f, 0.0f,
+        0, &op);
+      break;
+    case 12:
+      xnn_setup_deconvolution2d_nhwc_f32(
+        op, 0, 0, 0, 0, 0,
+        NULL, NULL, NULL);
+      break;
+    case 13:
+      xnn_create_divide_nd_f32(
+        0.0f, 0.0f, 0, &op);
+      break;
+    case 14:
+      xnn_setup_divide_nd_f32(
+        op, 0, NULL, 0, NULL,
+        NULL, NULL, NULL, NULL);
+      break;
+    case 15:
+      xnn_create_fully_connected_nc_f32(
+        0, 0, 0, 0,
+        NULL, NULL,
+        0.0f, 0.0f,
+        0, &op);
+      break;
+    case 16:
+      xnn_setup_fully_connected_nc_f32(
+        op, 0,
+        NULL, NULL, NULL);
+      break;
+    case 17:
+      xnn_create_global_average_pooling_nwc_f32(
+        0, 0, 0,
+        0.0f, 0.0f,
+        0, &op);
+      break;
+    case 18:
+      xnn_setup_global_average_pooling_nwc_f32(
+        op, 0, 0,
+        NULL, NULL, NULL);
+      break;
+    case 19:
+      xnn_create_hardswish_nc_f32(
+        0, 0, 0,
+        0, &op);
+      break;
+    case 20:
+      xnn_setup_hardswish_nc_f32(
+        op, 0,
+        NULL, NULL, NULL);
+      break;
+    case 21:
       xnn_create_max_pooling2d_nhwc_f32(
         0, 0, 0, 0,
         0, 0,
         0, 0,
         0, 0,
         0, 0, 0,
-        -INFINITY, INFINITY,
+        0.0f, 0.0f,
         0, &op);
       break;
-    case 12:
+    case 22:
       xnn_setup_max_pooling2d_nhwc_f32(
-        op, 0, 0, 0, NULL, NULL, NULL);
+        op, 0, 0, 0,
+        NULL, NULL, NULL);
       break;
-    case 13:
-      xnn_create_argmax_pooling2d_nhwc_f32(
-        0, 0, 0, 0,
-        0, 0,
-        0, 0, 0,
-        -INFINITY, INFINITY,
+    case 23:
+      xnn_create_maximum_nd_f32(
         0, &op);
       break;
-    case 14:
-      xnn_setup_argmax_pooling2d_nhwc_f32(
-        op, 0, 0, 0, NULL, NULL, NULL, NULL);
+    case 24:
+      xnn_setup_maximum_nd_f32(
+        op, 0, NULL, 0, NULL,
+        NULL, NULL, NULL, NULL);
       break;
-    case 15:
+    case 25:
+      xnn_create_minimum_nd_f32(
+        0, &op);
+      break;
+    case 26:
+      xnn_setup_minimum_nd_f32(
+        op, 0, NULL, 0, NULL,
+        NULL, NULL, NULL, NULL);
+      break;
+    case 27:
+      xnn_create_multiply_nd_f32(
+        0.0f, 0.0f,
+        0, &op);
+      break;
+    case 28:
+      xnn_setup_multiply_nd_f32(
+        op, 0, NULL, 0, NULL,
+        NULL, NULL, NULL, NULL);
+      break;
+    case 29:
+      xnn_create_prelu_nc_f32(
+        0, 0, 0,
+        NULL, 0, &op);
+      break;
+    case 30:
+      xnn_setup_prelu_nc_f32(
+        op, 0,
+        NULL, NULL, NULL);
+      break;
+    case 31:
+      xnn_create_resize_bilinear2d_nhwc_f32(
+        0, 0, 0,
+        0, &op);
+      break;
+    case 32:
+      xnn_setup_resize_bilinear2d_nhwc_f32(
+        op, 0, 0, 0, 0, 0,
+        NULL, NULL, NULL);
+      break;
+    case 33:
+      xnn_create_sigmoid_nc_f32(
+        0, 0, 0,
+        0, &op);
+      break;
+    case 34:
+      xnn_setup_sigmoid_nc_f32(
+        op, 0,
+        NULL, NULL, NULL);
+      break;
+    case 35:
+      xnn_create_softmax_nc_f32(
+        0, 0, 0,
+        0, &op);
+      break;
+    case 36:
+      xnn_setup_softmax_nc_f32(
+        op, 0,
+        NULL, NULL, NULL);
+      break;
+    case 37:
+      xnn_create_subtract_nd_f32(
+        0.0f, 0.0f,
+        0, &op);
+      break;
+    case 38:
+      xnn_setup_subtract_nd_f32(
+        op, 0, NULL, 0, NULL,
+        NULL, NULL, NULL, NULL);
+      break;
+    case 39:
+      xnn_create_channel_pad_nc_x32(
+        0, 0, 0, 0, 0,
+        NULL,
+        0, &op);
+      break;
+    case 40:
+      xnn_setup_channel_pad_nc_x32(
+        op, 0,
+        NULL, NULL, NULL);
+      break;
+    case 41:
+      xnn_create_channel_shuffle_nc_x32(
+        0, 0, 0, 0,
+        0, &op);
+      break;
+    case 42:
+      xnn_setup_channel_shuffle_nc_x32(
+        op, 0,
+        NULL, NULL, NULL);
+      break;
+    case 43:
       xnn_create_unpooling2d_nhwc_x32(
         0, 0, 0, 0,
         0, 0,
         0, 0, 0,
         0, &op);
       break;
-    case 17:
+    case 44:
       xnn_setup_unpooling2d_nhwc_x32(
-        op, 0, 0, 0, NULL, NULL, NULL, NULL);
-      break;
-    case 18:
-      xnn_create_channel_shuffle_nc_x32(
-        0, 0, 0, 0,
-        0, &op);
-      break;
-    case 19:
-      xnn_setup_channel_shuffle_nc_x32(
-        op, 0, NULL, NULL, NULL);
-      break;
-    case 20:
-      xnn_create_add_nc_f32(
-        0, 0, 0, 0,
-        -INFINITY, INFINITY,
-        0, &op);
-      break;
-    case 21:
-      xnn_setup_add_nc_f32(
-        op, 0, NULL, NULL, NULL, NULL);
-      break;
-    case 22:
-      xnn_create_channel_pad_nc_x32(
-        0, 0, 0, 0, 0, NULL, 0, &op);
-      break;
-    case 23:
-      xnn_setup_channel_pad_nc_x32(
-        op, 0, NULL, NULL, NULL);
-      break;
-    case 24:
-      xnn_create_clamp_nc_f32(
-        0, 0, 0,
-        -INFINITY, INFINITY,
-        0, &op);
-      break;
-    case 25:
-      xnn_setup_clamp_nc_f32(
-        op, 0, NULL, NULL, NULL);
-      break;
-    case 26:
-      xnn_create_hardswish_nc_f32(
-        0, 0, 0, 0, &op);
-      break;
-    case 27:
-      xnn_setup_hardswish_nc_f32(
-        op, 0, NULL, NULL, NULL);
-      break;
-    case 28:
-      xnn_create_prelu_nc_f32(
-        0, 0, 0, NULL, 0, &op);
-      break;
-    case 29:
-      xnn_setup_prelu_nc_f32(
-        op, 0, NULL, NULL, NULL);
+        op, 0, 0, 0,
+        NULL, NULL, NULL, NULL);
       break;
   }
 
