f21491684f928e5050c71396cd5e79a2bbe4ca74
Marat Dukhan <maratek@google.com>
2020-04-14 08:17:29 -0700

    Add a dummy variable in xnn_f32_default_params
    C standard requires structures to have at least one member. gcc & clang doesn't check it, but empty struct cause compilation error with MSVC.
    
    PiperOrigin-RevId: 306441387

--- src/xnnpack/params.h
+++ src/xnnpack/params.h
@@ -22,7 +22,8 @@ struct xnn_f16_output_params {
 };
 
 union xnn_f32_default_params {
-  /* Empty; serves to differentiate pointer types for micro-kernels without fused activation */
+  // Empty; serves to differentiate pointer types for micro-kernels without fused activation.
+  char _; // Dummy member variable to comply with the C standard
 };
 
 union xnn_f32_minmax_params {
