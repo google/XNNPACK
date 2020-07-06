a6879bd4eccf2f2b379c8f8b7322f54f03c308fe
Marat Dukhan <maratek@google.com>
2020-07-06 14:25:08 -0700

    Re-arrange packing functions
    PiperOrigin-RevId: 319855410

--- src/xnnpack/pack.h
+++ src/xnnpack/pack.h
@@ -19,6 +19,29 @@
 extern "C" {
 #endif
 
+
+XNN_INTERNAL void xnn_pack_f32_gemm_goi_w(
+  size_t g,
+  size_t nc,
+  size_t kc,
+  size_t nr,
+  size_t kr,
+  size_t sr,
+  const float* k,
+  const float* b,
+  float* packed_w);
+
+XNN_INTERNAL void xnn_pack_f16_gemm_goi_w(
+  size_t g,
+  size_t nc,
+  size_t kc,
+  size_t nr,
+  size_t kr,
+  size_t sr,
+  const uint16_t* k,
+  const uint16_t* b,
+  uint16_t* packed_w);
+
 XNN_INTERNAL void xnn_pack_q8_gemm_goi_w(
   size_t g,
   size_t nc,
@@ -31,6 +54,27 @@ XNN_INTERNAL void xnn_pack_q8_gemm_goi_w(
   const int32_t* b,
   void* packed_w);
 
+
+XNN_INTERNAL void xnn_pack_f32_gemm_io_w(
+  size_t nc,
+  size_t kc,
+  size_t nr,
+  size_t kr,
+  size_t sr,
+  const float* k,
+  const float* b,
+  float* packed_w);
+
+XNN_INTERNAL void xnn_pack_f16_gemm_io_w(
+  size_t nc,
+  size_t kc,
+  size_t nr,
+  size_t kr,
+  size_t sr,
+  const uint16_t* k,
+  const uint16_t* b,
+  uint16_t* packed_w);
+
 XNN_INTERNAL void xnn_pack_q8_gemm_io_w(
   size_t nc,
   size_t kc,
@@ -41,6 +85,31 @@ XNN_INTERNAL void xnn_pack_q8_gemm_io_w(
   const uint8_t* k,
   const int32_t* b,
   void* packed_w);
+
+
+XNN_INTERNAL void xnn_pack_f32_conv_goki_w(
+  size_t g,
+  size_t nc,
+  size_t ks,
+  size_t kc,
+  size_t nr,
+  size_t kr,
+  size_t sr,
+  const float* k,
+  const float* b,
+  float* packed_w);
+
+XNN_INTERNAL void xnn_pack_f16_conv_goki_w(
+  size_t g,
+  size_t nc,
+  size_t ks,
+  size_t kc,
+  size_t nr,
+  size_t kr,
+  size_t sr,
+  const uint16_t* k,
+  const uint16_t* b,
+  uint16_t* packed_w);
 
 XNN_INTERNAL void xnn_pack_q8_conv_goki_w(
   size_t g,
@@ -55,17 +124,71 @@ XNN_INTERNAL void xnn_pack_q8_conv_goki_w(
   const int32_t* b,
   void* packed_w);
 
+
+XNN_INTERNAL void xnn_pack_f32_conv_kgo_w(
+  size_t g,
+  size_t nc,
+  size_t ks,
+  size_t nr,
+  size_t kr,
+  const float* k,
+  const float* b,
+  float* packed_w);
+
+XNN_INTERNAL void xnn_pack_f16_conv_kgo_w(
+  size_t g,
+  size_t nc,
+  size_t ks,
+  size_t nr,
+  size_t kr,
+  const uint16_t* k,
+  const uint16_t* b,
+  uint16_t* packed_w);
+
 XNN_INTERNAL void xnn_pack_q8_conv_kgo_w(
   size_t g,
   size_t nc,
   size_t ks,
-  uint32_t nr,
-  uint32_t kr,
+  size_t nr,
+  size_t kr,
   uint8_t izp,
   uint8_t kzp,
   const uint8_t* k,
   const int32_t* b,
   void* packed_w);
+
+
+XNN_INTERNAL void xnn_pack_f32_deconv_goki_w(
+  size_t g,
+  size_t nc,
+  size_t kh,
+  size_t kw,
+  size_t kc,
+  size_t sh,
+  size_t sw,
+  size_t nr,
+  size_t kr,
+  size_t sr,
+  const float* k,
+  const float* b,
+  float* packed_w,
+  struct subconvolution_params* params);
+
+XNN_INTERNAL void xnn_pack_f16_deconv_goki_w(
+  size_t g,
+  size_t nc,
+  size_t kh,
+  size_t kw,
+  size_t kc,
+  size_t sh,
+  size_t sw,
+  size_t nr,
+  size_t kr,
+  size_t sr,
+  const uint16_t* k,
+  const uint16_t* b,
+  uint16_t* packed_w,
+  struct subconvolution_params* params);
 
 XNN_INTERNAL void xnn_pack_q8_deconv_goki_w(
   size_t g,
@@ -84,6 +207,25 @@ XNN_INTERNAL void xnn_pack_q8_deconv_goki_w(
   void* packed_w,
   struct subconvolution_params* params);
 
+
+XNN_INTERNAL void xnn_pack_f32_dwconv_ghw_w(
+  size_t h,
+  size_t w,
+  size_t c,
+  size_t cr,
+  const float* k,
+  const float* b,
+  float* packed_w);
+
+XNN_INTERNAL void xnn_pack_f16_dwconv_ghw_w(
+  size_t h,
+  size_t w,
+  size_t c,
+  size_t cr,
+  const uint16_t* k,
+  const uint16_t* b,
+  uint16_t* packed_w);
+
 XNN_INTERNAL void xnn_pack_q8_dwconv_ghw_w(
   size_t h,
   size_t w,
@@ -94,6 +236,25 @@ XNN_INTERNAL void xnn_pack_q8_dwconv_ghw_w(
   const uint8_t* k,
   const int32_t* b,
   void* packed_w);
+
+
+XNN_INTERNAL void xnn_pack_f32_dwconv_hwg_w(
+  size_t h,
+  size_t w,
+  size_t c,
+  size_t cr,
+  const float* k,
+  const float* b,
+  float* packed_w);
+
+XNN_INTERNAL void xnn_pack_f16_dwconv_hwg_w(
+  size_t h,
+  size_t w,
+  size_t c,
+  size_t cr,
+  const uint16_t* k,
+  const uint16_t* b,
+  uint16_t* packed_w);
 
 XNN_INTERNAL void xnn_pack_q8_dwconv_hwg_w(
   size_t h,
@@ -106,130 +267,6 @@ XNN_INTERNAL void xnn_pack_q8_dwconv_hwg_w(
   const int32_t* b,
   void* packed_w);
 
-XNN_INTERNAL void xnn_pack_f16_gemm_goi_w(
-  size_t g,
-  size_t nc,
-  size_t kc,
-  size_t nr,
-  size_t kr,
-  size_t sr,
-  const uint16_t* k,
-  const uint16_t* b,
-  uint16_t* packed_w);
-
-XNN_INTERNAL void xnn_pack_f16_gemm_io_w(
-  size_t nc,
-  size_t kc,
-  size_t nr,
-  size_t kr,
-  size_t sr,
-  const uint16_t* k,
-  const uint16_t* b,
-  uint16_t* packed_w);
-
-XNN_INTERNAL void xnn_pack_f16_gemminc_goi_w(
-  size_t g,
-  size_t nc,
-  size_t kc,
-  size_t nr,
-  size_t kr,
-  size_t sr,
-  const uint16_t* k,
-  uint16_t* packed_w);
-
-XNN_INTERNAL void xnn_pack_f16_conv_goki_w(
-  size_t g,
-  size_t nc,
-  size_t ks,
-  size_t kc,
-  size_t nr,
-  size_t kr,
-  size_t sr,
-  const uint16_t* k,
-  const uint16_t* b,
-  uint16_t* packed_w);
-
-XNN_INTERNAL void xnn_pack_f16_conv_kgo_w(
-  size_t g,
-  size_t nc,
-  size_t ks,
-  size_t nr,
-  size_t kr,
-  const uint16_t* k,
-  const uint16_t* b,
-  uint16_t* packed_w);
-
-XNN_INTERNAL void xnn_pack_f16_dconv_oki_w(
-  size_t nc,
-  size_t kc,
-  size_t nr,
-  size_t kh,
-  size_t kw,
-  const uint16_t* k,
-  const uint16_t* b,
-  uint16_t* packed_w);
-
-XNN_INTERNAL void xnn_pack_f16_deconv_goki_w(
-  size_t g,
-  size_t nc,
-  size_t kh,
-  size_t kw,
-  size_t kc,
-  size_t sh,
-  size_t sw,
-  size_t nr,
-  size_t kr,
-  size_t sr,
-  const uint16_t* k,
-  const uint16_t* b,
-  uint16_t* packed_w,
-  struct subconvolution_params* params);
-
-XNN_INTERNAL void xnn_pack_f16_dwconv_ghw_w(
-  size_t h,
-  size_t w,
-  size_t c,
-  size_t cr,
-  const uint16_t* k,
-  const uint16_t* b,
-  uint16_t* packed_w);
-
-XNN_INTERNAL void xnn_pack_f16_dwconv_hwg_w(
-  size_t h,
-  size_t w,
-  size_t c,
-  size_t cr,
-  const uint16_t* k,
-  const uint16_t* b,
-  uint16_t* packed_w);
-
-XNN_INTERNAL void xnn_pack_f16_chw_dwconv_ghw_w(
-  size_t kernel_size,
-  size_t groups,
-  const uint16_t* kernel,
-  const uint16_t* bias,
-  uint16_t* packed_weights);
-
-XNN_INTERNAL void xnn_pack_f32_gemm_goi_w(
-  size_t g,
-  size_t nc,
-  size_t kc,
-  size_t nr,
-  size_t kr,
-  size_t sr,
-  const float* k,
-  const float* b,
-  float* packed_w);
-
-XNN_INTERNAL void xnn_pack_f32_gemm_io_w(
-  size_t nc,
-  size_t kc,
-  size_t nr,
-  size_t kr,
-  size_t sr,
-  const float* k,
-  const float* b,
-  float* packed_w);
 
 XNN_INTERNAL void xnn_pack_f32_gemminc_goi_w(
   size_t g,
@@ -241,27 +278,16 @@ XNN_INTERNAL void xnn_pack_f32_gemminc_goi_w(
   const float* k,
   float* packed_w);
 
-XNN_INTERNAL void xnn_pack_f32_conv_goki_w(
+XNN_INTERNAL void xnn_pack_f16_gemminc_goi_w(
   size_t g,
   size_t nc,
-  size_t ks,
   size_t kc,
   size_t nr,
   size_t kr,
   size_t sr,
-  const float* k,
-  const float* b,
-  float* packed_w);
+  const uint16_t* k,
+  uint16_t* packed_w);
 
-XNN_INTERNAL void xnn_pack_f32_conv_kgo_w(
-  size_t g,
-  size_t nc,
-  size_t ks,
-  size_t nr,
-  size_t kr,
-  const float* k,
-  const float* b,
-  float* packed_w);
 
 XNN_INTERNAL void xnn_pack_f32_dconv_oki_w(
   size_t nc,
@@ -273,39 +299,16 @@ XNN_INTERNAL void xnn_pack_f32_dconv_oki_w(
   const float* b,
   float* packed_w);
 
-XNN_INTERNAL void xnn_pack_f32_deconv_goki_w(
-  size_t g,
+XNN_INTERNAL void xnn_pack_f16_dconv_oki_w(
   size_t nc,
+  size_t kc,
+  size_t nr,
   size_t kh,
   size_t kw,
-  size_t kc,
-  size_t sh,
-  size_t sw,
-  size_t nr,
-  size_t kr,
-  size_t sr,
-  const float* k,
-  const float* b,
-  float* packed_w,
-  struct subconvolution_params* params);
+  const uint16_t* k,
+  const uint16_t* b,
+  uint16_t* packed_w);
 
-XNN_INTERNAL void xnn_pack_f32_dwconv_ghw_w(
-  size_t h,
-  size_t w,
-  size_t c,
-  size_t cr,
-  const float* k,
-  const float* b,
-  float* packed_w);
-
-XNN_INTERNAL void xnn_pack_f32_dwconv_hwg_w(
-  size_t h,
-  size_t w,
-  size_t c,
-  size_t cr,
-  const float* k,
-  const float* b,
-  float* packed_w);
 
 XNN_INTERNAL void xnn_pack_f32_chw_dwconv_ghw_w(
   size_t kernel_size,
@@ -314,6 +317,14 @@ XNN_INTERNAL void xnn_pack_f32_chw_dwconv_ghw_w(
   const float* bias,
   float* packed_weights);
 
+XNN_INTERNAL void xnn_pack_f16_chw_dwconv_ghw_w(
+  size_t kernel_size,
+  size_t groups,
+  const uint16_t* kernel,
+  const uint16_t* bias,
+  uint16_t* packed_weights);
+
+
 XNN_INTERNAL void xnn_pack_f32_chw_dwconv_hwg_w(
   size_t kernel_size,
   size_t groups,
@@ -321,12 +332,6 @@ XNN_INTERNAL void xnn_pack_f32_chw_dwconv_hwg_w(
   const float* bias,
   float* packed_weights);
 
-XNN_INTERNAL void xnn_pack_f16_vmulcaddc_w(
-  size_t c,
-  size_t cr,
-  const uint16_t* s,
-  const uint16_t* b,
-  uint16_t* packed_w);
 
 XNN_INTERNAL void xnn_pack_f32_vmulcaddc_w(
   size_t c,
@@ -334,6 +339,13 @@ XNN_INTERNAL void xnn_pack_f32_vmulcaddc_w(
   const float* s,
   const float* b,
   float* packed_w);
+
+XNN_INTERNAL void xnn_pack_f16_vmulcaddc_w(
+  size_t c,
+  size_t cr,
+  const uint16_t* s,
+  const uint16_t* b,
+  uint16_t* packed_w);
 
 #ifdef __cplusplus
 }  // extern "C"
