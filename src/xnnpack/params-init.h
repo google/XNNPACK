faa2e5c0dd66344612b5d6c79e6da9617b0a619e
Marat Dukhan <maratek@google.com>
2020-07-10 17:34:44 -0700

    Fix CHW depthwise convolution in WAsm SIMD
    PiperOrigin-RevId: 320706830

--- src/xnnpack/params-init.h
+++ src/xnnpack/params-init.h
@@ -625,20 +625,20 @@ static inline union xnn_f32_chw_params xnn_init_f32_chw_params(
     params.scalar.max = output_max;
 
     const uint32_t w4 = (width - 1) & 3;
-    params.scalar.mask[0] = INT32_C(0xFFFFFFFF);
-    params.scalar.mask[1] = -(int32_t) (w4 >= 1);
-    params.scalar.mask[2] = -(int32_t) (w4 >= 2);
-    params.scalar.mask[3] = -(int32_t) (w4 >= 3);
+    params.scalar.mask[0] = UINT32_C(0xFFFFFFFF);
+    params.scalar.mask[1] = -(uint32_t) (w4 >= 1);
+    params.scalar.mask[2] = -(uint32_t) (w4 >= 2);
+    params.scalar.mask[3] = -(uint32_t) (w4 >= 3);
 
     const uint32_t w8 = (width - 1) & 7;
-    params.scalar.mask_even[0] = INT32_C(0xFFFFFFFF);
-    params.scalar.mask_even[1] = -(int32_t) (w8 >= 2);
-    params.scalar.mask_even[2] = -(int32_t) (w8 >= 4);
-    params.scalar.mask_even[3] = -(int32_t) (w8 >= 6);
-    params.scalar.mask_odd[0] = -(int32_t) (w8 >= 1);
-    params.scalar.mask_odd[1] = -(int32_t) (w8 >= 3);
-    params.scalar.mask_odd[2] = -(int32_t) (w8 >= 5);
-    params.scalar.mask_odd[3] = -(int32_t) (w8 >= 7);
+    params.scalar.mask_even[0] = UINT32_C(0xFFFFFFFF);
+    params.scalar.mask_even[1] = -(uint32_t) (w8 >= 2);
+    params.scalar.mask_even[2] = -(uint32_t) (w8 >= 4);
+    params.scalar.mask_even[3] = -(uint32_t) (w8 >= 6);
+    params.scalar.mask_odd[0] = -(uint32_t) (w8 >= 1);
+    params.scalar.mask_odd[1] = -(uint32_t) (w8 >= 3);
+    params.scalar.mask_odd[2] = -(uint32_t) (w8 >= 5);
+    params.scalar.mask_odd[3] = -(uint32_t) (w8 >= 7);
   #endif
   return params;
 }
@@ -679,6 +679,22 @@ static inline void xnn_update_f32_chw_params(
     params->neon.mask_odd[1] = -(uint32_t) (w8 >= 3);
     params->neon.mask_odd[2] = -(uint32_t) (w8 >= 5);
     params->neon.mask_odd[3] = -(uint32_t) (w8 >= 7);
+  #else
+    const uint32_t w4 = (width - 1) & 3;
+    params->scalar.mask[0] = UINT32_C(0xFFFFFFFF);
+    params->scalar.mask[1] = -(uint32_t) (w4 >= 1);
+    params->scalar.mask[2] = -(uint32_t) (w4 >= 2);
+    params->scalar.mask[3] = -(uint32_t) (w4 >= 3);
+
+    const uint32_t w8 = (width - 1) & 7;
+    params->scalar.mask_even[0] = UINT32_C(0xFFFFFFFF);
+    params->scalar.mask_even[1] = -(uint32_t) (w8 >= 2);
+    params->scalar.mask_even[2] = -(uint32_t) (w8 >= 4);
+    params->scalar.mask_even[3] = -(uint32_t) (w8 >= 6);
+    params->scalar.mask_odd[0] = -(uint32_t) (w8 >= 1);
+    params->scalar.mask_odd[1] = -(uint32_t) (w8 >= 3);
+    params->scalar.mask_odd[2] = -(uint32_t) (w8 >= 5);
+    params->scalar.mask_odd[3] = -(uint32_t) (w8 >= 7);
   #endif
 }
 
@@ -692,20 +708,20 @@ static inline union xnn_f32_chw_params xnn_init_scalar_f32_chw_params(
   params.scalar.max = output_max;
 
   const uint32_t w4 = (width - 1) & 3;
-  params.scalar.mask[0] = INT32_C(0xFFFFFFFF);
-  params.scalar.mask[1] = -(int32_t) (w4 >= 1);
-  params.scalar.mask[2] = -(int32_t) (w4 >= 2);
-  params.scalar.mask[3] = -(int32_t) (w4 >= 3);
+  params.scalar.mask[0] = UINT32_C(0xFFFFFFFF);
+  params.scalar.mask[1] = -(uint32_t) (w4 >= 1);
+  params.scalar.mask[2] = -(uint32_t) (w4 >= 2);
+  params.scalar.mask[3] = -(uint32_t) (w4 >= 3);
 
   const uint32_t w8 = (width - 1) & 7;
-  params.scalar.mask_even[0] = INT32_C(0xFFFFFFFF);
-  params.scalar.mask_even[1] = -(int32_t) (w8 >= 2);
-  params.scalar.mask_even[2] = -(int32_t) (w8 >= 4);
-  params.scalar.mask_even[3] = -(int32_t) (w8 >= 6);
-  params.scalar.mask_odd[0] = -(int32_t) (w8 >= 1);
-  params.scalar.mask_odd[1] = -(int32_t) (w8 >= 3);
-  params.scalar.mask_odd[2] = -(int32_t) (w8 >= 5);
-  params.scalar.mask_odd[3] = -(int32_t) (w8 >= 7);
+  params.scalar.mask_even[0] = UINT32_C(0xFFFFFFFF);
+  params.scalar.mask_even[1] = -(uint32_t) (w8 >= 2);
+  params.scalar.mask_even[2] = -(uint32_t) (w8 >= 4);
+  params.scalar.mask_even[3] = -(uint32_t) (w8 >= 6);
+  params.scalar.mask_odd[0] = -(uint32_t) (w8 >= 1);
+  params.scalar.mask_odd[1] = -(uint32_t) (w8 >= 3);
+  params.scalar.mask_odd[2] = -(uint32_t) (w8 >= 5);
+  params.scalar.mask_odd[3] = -(uint32_t) (w8 >= 7);
 
   return params;
 }
