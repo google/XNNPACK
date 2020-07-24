18493512fe48d5f3395b2de0834ddf776b1e699d
Marat Dukhan <maratek@google.com>
2020-07-24 14:13:48 -0700

    WAsm SIMD versions of GAVGPOOL CW microkernel
    PiperOrigin-RevId: 323069679

--- test/f32-gavgpool-cw.cc
+++ test/f32-gavgpool-cw.cc
@@ -307,6 +307,184 @@
   }
 #endif  // !XNN_ARCH_ASMJS && !XNN_ARCH_WASM && !XNN_COMPILER_MSVC && !XNN_COMPILER_ICC
 
+#if XNN_ARCH_WASMSIMD
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_ARM_X4, elements_eq_4) {
+    GAvgPoolCWMicrokernelTester()
+      .elements(4)
+      .channels(4)
+      .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_arm_x4);
+  }
+
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_ARM_X4, elements_div_4) {
+    for (size_t elements = 8; elements < 32; elements += 4) {
+      GAvgPoolCWMicrokernelTester()
+        .elements(elements)
+        .channels(4)
+        .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_arm_x4);
+    }
+  }
+
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_ARM_X4, elements_lt_4) {
+    for (size_t elements = 1; elements < 4; elements++) {
+      GAvgPoolCWMicrokernelTester()
+        .elements(elements)
+        .channels(4)
+        .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_arm_x4);
+    }
+  }
+
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_ARM_X4, elements_gt_4) {
+    for (size_t elements = 5; elements < 8; elements++) {
+      GAvgPoolCWMicrokernelTester()
+        .elements(elements)
+        .channels(4)
+        .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_arm_x4);
+    }
+  }
+
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_ARM_X4, channels_lt_4) {
+    for (size_t channels = 1; channels < 4; channels++) {
+      for (size_t elements = 1; elements < 16; elements += 3) {
+        GAvgPoolCWMicrokernelTester()
+          .elements(elements)
+          .channels(channels)
+          .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_arm_x4);
+      }
+    }
+  }
+
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_ARM_X4, channels_gt_4) {
+    for (size_t channels = 5; channels < 8; channels++) {
+      for (size_t elements = 1; elements < 16; elements += 3) {
+        GAvgPoolCWMicrokernelTester()
+          .elements(elements)
+          .channels(channels)
+          .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_arm_x4);
+      }
+    }
+  }
+
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_ARM_X4, channels_div_4) {
+    for (size_t channels = 8; channels <= 16; channels += 4) {
+      for (size_t elements = 1; elements < 16; elements += 3) {
+        GAvgPoolCWMicrokernelTester()
+          .elements(elements)
+          .channels(channels)
+          .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_arm_x4);
+      }
+    }
+  }
+
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_ARM_X4, qmin) {
+    for (size_t elements = 1; elements < 16; elements += 3) {
+      GAvgPoolCWMicrokernelTester()
+        .elements(elements)
+        .channels(4)
+        .qmin(128)
+        .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_arm_x4);
+    }
+  }
+
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_ARM_X4, qmax) {
+    for (size_t elements = 1; elements < 16; elements += 3) {
+      GAvgPoolCWMicrokernelTester()
+        .elements(elements)
+        .channels(4)
+        .qmax(128)
+        .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_arm_x4);
+    }
+  }
+#endif  // XNN_ARCH_WASMSIMD
+
+#if XNN_ARCH_WASMSIMD
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_X86_X4, elements_eq_4) {
+    GAvgPoolCWMicrokernelTester()
+      .elements(4)
+      .channels(4)
+      .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_x86_x4);
+  }
+
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_X86_X4, elements_div_4) {
+    for (size_t elements = 8; elements < 32; elements += 4) {
+      GAvgPoolCWMicrokernelTester()
+        .elements(elements)
+        .channels(4)
+        .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_x86_x4);
+    }
+  }
+
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_X86_X4, elements_lt_4) {
+    for (size_t elements = 1; elements < 4; elements++) {
+      GAvgPoolCWMicrokernelTester()
+        .elements(elements)
+        .channels(4)
+        .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_x86_x4);
+    }
+  }
+
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_X86_X4, elements_gt_4) {
+    for (size_t elements = 5; elements < 8; elements++) {
+      GAvgPoolCWMicrokernelTester()
+        .elements(elements)
+        .channels(4)
+        .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_x86_x4);
+    }
+  }
+
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_X86_X4, channels_lt_4) {
+    for (size_t channels = 1; channels < 4; channels++) {
+      for (size_t elements = 1; elements < 16; elements += 3) {
+        GAvgPoolCWMicrokernelTester()
+          .elements(elements)
+          .channels(channels)
+          .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_x86_x4);
+      }
+    }
+  }
+
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_X86_X4, channels_gt_4) {
+    for (size_t channels = 5; channels < 8; channels++) {
+      for (size_t elements = 1; elements < 16; elements += 3) {
+        GAvgPoolCWMicrokernelTester()
+          .elements(elements)
+          .channels(channels)
+          .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_x86_x4);
+      }
+    }
+  }
+
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_X86_X4, channels_div_4) {
+    for (size_t channels = 8; channels <= 16; channels += 4) {
+      for (size_t elements = 1; elements < 16; elements += 3) {
+        GAvgPoolCWMicrokernelTester()
+          .elements(elements)
+          .channels(channels)
+          .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_x86_x4);
+      }
+    }
+  }
+
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_X86_X4, qmin) {
+    for (size_t elements = 1; elements < 16; elements += 3) {
+      GAvgPoolCWMicrokernelTester()
+        .elements(elements)
+        .channels(4)
+        .qmin(128)
+        .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_x86_x4);
+    }
+  }
+
+  TEST(F32_GAVGPOOL_CW__WASMSIMD_X86_X4, qmax) {
+    for (size_t elements = 1; elements < 16; elements += 3) {
+      GAvgPoolCWMicrokernelTester()
+        .elements(elements)
+        .channels(4)
+        .qmax(128)
+        .Test(xnn_f32_gavgpool_cw_ukernel__wasmsimd_x86_x4);
+    }
+  }
+#endif  // XNN_ARCH_WASMSIMD
+
 TEST(F32_GAVGPOOL_CW__SCALAR_X1, elements_eq_4) {
   GAvgPoolCWMicrokernelTester()
     .elements(4)
