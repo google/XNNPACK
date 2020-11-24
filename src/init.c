2bba20962f8f616fab385b2c7340362f166dd7bb
Frank Barchard <fbarchard@google.com>
2020-11-23 16:25:21 -0800

    Enable dwconv2d_chw_3x3p1__neonfma_4x4 for AArch64
    PiperOrigin-RevId: 343950634

--- src/init.c
+++ src/init.c
@@ -1298,10 +1298,10 @@ static void init(void) {
         .output_width_tile = 2,
       };
       xnn_params.f32.dwconv2d_chw_3x3 = (struct dwconv2d_chw_parameters) {
-        .ukernel = (xnn_dwconv2d_chw_ukernel_function) xnn_f32_dwconv2d_chw_ukernel_3x3p1__neonfma_3x4,
+        .ukernel = (xnn_dwconv2d_chw_ukernel_function) xnn_f32_dwconv2d_chw_ukernel_3x3p1__neonfma_4x4,
         .input_width_tile = 4,
         .output_width_tile = 4,
-        .output_height_tile = 3,
+        .output_height_tile = 4,
       };
       xnn_params.f32.dwconv2d_chw_3x3s2 = (struct dwconv2d_chw_parameters) {
         .ukernel = (xnn_dwconv2d_chw_ukernel_function) xnn_f32_dwconv2d_chw_ukernel_3x3s2p1__neonfma_2x4_acc2,
