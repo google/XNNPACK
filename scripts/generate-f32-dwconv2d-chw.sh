d173b11af6aced5cd242fb892b8db010fdf01385
Frank Barchard <fbarchard@google.com>
2020-11-25 11:42:09 -0800

    Web Assemble DWConv2D adapted from Neon for 3x3 stride 2
    PiperOrigin-RevId: 344290177

--- scripts/generate-f32-dwconv2d-chw.sh
+++ scripts/generate-f32-dwconv2d-chw.sh
@@ -241,6 +241,26 @@ tools/xngen src/f32-dwconv2d-chw/3x3p1-wasmsimd.c.in -D ROW_TILE=1 -D ACCUMULATO
 tools/xngen src/f32-dwconv2d-chw/3x3p1-wasmsimd.c.in -D ROW_TILE=1 -D ACCUMULATORS=4 -D X86=1 -o src/f32-dwconv2d-chw/gen/3x3p1-minmax-wasmsimd-x86-1x4-acc4.c
 tools/xngen src/f32-dwconv2d-chw/3x3p1-wasmsimd.c.in -D ROW_TILE=2 -D ACCUMULATORS=2 -D X86=1 -o src/f32-dwconv2d-chw/gen/3x3p1-minmax-wasmsimd-x86-2x4-acc2.c
 
+tools/xngen src/f32-dwconv2d-chw/3x3s2p1-wasmsimd.c.in -D ROW_TILE=1 -D ACCUMULATORS=1 -D X86=0 -o src/f32-dwconv2d-chw/gen/3x3s2p1-minmax-wasmsimd-arm-1x4.c
+tools/xngen src/f32-dwconv2d-chw/3x3s2p1-wasmsimd.c.in -D ROW_TILE=2 -D ACCUMULATORS=1 -D X86=0 -o src/f32-dwconv2d-chw/gen/3x3s2p1-minmax-wasmsimd-arm-2x4.c
+tools/xngen src/f32-dwconv2d-chw/3x3s2p1-wasmsimd.c.in -D ROW_TILE=3 -D ACCUMULATORS=1 -D X86=0 -o src/f32-dwconv2d-chw/gen/3x3s2p1-minmax-wasmsimd-arm-3x4.c
+tools/xngen src/f32-dwconv2d-chw/3x3s2p1-wasmsimd.c.in -D ROW_TILE=4 -D ACCUMULATORS=1 -D X86=0 -o src/f32-dwconv2d-chw/gen/3x3s2p1-minmax-wasmsimd-arm-4x4.c
+
+tools/xngen src/f32-dwconv2d-chw/3x3s2p1-wasmsimd.c.in -D ROW_TILE=1 -D ACCUMULATORS=2 -D X86=0 -o src/f32-dwconv2d-chw/gen/3x3s2p1-minmax-wasmsimd-arm-1x4-acc2.c
+tools/xngen src/f32-dwconv2d-chw/3x3s2p1-wasmsimd.c.in -D ROW_TILE=1 -D ACCUMULATORS=3 -D X86=0 -o src/f32-dwconv2d-chw/gen/3x3s2p1-minmax-wasmsimd-arm-1x4-acc3.c
+tools/xngen src/f32-dwconv2d-chw/3x3s2p1-wasmsimd.c.in -D ROW_TILE=1 -D ACCUMULATORS=4 -D X86=0 -o src/f32-dwconv2d-chw/gen/3x3s2p1-minmax-wasmsimd-arm-1x4-acc4.c
+tools/xngen src/f32-dwconv2d-chw/3x3s2p1-wasmsimd.c.in -D ROW_TILE=2 -D ACCUMULATORS=2 -D X86=0 -o src/f32-dwconv2d-chw/gen/3x3s2p1-minmax-wasmsimd-arm-2x4-acc2.c
+
+tools/xngen src/f32-dwconv2d-chw/3x3s2p1-wasmsimd.c.in -D ROW_TILE=1 -D ACCUMULATORS=1 -D X86=1 -o src/f32-dwconv2d-chw/gen/3x3s2p1-minmax-wasmsimd-x86-1x4.c
+tools/xngen src/f32-dwconv2d-chw/3x3s2p1-wasmsimd.c.in -D ROW_TILE=2 -D ACCUMULATORS=1 -D X86=1 -o src/f32-dwconv2d-chw/gen/3x3s2p1-minmax-wasmsimd-x86-2x4.c
+tools/xngen src/f32-dwconv2d-chw/3x3s2p1-wasmsimd.c.in -D ROW_TILE=3 -D ACCUMULATORS=1 -D X86=1 -o src/f32-dwconv2d-chw/gen/3x3s2p1-minmax-wasmsimd-x86-3x4.c
+tools/xngen src/f32-dwconv2d-chw/3x3s2p1-wasmsimd.c.in -D ROW_TILE=4 -D ACCUMULATORS=1 -D X86=1 -o src/f32-dwconv2d-chw/gen/3x3s2p1-minmax-wasmsimd-x86-4x4.c
+
+tools/xngen src/f32-dwconv2d-chw/3x3s2p1-wasmsimd.c.in -D ROW_TILE=1 -D ACCUMULATORS=2 -D X86=1 -o src/f32-dwconv2d-chw/gen/3x3s2p1-minmax-wasmsimd-x86-1x4-acc2.c
+tools/xngen src/f32-dwconv2d-chw/3x3s2p1-wasmsimd.c.in -D ROW_TILE=1 -D ACCUMULATORS=3 -D X86=1 -o src/f32-dwconv2d-chw/gen/3x3s2p1-minmax-wasmsimd-x86-1x4-acc3.c
+tools/xngen src/f32-dwconv2d-chw/3x3s2p1-wasmsimd.c.in -D ROW_TILE=1 -D ACCUMULATORS=4 -D X86=1 -o src/f32-dwconv2d-chw/gen/3x3s2p1-minmax-wasmsimd-x86-1x4-acc4.c
+tools/xngen src/f32-dwconv2d-chw/3x3s2p1-wasmsimd.c.in -D ROW_TILE=2 -D ACCUMULATORS=2 -D X86=1 -o src/f32-dwconv2d-chw/gen/3x3s2p1-minmax-wasmsimd-x86-2x4-acc2.c
+
 tools/xngen src/f32-dwconv2d-chw/5x5p2-wasmsimd.c.in -D ROW_TILE=1 -D ACCUMULATORS=1 -D X86=0 -o src/f32-dwconv2d-chw/gen/5x5p2-minmax-wasmsimd-arm-1x4.c
 tools/xngen src/f32-dwconv2d-chw/5x5p2-wasmsimd.c.in -D ROW_TILE=2 -D ACCUMULATORS=1 -D X86=0 -o src/f32-dwconv2d-chw/gen/5x5p2-minmax-wasmsimd-arm-2x4.c
 tools/xngen src/f32-dwconv2d-chw/5x5p2-wasmsimd.c.in -D ROW_TILE=3 -D ACCUMULATORS=1 -D X86=0 -o src/f32-dwconv2d-chw/gen/5x5p2-minmax-wasmsimd-arm-3x4.c
