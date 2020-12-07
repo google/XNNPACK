c7634889e1e9e794dccb10dc839637fb0bcf900f
Marat Dukhan <maratek@google.com>
2020-12-07 15:11:12 -0800

    CONV2D HWC2CHW microkernel for ARM NEON
    PiperOrigin-RevId: 346189749

--- src/xnnpack/conv.h
+++ src/xnnpack/conv.h
@@ -72,6 +72,7 @@ DECLARE_F32_CONV_HWC_UKERNEL_FUNCTION(xnn_f32_conv_hwc_ukernel_3x3s2p0p1c3x4__sc
       size_t output_channel_stride,                          \
       const union xnn_f32_minmax_params* params);
 
+DECLARE_F32_CONV_HWC2CHW_UKERNEL_FUNCTION(xnn_f32_conv_hwc2chw_ukernel_3x3s2p1c3x4__neon_2x2)
 DECLARE_F32_CONV_HWC2CHW_UKERNEL_FUNCTION(xnn_f32_conv_hwc2chw_ukernel_3x3s2p1c3x4__neonfma_2x2)
 DECLARE_F32_CONV_HWC2CHW_UKERNEL_FUNCTION(xnn_f32_conv_hwc2chw_ukernel_3x3s2p1c3x4__scalar_1x1)
 DECLARE_F32_CONV_HWC2CHW_UKERNEL_FUNCTION(xnn_f32_conv_hwc2chw_ukernel_3x3s2p1c3x4__sse_1x1)
