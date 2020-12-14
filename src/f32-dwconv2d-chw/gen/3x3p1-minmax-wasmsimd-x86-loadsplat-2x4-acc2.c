c30f310b5a618b06358af0fd96021b7e52b5b320
Frank Barchard <fbarchard@google.com>
2020-12-14 14:56:32 -0800

    WASMSIMD dwconv2d chw 3x3p1 replace loadsplat with vector load and shuffle
    PiperOrigin-RevId: 347475817

--- src/f32-dwconv2d-chw/gen/3x3p1-minmax-wasmsimd-x86-loadsplat-2x4-acc2.c
+++ src/f32-dwconv2d-chw/gen/3x3p1-minmax-wasmsimd-x86-loadsplat-2x4-acc2.c
@@ -36,16 +36,19 @@ void xnn_f32_dwconv2d_chw_ukernel_3x3p1__wasmsimd_x86_loadsplat_2x4_acc2(
   const v128_t vmax = wasm_v32x4_load_splat(&params->scalar.max);
   const v128_t vmin = wasm_v32x4_load_splat(&params->scalar.min);
 
-  const v128_t vbias = wasm_v32x4_load_splat(weights);
-  const v128_t vk00 = wasm_v32x4_load_splat(weights + 1);
-  const v128_t vk01 = wasm_v32x4_load_splat(weights + 2);
-  const v128_t vk02 = wasm_v32x4_load_splat(weights + 3);
-  const v128_t vk10 = wasm_v32x4_load_splat(weights + 4);
-  const v128_t vk11 = wasm_v32x4_load_splat(weights + 5);
-  const v128_t vk12 = wasm_v32x4_load_splat(weights + 6);
-  const v128_t vk20 = wasm_v32x4_load_splat(weights + 7);
-  const v128_t vk21 = wasm_v32x4_load_splat(weights + 8);
-  const v128_t vk22 = wasm_v32x4_load_splat(weights + 9);
+  const v128_t vw0123 = wasm_v128_load(weights);
+  const v128_t vw4567 = wasm_v128_load(weights + 4);
+  const v128_t vw89 = wasm_v64x2_load_splat(weights + 8);
+  const v128_t vbias = wasm_v32x4_shuffle(vw0123, vw0123, 0, 0, 0, 0);
+  const v128_t vk00 = wasm_v32x4_shuffle(vw0123, vw0123, 1, 1, 1, 1);
+  const v128_t vk01 = wasm_v32x4_shuffle(vw0123, vw0123, 2, 2, 2, 2);
+  const v128_t vk02 = wasm_v32x4_shuffle(vw0123, vw0123, 3, 3, 3, 3);
+  const v128_t vk10 = wasm_v32x4_shuffle(vw4567, vw4567, 0, 0, 0, 0);
+  const v128_t vk11 = wasm_v32x4_shuffle(vw4567, vw4567, 1, 1, 1, 1);
+  const v128_t vk12 = wasm_v32x4_shuffle(vw4567, vw4567, 2, 2, 2, 2);
+  const v128_t vk20 = wasm_v32x4_shuffle(vw4567, vw4567, 3, 3, 3, 3);
+  const v128_t vk21 = wasm_v32x4_shuffle(vw89, vw89, 0, 0, 0, 0);
+  const v128_t vk22 = wasm_v32x4_shuffle(vw89, vw89, 1, 1, 1, 1);
 
   const v128_t vzero = wasm_f32x4_splat(0.0f);
 
