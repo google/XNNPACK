b126f9c388881af7bbe182892bd8580831bd90e4
Nikita Shulga <nshulga@fb.com>
2020-12-21 14:35:12 -0800

    Update cpuinfo checkout hash Delete cpuinfo.patch as it was previously applied as https://github.com/pytorch/cpuinfo/pull/50

--- cmake/DownloadCpuinfo.cmake
+++ cmake/DownloadCpuinfo.cmake
@@ -12,12 +12,12 @@ PROJECT(cpuinfo-download NONE)
 
 INCLUDE(ExternalProject)
 ExternalProject_Add(cpuinfo
-  URL https://github.com/pytorch/cpuinfo/archive/ed8b86a253800bafdb7b25c5c399f91bff9cb1f3.zip
-  URL_HASH SHA256=a7f9a188148a1660149878f737f42783e72f33a4f842f3e362fee2c981613e53
+  URL https://github.com/pytorch/cpuinfo/archive/5916273f79a21551890fd3d56fc5375a78d1598d.zip
+  URL_HASH SHA256=2a160c527d3c58085ce260f34f9e2b161adc009b34186a2baf24e74376e89e6d
   SOURCE_DIR "${CMAKE_BINARY_DIR}/cpuinfo-source"
   BINARY_DIR "${CMAKE_BINARY_DIR}/cpuinfo"
   CONFIGURE_COMMAND ""
-  PATCH_COMMAND patch -p0 -i "${CMAKE_CURRENT_SOURCE_DIR}/cmake/cpuinfo.patch"
+  PATCH_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
   TEST_COMMAND ""
