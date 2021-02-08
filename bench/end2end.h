dc909cbba03282c6e624cf8921c09b8437095d4d
Frank Barchard <fbarchard@google.com>
2021-02-08 13:59:31 -0800

    qs8-gemm-e2e benchmark for comparing performance of quantized microkernels
    PiperOrigin-RevId: 356347150

--- bench/end2end.h
+++ bench/end2end.h
@@ -14,3 +14,7 @@
   BENCHMARK_CAPTURE(benchmark_fn, mobilenet_v2, models::FP32MobileNetV2)->Unit(benchmark::kMicrosecond)->UseRealTime(); \
   BENCHMARK_CAPTURE(benchmark_fn, mobilenet_v3_large, models::FP32MobileNetV3Large)->Unit(benchmark::kMicrosecond)->UseRealTime(); \
   BENCHMARK_CAPTURE(benchmark_fn, mobilenet_v3_small, models::FP32MobileNetV3Small)->Unit(benchmark::kMicrosecond)->UseRealTime();
+
+#define BENCHMARK_QS8_END2END(benchmark_fn) \
+  BENCHMARK_CAPTURE(benchmark_fn, mobilenet_v1, models::QS8MobileNetV1)->Unit(benchmark::kMicrosecond)->UseRealTime(); \
+  BENCHMARK_CAPTURE(benchmark_fn, mobilenet_v2, models::QS8MobileNetV2)->Unit(benchmark::kMicrosecond)->UseRealTime();
---
+++ bench/qs8-gemm-e2e.cc
@@ -0,0 +1,118 @@
+// Copyright 2021 Google LLC
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <random>
+#include <vector>
+
+#include <xnnpack.h>
+
+#include <benchmark/benchmark.h>
+
+#include "bench/end2end.h"
+#include "bench/utils.h"
+#include "models/models.h"
+#include <xnnpack/gemm.h>
+#include <xnnpack/igemm.h>
+#include <xnnpack/params.h>
+
+
+static void GEMMEnd2EndBenchmark(
+  benchmark::State& state,
+  models::ExecutionPlanFactory model_factory,
+  xnn_qs8_gemm_ukernel_function gemm,
+  xnn_qs8_igemm_ukernel_function igemm,
+  xnn_qs8_gemm_ukernel_function gemm1,
+  xnn_qs8_igemm_ukernel_function igemm1,
+  uint8_t mr, uint8_t nr, uint8_t log2_kr = 0, uint8_t log2_sr = 0,
+  benchmark::utils::IsaCheckFunction isa_check = nullptr)
+{
+  if (isa_check && !isa_check(state)) {
+    return;
+  }
+  if (xnn_initialize(nullptr /* allocator */) != xnn_status_success) {
+    state.SkipWithError("failed to initialize XNNPACK");
+    return;
+  }
+
+  // Override microkernels chosen in xnn_initialize
+  // Note: do not directly assign to xnn_params.qs8.gemm because it breaks older gcc.
+  xnn_params.qs8.gemm.minmax.gemm = xnn_init_hmp_gemm_ukernel(xnn_gemm_ukernel_function(gemm));
+  xnn_params.qs8.gemm.minmax.igemm = xnn_init_hmp_igemm_ukernel(xnn_igemm_ukernel_function(igemm));
+  xnn_params.qs8.gemm.minmax.gemm1 = xnn_init_hmp_gemm_ukernel(xnn_gemm_ukernel_function(gemm1));
+  xnn_params.qs8.gemm.minmax.igemm1 = xnn_init_hmp_igemm_ukernel(xnn_igemm_ukernel_function(igemm1));
+  xnn_params.qs8.gemm.mr = mr;
+  xnn_params.qs8.gemm.nr = nr;
+  xnn_params.qs8.gemm.log2_kr = log2_kr;
+  xnn_params.qs8.gemm.log2_sr = log2_sr;
+
+  auto execution_plan = model_factory(nullptr);
+  if (execution_plan.empty()) {
+    state.SkipWithError("failed to create a model");
+    return;
+  }
+
+  for (auto _ : state) {
+    for (const std::unique_ptr<xnn_operator, decltype(&xnn_delete_operator)>& op : execution_plan) {
+      xnn_status status = xnn_run_operator(op.get(), nullptr);
+      if (status != xnn_status_success) {
+        state.SkipWithError("failed to run a model");
+        return;
+      }
+    }
+  }
+
+  const uint64_t cpu_frequency = benchmark::utils::GetCurrentCpuFrequency();
+  if (cpu_frequency != 0) {
+    state.counters["cpufreq"] = cpu_frequency;
+  }
+}
+
+#if XNN_ARCH_ARM64 && XNN_ENABLE_ASSEMBLY
+  static void qs8_gemm_minmax_ukernel_4x16c4__aarch64_neondot_ld64(benchmark::State& state, models::ExecutionPlanFactory model) {
+    GEMMEnd2EndBenchmark(state, model,
+      xnn_qs8_gemm_minmax_ukernel_4x16c4__aarch64_neondot_ld64,
+      xnn_qs8_igemm_minmax_ukernel_4x16c4__neondot,
+      xnn_qs8_gemm_minmax_ukernel_1x16c4__aarch64_neondot_ld64,
+      xnn_qs8_igemm_minmax_ukernel_1x16c4__neondot,
+      4 /* mr */, 16 /* nr */, 2 /* log2_kr */, 0 /* log2_sr */,
+      benchmark::utils::CheckNEONDOT);
+  }
+
+  BENCHMARK_QS8_END2END(qs8_gemm_minmax_ukernel_4x16c4__aarch64_neondot_ld64)
+#endif  // XNN_ARCH_ARM64 && XNN_ENABLE_ASSEMBLY
+
+
+#if XNN_ARCH_ARM || XNN_ARCH_ARM64
+  static void qs8_gemm_minmax_ukernel_2x16__neon_mlal_lane(benchmark::State& state, models::ExecutionPlanFactory model) {
+    GEMMEnd2EndBenchmark(state, model,
+      xnn_qs8_gemm_minmax_ukernel_2x16__neon_mlal_lane,
+      xnn_qs8_igemm_minmax_ukernel_2x16__neon_mlal_lane,
+      xnn_qs8_gemm_minmax_ukernel_1x16__neon_mlal_lane,
+      xnn_qs8_igemm_minmax_ukernel_1x16__neon_mlal_lane,
+      2 /* mr */, 16 /* nr */, 0 /* log2_kr */, 0 /* log2_sr */,
+      benchmark::utils::CheckNEON);
+  }
+
+  BENCHMARK_QS8_END2END(qs8_gemm_minmax_ukernel_2x16__neon_mlal_lane);
+#endif  // XNN_ARCH_ARM || XNN_ARCH_ARM64
+
+#if SCALAR_IGEMM
+static void qs8_gemm_minmax_ukernel_8x8c4__scalar(benchmark::State& state, models::ExecutionPlanFactory model) {
+  GEMMEnd2EndBenchmark(state, model,
+    xnn_qs8_gemm_minmax_ukernel_8x8c4__scalar,
+    xnn_qs8_igemm_minmax_ukernel_8x8c4__scalar,
+    xnn_qs8_gemm_minmax_ukernel_8x8c4__scalar,
+    xnn_qs8_igemm_minmax_ukernel_8x8c4__scalar,
+    8 /* mr */, 8 /* nr */, 2 /* log2_kr */, 0 /* log2_sr */);
+
+BENCHMARK_QS8_END2END(qs8_gemm_minmax_ukernel_8x8c4__scalar);
+#endif  // SCALAR_IGEMM
+
+#ifndef XNNPACK_BENCHMARK_NO_MAIN
+BENCHMARK_MAIN();
+#endif
