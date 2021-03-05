fb8d1f1b2bb2e32c141564528a39748c4631b453
Frank Barchard <fbarchard@google.com>
2021-03-05 13:16:41 -0800

    Increase minimum value to avoid f16_vrdivc producing inf
    PiperOrigin-RevId: 361211049

--- test/vbinaryc-microkernel-tester.h
+++ test/vbinaryc-microkernel-tester.h
@@ -90,7 +90,7 @@ class VBinOpCMicrokernelTester {
   void Test(xnn_f16_vbinary_ukernel_function vbinaryc, OpType op_type) const {
     std::random_device random_device;
     auto rng = std::mt19937(random_device());
-    auto f32rng = std::bind(std::uniform_real_distribution<float>(1.0e-3f, 1.0f), rng);
+    auto f32rng = std::bind(std::uniform_real_distribution<float>(1.0e-2f, 1.0f), rng);
     auto f16rng = std::bind(fp16_ieee_from_fp32_value, f32rng);
 
     std::vector<uint16_t> a(batch_size() + XNN_EXTRA_BYTES / sizeof(uint16_t));
