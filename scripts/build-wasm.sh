c7e2a1b67abc8950528204c0ca116c296d731f93
arjunsurendran24 <arjunsurendr@gmail.com>
2021-03-15 15:02:20 -0700

    Build script for WASM build
    The scripts can be used like follows - ./scripts/build-wasm.sh
    Additionally the following options are provided for customizing the
    build
    - enable_simd - Build with SIMD enabled
    - enable_threads - Build with pthreads
    - enable_tests - Build with tests
    - enable_benchmarks - Build with benchmarks enabled

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-#
-# Copyright 2021 Adobe
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
 
 set -e
 
@@ -16,34 +11,44 @@ CMAKE_ARGS=()
 CMAKE_ARGS+=("-DCMAKE_BUILD_TYPE=Release")
 CMAKE_ARGS+=("-DXNNPACK_LIBRARY_TYPE=static")
 
-if [ "$1"  ==  "build_only" ] || [ "$2"  ==  "build_only" ] || [ "$3"  ==  "build_only" ];
-then
-    CMAKE_ARGS+=("-DXNNPACK_BUILD_BENCHMARKS=OFF")
-    CMAKE_ARGS+=("-DXNNPACK_BUILD_TESTS=OFF")
+if [ "$1" == "enable_tests" ] || [ "$2" == "enable_tests" ] || [ "$3" == "enable_tests" ] || [ "$4" == "enable_tests" ]; then
+  echo "Building tests"
+  CMAKE_ARGS+=("-DXNNPACK_BUILD_TESTS=ON")
 else
-    CMAKE_ARGS+=("-DXNNPACK_BUILD_BENCHMARKS=ON")
-    CMAKE_ARGS+=("-DXNNPACK_BUILD_TESTS=ON")
+  echo "Skipping tests"
+  CMAKE_ARGS+=("-DXNNPACK_BUILD_TESTS=OFF")
 fi
 
-if [ "$1"  ==  "enable_simd" ] || [ "$2"  ==  "enable_simd" ] || [ "$3"  ==  "enable_simd" ];
-then
-    CMAKE_ARGS+=("-DXNNPACK_BUILD_WASM_WITH_SIMD=ON")
+if [ "$1" == "enable_benchmarks" ] || [ "$2" == "enable_benchmarks" ] || [ "$3" == "enable_benchmarks" ] || [ "$4" == "enable_benchmarks" ]; then
+  echo "Building benchmarks"
+  CMAKE_ARGS+=("-DXNNPACK_BUILD_BENCHMARKS=ON")
+else
+  echo "Skipping benchmarks"
+  CMAKE_ARGS+=("-DXNNPACK_BUILD_BENCHMARKS=OFF")
 fi
 
-if [ "$1"  ==  "enable_threads" ] || [ "$2"  ==  "enable_threads" ] || [ "$3"  ==  "enable_threads" ];
-then
-    CMAKE_ARGS+=("-DXNNPACK_BUILD_WASM_WITH_PTHREAD=ON")
+if [ "$1" == "enable_simd" ] || [ "$2" == "enable_simd" ] || [ "$3" == "enable_simd" ] || [ "$4" == "enable_simd" ]; then
+  echo "Building with SIMD enabled"
+  CMAKE_ARGS+=("-DXNNPACK_BUILD_WASM_WITH_SIMD=ON")
+else
+  CMAKE_ARGS+=("-DXNNPACK_BUILD_WASM_WITH_SIMD=OFF")
+fi
+
+if [ "$1" == "enable_threads" ] || [ "$2" == "enable_threads" ] || [ "$3" == "enable_threads" ] || [ "$4" == "enable_threads" ]; then
+  echo "Building with threading support"
+  CMAKE_ARGS+=("-DXNNPACK_BUILD_WASM_WITH_PTHREAD=ON")
+else
+  CMAKE_ARGS+=("-DXNNPACK_BUILD_WASM_WITH_PTHREAD=OFF")
 fi
 
 CMAKE_ARGS+=()
 
-cd build/local && emcmake cmake ../.. \
-    "${CMAKE_ARGS[@]}"
+cd build/wasm && emcmake cmake ../.. \
+  "${CMAKE_ARGS[@]}"
 
 # Cross-platform parallel build
-if [ "$(uname)" == "Darwin" ]
-then
-  emmake make "-j$(sysctl -n hw.ncpu)"
+if [ "$(uname)" == "Darwin" ]; then
+  emmake make "-j$(sysctl -n hw.ncpu)" --ignore-errors
 else
   emmake make "-j$(nproc)"
 fi
