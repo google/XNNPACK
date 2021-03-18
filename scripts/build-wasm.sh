1b1229e9098a1ec9131a6806231c094e573e7a9f
arjunsurendran24 <arjunsurendr@gmail.com>
2021-03-18 10:01:51 -0700

    Addressing reviewer comments
    1. Removed SIMD and thread compile options from CMakeLists
    2. Building with Ninja
    3. Move Linker Arguments to build script
    4. Taking CMAKE options directly from CLI

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -17,44 +17,34 @@ CMAKE_ARGS=()
 CMAKE_ARGS+=("-DCMAKE_BUILD_TYPE=Release")
 CMAKE_ARGS+=("-DXNNPACK_LIBRARY_TYPE=static")
 
-if [ "$1" == "enable_tests" ] || [ "$2" == "enable_tests" ] || [ "$3" == "enable_tests" ] || [ "$4" == "enable_tests" ]; then
-  echo "Building tests"
-  CMAKE_ARGS+=("-DXNNPACK_BUILD_TESTS=ON")
-else
-  echo "Skipping tests"
-  CMAKE_ARGS+=("-DXNNPACK_BUILD_TESTS=OFF")
-fi
-
-if [ "$1" == "enable_benchmarks" ] || [ "$2" == "enable_benchmarks" ] || [ "$3" == "enable_benchmarks" ] || [ "$4" == "enable_benchmarks" ]; then
-  echo "Building benchmarks"
-  CMAKE_ARGS+=("-DXNNPACK_BUILD_BENCHMARKS=ON")
-else
-  echo "Skipping benchmarks"
-  CMAKE_ARGS+=("-DXNNPACK_BUILD_BENCHMARKS=OFF")
-fi
-
-if [ "$1" == "enable_simd" ] || [ "$2" == "enable_simd" ] || [ "$3" == "enable_simd" ] || [ "$4" == "enable_simd" ]; then
-  echo "Building with SIMD enabled"
-  CMAKE_ARGS+=("-DXNNPACK_BUILD_WASM_WITH_SIMD=ON")
-else
-  CMAKE_ARGS+=("-DXNNPACK_BUILD_WASM_WITH_SIMD=OFF")
-fi
-
-if [ "$1" == "enable_threads" ] || [ "$2" == "enable_threads" ] || [ "$3" == "enable_threads" ] || [ "$4" == "enable_threads" ]; then
-  echo "Building with threading support"
-  CMAKE_ARGS+=("-DXNNPACK_BUILD_WASM_WITH_PTHREAD=ON")
-else
-  CMAKE_ARGS+=("-DXNNPACK_BUILD_WASM_WITH_PTHREAD=OFF")
+# If Ninja is installed, prefer it to Make
+if [ -x "$(command -v ninja)" ]
+then
+  CMAKE_ARGS+=("-GNinja")
 fi
 
 CMAKE_ARGS+=()
 
+CMAKE_ARGS+=("-DXNNPACK_BUILD_BENCHMARKS=OFF")
+CMAKE_ARGS+=("-DXNNPACK_BUILD_TESTS=OFF")
+
+# Use-specified CMake arguments go last to allow overridding defaults
+CMAKE_ARGS+=($@)
+
+if [[ "$*" == "-DXNNPACK_BUILD_TESTS=ON" ]]
+then
+    CMAKE_ARGS+=("-DCMAKE_EXE_LINKER_FLAGS=-s ASSERTIONS=2 -s ERROR_ON_UNDEFINED_SYMBOLS=1 -s DEMANGLE_SUPPORT=1 -s EXIT_RUNTIME=1 -s ALLOW_MEMORY_GROWTH=1")
+    CMAKE_ARGS+=("-DCMAKE_EXECUTABLE_SUFFIX=\".html\"")
+fi
+
+if [[ "$*" == "-DXNNPACK_BUILD_BENCHMARKS=ON" ]]
+then
+    CMAKE_ARGS+=("-DXNNPACK_BUILD_TESTS=ON")
+    CMAKE_ARGS+=("-DCMAKE_EXE_LINKER_FLAGS=-s ASSERTIONS=2 -s ERROR_ON_UNDEFINED_SYMBOLS=1 -s EXIT_RUNTIME=1 -s ALLOW_MEMORY_GROWTH=1 -s TOTAL_MEMORY=436207616")
+    CMAKE_ARGS+=("-DCMAKE_EXECUTABLE_SUFFIX=\".html\"")
+fi
+
 cd build/wasm && emcmake cmake ../.. \
   "${CMAKE_ARGS[@]}"
 
-# Cross-platform parallel build
-if [ "$(uname)" == "Darwin" ]; then
-  emmake make "-j$(sysctl -n hw.ncpu)" --ignore-errors
-else
-  emmake make "-j$(nproc)"
-fi
+cmake --build .
