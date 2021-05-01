184a8e1e2a9f88d12af0d63230c994f275db27bc
Frank Barchard <fbarchard@google.com>
2021-04-30 17:52:16 -0700

    Enable A53 C8 microkernel load A with ldr/ldr/ins
    PiperOrigin-RevId: 371435767

--- src/init.c
+++ src/init.c
@@ -871,12 +871,13 @@ static void init(void) {
           switch (cpuinfo_get_core(0)->uarch) {
             case cpuinfo_uarch_cortex_a53:
             case cpuinfo_uarch_cortex_a55r0:
-              xnn_params.qs8.gemm.minmax.gemm = xnn_init_hmp_gemm_ukernel((xnn_gemm_ukernel_function) xnn_qs8_gemm_minmax_ukernel_4x16__neon_mlal_lane_prfm);
-              xnn_params.qs8.gemm.minmax.igemm = xnn_init_hmp_igemm_ukernel((xnn_igemm_ukernel_function) xnn_qs8_igemm_minmax_ukernel_4x16__neon_mlal_lane_prfm);
-              xnn_params.qs8.gemm.minmax.gemm1 = xnn_init_hmp_gemm_ukernel((xnn_gemm_ukernel_function) xnn_qs8_gemm_minmax_ukernel_1x16__neon_mlal_lane_prfm);
-              xnn_params.qs8.gemm.minmax.igemm1 = xnn_init_hmp_igemm_ukernel((xnn_igemm_ukernel_function) xnn_qs8_igemm_minmax_ukernel_1x16__neon_mlal_lane_prfm);
-              xnn_params.qs8.gemm.mr = 4;
-              xnn_params.qs8.gemm.nr = 16;
+              xnn_params.qs8.gemm.minmax.gemm = xnn_init_hmp_gemm_ukernel((xnn_gemm_ukernel_function) xnn_qs8_gemm_minmax_ukernel_2x8c8__aarch64_neon_mlal_padal_prfm_cortex_a53);
+              xnn_params.qs8.gemm.minmax.igemm = xnn_init_hmp_igemm_ukernel((xnn_igemm_ukernel_function) xnn_qs8_igemm_minmax_ukernel_2x8c8__aarch64_neon_mlal_padal_prfm_cortex_a53);
+              xnn_params.qs8.gemm.minmax.gemm1 = xnn_init_hmp_gemm_ukernel((xnn_gemm_ukernel_function) xnn_qs8_gemm_minmax_ukernel_1x8c8__neon_mlal_padal);
+              xnn_params.qs8.gemm.minmax.igemm1 = xnn_init_hmp_igemm_ukernel((xnn_igemm_ukernel_function) xnn_qs8_igemm_minmax_ukernel_1x8c8__neon_mlal_padal);
+              xnn_params.qs8.gemm.mr = 2;
+              xnn_params.qs8.gemm.nr = 8;
+              xnn_params.qs8.gemm.log2_kr = 3;
               break;
 
             case cpuinfo_uarch_cortex_a72:
