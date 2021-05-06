b1eec08debf2a8533bc70339c17188b98091360b
Marat Dukhan <maratek@google.com>
2021-05-05 23:24:55 -0700

    Refactor filenames of Clamp/Relu/Sigmoid microkernels
    Prefix filenames with the microkernel type, similarly to other vector
    elementwise microkernels.
    
    PiperOrigin-RevId: 372280088

--- scripts/generate-f16-vrelu.sh
+++ scripts/generate-f16-vrelu.sh
@@ -5,8 +5,8 @@
 # LICENSE file in the root directory of this source tree.
 
 ################################### ARM NEON ##################################
-tools/xngen src/f16-vrelu/neonfp16arith.c.in -D BATCH_TILE=8  -o src/f16-vrelu/gen/neonfp16arith-x8.c
-tools/xngen src/f16-vrelu/neonfp16arith.c.in -D BATCH_TILE=16 -o src/f16-vrelu/gen/neonfp16arith-x16.c
+tools/xngen src/f16-vrelu/neonfp16arith.c.in -D BATCH_TILE=8  -o src/f16-vrelu/gen/vrelu-neonfp16arith-x8.c
+tools/xngen src/f16-vrelu/neonfp16arith.c.in -D BATCH_TILE=16 -o src/f16-vrelu/gen/vrelu-neonfp16arith-x16.c
 
 ################################## Unit tests #################################
 tools/generate-vunary-test.py --spec test/f16-vrelu.yaml --output test/f16-vrelu.cc
