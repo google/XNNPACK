3b6c36e75001544dfea2b61673a7f9d8e89db986
Marat Dukhan <maratek@google.com>
2021-08-06 22:03:01 -0700

    Refactor F32 VRELU benchmarks
    - Use benchmark::utils::UnaryElementwiseParameters
    - Refactor similarly to QADDC & QMULC benchmarks
    
    PiperOrigin-RevId: 389325927

--- bench/f32-vrelu.cc
+++ bench/f32-vrelu.cc
@@ -11,6 +11,7 @@
 
 #include <benchmark/benchmark.h>
 #include "bench/utils.h"
+
 #include <xnnpack/AlignedAllocator.h>
 #include <xnnpack/common.h>
 #include <xnnpack/params.h>
@@ -26,19 +27,19 @@ static void f32_vrelu(
     return;
   }
 
-  const size_t elements = state.range(0);
+  const size_t num_elements = state.range(0);
 
   std::random_device random_device;
   auto rng = std::mt19937(random_device());
   auto f32rng = std::bind(std::uniform_real_distribution<float>(-10.0f, 10.0f), std::ref(rng));
 
-  std::vector<float, AlignedAllocator<float, 64>> x(elements);
+  std::vector<float, AlignedAllocator<float, 64>> x(num_elements);
   std::generate(x.begin(), x.end(), std::ref(f32rng));
-  std::vector<float, AlignedAllocator<float, 64>> y(elements);
+  std::vector<float, AlignedAllocator<float, 64>> y(num_elements);
   std::generate(x.begin(), x.end(), std::ref(f32rng));
 
   for (auto _ : state) {
-    f32_vrelu(elements * sizeof(float), x.data(), y.data(), NULL);
+    f32_vrelu(num_elements * sizeof(float), x.data(), y.data(), NULL);
   }
 
   const uint64_t cpu_frequency = benchmark::utils::GetCurrentCpuFrequency();
@@ -46,131 +47,137 @@ static void f32_vrelu(
     state.counters["cpufreq"] = cpu_frequency;
   }
 
-  const size_t elements_per_iteration = elements;
+  const size_t elements_per_iteration = num_elements;
   state.counters["elements"] =
     benchmark::Counter(uint64_t(state.iterations()) * elements_per_iteration, benchmark::Counter::kIsRate);
 
-  const size_t bytes_per_iteration = 2 * elements * sizeof(float);
+  const size_t bytes_per_iteration = 2 * num_elements * sizeof(float);
   state.counters["bytes"] =
     benchmark::Counter(uint64_t(state.iterations()) * bytes_per_iteration, benchmark::Counter::kIsRate);
 }
 
 #if XNN_ARCH_X86 || XNN_ARCH_X86_64
-  BENCHMARK_CAPTURE(f32_vrelu, sse_x4, xnn_f32_vrelu_ukernel__sse_x4)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, sse_x4,
+                    xnn_f32_vrelu_ukernel__sse_x4)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 
-  BENCHMARK_CAPTURE(f32_vrelu, sse_x8, xnn_f32_vrelu_ukernel__sse_x8)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, sse_x8,
+                    xnn_f32_vrelu_ukernel__sse_x8)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 
-  BENCHMARK_CAPTURE(f32_vrelu, avx_x8, xnn_f32_vrelu_ukernel__avx_x8, benchmark::utils::CheckAVX)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, avx_x8,
+                    xnn_f32_vrelu_ukernel__avx_x8,
+                    benchmark::utils::CheckAVX)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 
-  BENCHMARK_CAPTURE(f32_vrelu, avx_x16, xnn_f32_vrelu_ukernel__avx_x16, benchmark::utils::CheckAVX)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, avx_x16,
+                    xnn_f32_vrelu_ukernel__avx_x16,
+                    benchmark::utils::CheckAVX)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 
-  BENCHMARK_CAPTURE(f32_vrelu, avx512f_x16, xnn_f32_vrelu_ukernel__avx512f_x16, benchmark::utils::CheckAVX512F)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, avx512f_x16,
+                    xnn_f32_vrelu_ukernel__avx512f_x16,
+                    benchmark::utils::CheckAVX512F)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 
-  BENCHMARK_CAPTURE(f32_vrelu, avx512f_x32, xnn_f32_vrelu_ukernel__avx512f_x32, benchmark::utils::CheckAVX512F)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, avx512f_x32,
+                    xnn_f32_vrelu_ukernel__avx512f_x32,
+                    benchmark::utils::CheckAVX512F)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 #endif  // XNN_ARCH_X86 || XNN_ARCH_X86_64
 
 #if XNN_ARCH_ARM || XNN_ARCH_ARM64
-  BENCHMARK_CAPTURE(f32_vrelu, neon_x4, xnn_f32_vrelu_ukernel__neon_x4, benchmark::utils::CheckNEON)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, neon_x4,
+                    xnn_f32_vrelu_ukernel__neon_x4,
+                    benchmark::utils::CheckNEON)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
-  BENCHMARK_CAPTURE(f32_vrelu, neon_x8, xnn_f32_vrelu_ukernel__neon_x8, benchmark::utils::CheckNEON)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, neon_x8,
+                    xnn_f32_vrelu_ukernel__neon_x8,
+                    benchmark::utils::CheckNEON)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 #endif  // XNN_ARCH_ARM || XNN_ARCH_ARM64
 
 
 #if XNN_ARCH_WASM || XNN_ARCH_WASMSIMD
-  BENCHMARK_CAPTURE(f32_vrelu, wasm_x1, xnn_f32_vrelu_ukernel__wasm_x1)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, wasm_x1,
+                    xnn_f32_vrelu_ukernel__wasm_x1)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 
-  BENCHMARK_CAPTURE(f32_vrelu, wasm_x2, xnn_f32_vrelu_ukernel__wasm_x2)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, wasm_x2,
+                    xnn_f32_vrelu_ukernel__wasm_x2)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 
-  BENCHMARK_CAPTURE(f32_vrelu, wasm_x4, xnn_f32_vrelu_ukernel__wasm_x4)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, wasm_x4,
+                    xnn_f32_vrelu_ukernel__wasm_x4)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 
-  BENCHMARK_CAPTURE(f32_vrelu, wasm_x8, xnn_f32_vrelu_ukernel__wasm_x8)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, wasm_x8,
+                    xnn_f32_vrelu_ukernel__wasm_x8)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 
-  BENCHMARK_CAPTURE(f32_vrelu, wasm32_shr_x1, xnn_f32_vrelu_ukernel__wasm32_shr_x1)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, wasm32_shr_x1,
+                    xnn_f32_vrelu_ukernel__wasm32_shr_x1)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 
-  BENCHMARK_CAPTURE(f32_vrelu, wasm32_shr_x2, xnn_f32_vrelu_ukernel__wasm32_shr_x2)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, wasm32_shr_x2,
+                    xnn_f32_vrelu_ukernel__wasm32_shr_x2)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 
-  BENCHMARK_CAPTURE(f32_vrelu, wasm32_shr_x4, xnn_f32_vrelu_ukernel__wasm32_shr_x4)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, wasm32_shr_x4,
+                    xnn_f32_vrelu_ukernel__wasm32_shr_x4)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 #endif  // XNN_ARCH_WASM || XNN_ARCH_WASMSIMD
 
 #if XNN_ARCH_WASMSIMD
-  BENCHMARK_CAPTURE(f32_vrelu, wasmsimd_x4, xnn_f32_vrelu_ukernel__wasmsimd_x4)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, wasmsimd_x4,
+                    xnn_f32_vrelu_ukernel__wasmsimd_x4)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 
-  BENCHMARK_CAPTURE(f32_vrelu, wasmsimd_x8, xnn_f32_vrelu_ukernel__wasmsimd_x8)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, wasmsimd_x8,
+                    xnn_f32_vrelu_ukernel__wasmsimd_x8)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 
-  BENCHMARK_CAPTURE(f32_vrelu, wasmsimd_x16, xnn_f32_vrelu_ukernel__wasmsimd_x16)
-    ->RangeMultiplier(10)
-    ->Range(1000, 100000000)
+  BENCHMARK_CAPTURE(f32_vrelu, wasmsimd_x16,
+                    xnn_f32_vrelu_ukernel__wasmsimd_x16)
+    ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
     ->UseRealTime();
 #endif  // XNN_ARCH_WASMSIMD
 
-BENCHMARK_CAPTURE(f32_vrelu, scalar_x1, xnn_f32_vrelu_ukernel__scalar_x1)
-  ->RangeMultiplier(10)
-  ->Range(1000, 100000000)
+BENCHMARK_CAPTURE(f32_vrelu, scalar_x1,
+                  xnn_f32_vrelu_ukernel__scalar_x1)
+  ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
   ->UseRealTime();
 
-BENCHMARK_CAPTURE(f32_vrelu, scalar_x2, xnn_f32_vrelu_ukernel__scalar_x2)
-  ->RangeMultiplier(10)
-  ->Range(1000, 100000000)
+BENCHMARK_CAPTURE(f32_vrelu, scalar_x2,
+                  xnn_f32_vrelu_ukernel__scalar_x2)
+  ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
   ->UseRealTime();
 
-BENCHMARK_CAPTURE(f32_vrelu, scalar_x4, xnn_f32_vrelu_ukernel__scalar_x4)
-  ->RangeMultiplier(10)
-  ->Range(1000, 100000000)
+BENCHMARK_CAPTURE(f32_vrelu, scalar_x4,
+                  xnn_f32_vrelu_ukernel__scalar_x4)
+  ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
   ->UseRealTime();
 
-BENCHMARK_CAPTURE(f32_vrelu, scalar_x8, xnn_f32_vrelu_ukernel__scalar_x8)
-  ->RangeMultiplier(10)
-  ->Range(1000, 100000000)
+BENCHMARK_CAPTURE(f32_vrelu, scalar_x8,
+                  xnn_f32_vrelu_ukernel__scalar_x8)
+  ->Apply(benchmark::utils::UnaryElementwiseParameters<float, float>)
   ->UseRealTime();
 
 #ifndef XNNPACK_BENCHMARK_NO_MAIN
