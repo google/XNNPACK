694d2524757f9040e65a02c374e152a462fe57eb
Marat Dukhan <maratek@google.com>
2021-09-21 12:11:41 -0700

    Fix incorrect initialization of QC8 GEMM/IGEMM parameters on AArch32+NEONDOT
    PiperOrigin-RevId: 398053926

--- src/init.c
+++ src/init.c
@@ -108,7 +108,7 @@ static void init(void) {
         xnn_params.qc8.gemm.minmax.gemm1 = xnn_init_hmp_gemm_ukernel((xnn_gemm_ukernel_function) xnn_qc8_gemm_minmax_fp32_ukernel_1x8c4__neondot);
         xnn_params.qc8.gemm.minmax.igemm = xnn_init_hmp_igemm_ukernel((xnn_igemm_ukernel_function) xnn_qc8_igemm_minmax_fp32_ukernel_4x8c4__neondot);
         xnn_params.qc8.gemm.minmax.igemm1 = xnn_init_hmp_igemm_ukernel((xnn_igemm_ukernel_function) xnn_qc8_igemm_minmax_fp32_ukernel_1x8c4__neondot);
-        xnn_params.qc8.gemm.init.qc8 = xnn_init_qs8_minmax_neon_fp32_params;
+        xnn_params.qc8.gemm.init.qc8 = xnn_init_qs8_minmax_neon_params;
         xnn_params.qc8.gemm.mr = 4;
         xnn_params.qc8.gemm.nr = 8;
         xnn_params.qc8.gemm.log2_kr = 2;
