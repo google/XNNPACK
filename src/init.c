b654abff8d91dec5c36f19053901bca1d7ad37e2
Marat Dukhan <maratek@google.com>
2021-11-22 19:45:39 -0800

    Fix broken QC8 Convolution on AArch32 ARMv8 processors
    PiperOrigin-RevId: 411702638

--- src/init.c
+++ src/init.c
@@ -118,7 +118,7 @@ static void init(void) {
         xnn_params.qc8.gemm.minmax.igemm = xnn_init_hmp_igemm_ukernel((xnn_igemm_ukernel_function) xnn_qc8_igemm_minmax_fp32_ukernel_2x8c2s4__neonv8_mlal);
         xnn_params.qc8.gemm.minmax.gemm1 = xnn_init_hmp_gemm_ukernel((xnn_gemm_ukernel_function) xnn_qc8_gemm_minmax_fp32_ukernel_1x8c2s4__neonv8_mlal);
         xnn_params.qc8.gemm.minmax.igemm1 = xnn_init_hmp_igemm_ukernel((xnn_igemm_ukernel_function) xnn_qc8_igemm_minmax_fp32_ukernel_1x8c2s4__neonv8_mlal);
-        xnn_params.qc8.gemm.init.qc8 = xnn_init_qs8_minmax_neon_fp32_params;
+        xnn_params.qc8.gemm.init.qc8 = xnn_init_qs8_minmax_neon_params;
         xnn_params.qc8.gemm.mr = 2;
         xnn_params.qc8.gemm.nr = 8;
         xnn_params.qc8.gemm.log2_kr = 1;
@@ -137,11 +137,11 @@ static void init(void) {
 
       if (cpuinfo_has_arm_neon_v8()) {
         xnn_params.qc8.dwconv[0].minmax.unipass = (xnn_dwconv_unipass_ukernel_function) xnn_qc8_dwconv_minmax_fp32_ukernel_up16x9__neonv8_mla8_ld64;
-        xnn_params.qc8.dwconv[0].init.qc8 = xnn_init_qs8_minmax_neon_fp32_params;
+        xnn_params.qc8.dwconv[0].init.qc8 = xnn_init_qs8_minmax_neon_params;
         xnn_params.qc8.dwconv[0].channel_tile = 16;
         xnn_params.qc8.dwconv[0].primary_tile = 9;
         xnn_params.qc8.dwconv[1].minmax.unipass = (xnn_dwconv_unipass_ukernel_function) xnn_qc8_dwconv_minmax_fp32_ukernel_up8x25__neonv8_mla8_ld64;
-        xnn_params.qc8.dwconv[1].init.qc8 = xnn_init_qs8_minmax_neon_fp32_params;
+        xnn_params.qc8.dwconv[1].init.qc8 = xnn_init_qs8_minmax_neon_params;
         xnn_params.qc8.dwconv[1].channel_tile = 8;
         xnn_params.qc8.dwconv[1].primary_tile = 25;
       } else {
