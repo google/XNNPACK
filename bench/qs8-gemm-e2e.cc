9f3f4204ecb3b8d949acf20fb95d9c3087a907d7
Frank Barchard <fbarchard@google.com>
2021-12-16 18:13:51 -0800

    QS8 4x8 LD64 dot product GEMM AArch32 microkernel
    PiperOrigin-RevId: 416933737

--- bench/qs8-gemm-e2e.cc
+++ bench/qs8-gemm-e2e.cc
@@ -76,6 +76,16 @@ static void GEMMEnd2EndBenchmark(
 }
 
 #if XNN_ARCH_ARM && XNN_ENABLE_ASSEMBLY
+  static void qs8_gemm_4x8c4__aarch32_neondot_ld64(benchmark::State& state, models::ExecutionPlanFactory model) {
+    GEMMEnd2EndBenchmark(state, model,
+      xnn_qs8_gemm_minmax_rndnu_ukernel_4x8c4__aarch32_neondot_ld64,
+      xnn_qs8_igemm_minmax_rndnu_ukernel_4x8c4__neondot,
+      xnn_qs8_gemm_minmax_rndnu_ukernel_1x8c4__neondot,
+      xnn_qs8_igemm_minmax_rndnu_ukernel_1x8c4__neondot,
+      xnn_init_qs8_conv_minmax_rndnu_neon_params,
+      4 /* mr */, 8 /* nr */, 2 /* log2_kr */, 0 /* log2_sr */,
+      benchmark::utils::CheckNEONDOT);
+  }
   static void qs8_gemm_4x8__aarch32_neon_mlal_lane_ld64(benchmark::State& state, models::ExecutionPlanFactory model) {
     GEMMEnd2EndBenchmark(state, model,
       xnn_qs8_gemm_minmax_rndnu_ukernel_4x8__aarch32_neon_mlal_lane_ld64,
@@ -96,7 +106,7 @@ static void GEMMEnd2EndBenchmark(
       4 /* mr */, 8 /* nr */, 0 /* log2_kr */, 0 /* log2_sr */,
       benchmark::utils::CheckNEON);
   }
-
+  BENCHMARK_QS8_END2END(qs8_gemm_4x8c4__aarch32_neondot_ld64)
   BENCHMARK_QS8_END2END(qs8_gemm_4x8__aarch32_neon_mlal_lane_prfm_ld64)
   BENCHMARK_QS8_END2END(qs8_gemm_4x8__aarch32_neon_mlal_lane_ld64)
 #endif  // XNN_ARCH_ARM && XNN_ENABLE_ASSEMBLY
