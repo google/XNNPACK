23cb2a0cb7e1393f52d35f231cf85c8002b25bc8
Frank Barchard <fbarchard@google.com>
2022-01-24 15:59:35 -0800

    QS8 4x8 dot product GEMM AArch32 microkernel for Cortex A55 quantization optimized
    - reduce stack push by 16 bytes improving QS8 GEMM performance
    - FP32 quantization added.
    - Remainder with prologue for remainder after epilogue.
    - Use TST to check for remainder.
    - Sub k not kc for epilogue test.
    - Read d10,d11 for B6/B7 instead of q10, q11
    
    PiperOrigin-RevId: 423933115

--- scripts/generate-qs8-gemm.sh
+++ scripts/generate-qs8-gemm.sh
@@ -729,7 +729,7 @@ tools/xngen src/qs8-gemm/4x8-aarch32-neon-mlal-lane-ld64.S.in        -D PREFETCH
 ### C4 micro-kernels
 tools/xngen src/qs8-gemm/4x8c4-aarch32-neondot-ld64.S.in             -D PREFETCH=0 -D REQUANTIZATION=RNDNU -D CHANNELWISE=0 -D DATATYPE=QS8 -o src/qs8-gemm/gen/4x8c4-minmax-rndnu-aarch32-neondot-ld64.S &
 tools/xngen src/qs8-gemm/4x8c4-aarch32-neondot-ld64.S.in             -D PREFETCH=0 -D REQUANTIZATION=FP32  -D CHANNELWISE=1 -D DATATYPE=QC8 -o src/qc8-gemm/gen/4x8c4-minmax-fp32-aarch32-neondot-ld64.S &
-tools/xngen src/qs8-gemm/4x8c4-aarch32-neondot-cortex-a55.S.in                     -D REQUANTIZATION=RNDNU -D CHANNELWISE=0                 -o src/qs8-gemm/gen/4x8c4-minmax-rndnu-aarch32-neondot-cortex-a55.S &
+tools/xngen src/qs8-gemm/4x8c4-aarch32-neondot-cortex-a55.S.in       -D PREFETCH=0 -D REQUANTIZATION=RNDNU -D CHANNELWISE=0 -D DATATYPE=QS8 -o src/qs8-gemm/gen/4x8c4-minmax-rndnu-aarch32-neondot-cortex-a55.S &
 
 ############################### AArch64 assembly ##############################
 ### Cortex-A53 lane micro-kernels
