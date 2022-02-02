9e4d2aa68dd476d5e56464c5dee4bebb3496fb94
Frank Barchard <fbarchard@google.com>
2022-02-02 00:31:21 -0800

    QS8 4x8 lane GEMM AArch32 microkernel for Cortex A53
    - q4 and q5 for weights.  additional push of d12/d13.
    - kc (r2) and lr pushed onto stack.
    - load weights and expand in previous block
    
    PiperOrigin-RevId: 425813466

--- bench/qs8-gemm-e2e.cc
+++ bench/qs8-gemm-e2e.cc
@@ -76,6 +76,16 @@ static void GEMMEnd2EndBenchmark(
 }
 
 #if XNN_ARCH_ARM && XNN_ENABLE_ASSEMBLY
+  static void qs8_gemm_4x8c4__aarch32_neondot_cortex_a55(benchmark::State& state, models::ExecutionPlanFactory model) {
+    GEMMEnd2EndBenchmark(state, model,
+      xnn_qs8_gemm_minmax_rndnu_ukernel_4x8c4__aarch32_neondot_cortex_a55,
+      xnn_qs8_igemm_minmax_rndnu_ukernel_4x8c4__aarch32_neondot_cortex_a55,
+      xnn_qs8_gemm_minmax_rndnu_ukernel_1x8c4__neondot,
+      xnn_qs8_igemm_minmax_rndnu_ukernel_1x8c4__neondot,
+      xnn_init_qs8_conv_minmax_rndnu_neon_params,
+      4 /* mr */, 8 /* nr */, 2 /* log2_kr */, 0 /* log2_sr */,
+      benchmark::utils::CheckNEONDOT);
+  }
   static void qs8_gemm_4x8c4__aarch32_neondot_ld64(benchmark::State& state, models::ExecutionPlanFactory model) {
     GEMMEnd2EndBenchmark(state, model,
       xnn_qs8_gemm_minmax_rndnu_ukernel_4x8c4__aarch32_neondot_ld64,
@@ -86,15 +96,25 @@ static void GEMMEnd2EndBenchmark(
       4 /* mr */, 8 /* nr */, 2 /* log2_kr */, 0 /* log2_sr */,
       benchmark::utils::CheckNEONDOT);
   }
-  static void qs8_gemm_4x8c4__aarch32_neondot_cortex_a55(benchmark::State& state, models::ExecutionPlanFactory model) {
+  static void qs8_gemm_4x8__aarch32_neon_mlal_lane_cortex_a53(benchmark::State& state, models::ExecutionPlanFactory model) {
     GEMMEnd2EndBenchmark(state, model,
-      xnn_qs8_gemm_minmax_rndnu_ukernel_4x8c4__aarch32_neondot_cortex_a55,
-      xnn_qs8_igemm_minmax_rndnu_ukernel_4x8c4__aarch32_neondot_cortex_a55,
-      xnn_qs8_gemm_minmax_rndnu_ukernel_1x8c4__neondot,
-      xnn_qs8_igemm_minmax_rndnu_ukernel_1x8c4__neondot,
+      xnn_qs8_gemm_minmax_rndnu_ukernel_4x8__aarch32_neon_mlal_lane_cortex_a53,
+      xnn_qs8_igemm_minmax_rndnu_ukernel_4x8__aarch32_neon_mlal_lane_ld64,
+      xnn_qs8_gemm_minmax_rndnu_ukernel_1x8__neon_mlal_lane,
+      xnn_qs8_igemm_minmax_rndnu_ukernel_1x8__neon_mlal_lane,
       xnn_init_qs8_conv_minmax_rndnu_neon_params,
-      4 /* mr */, 8 /* nr */, 2 /* log2_kr */, 0 /* log2_sr */,
-      benchmark::utils::CheckNEONDOT);
+      4 /* mr */, 8 /* nr */, 0 /* log2_kr */, 0 /* log2_sr */,
+      benchmark::utils::CheckNEON);
+  }
+  static void qs8_gemm_4x8__aarch32_neon_mlal_lane_prfm_cortex_a53(benchmark::State& state, models::ExecutionPlanFactory model) {
+    GEMMEnd2EndBenchmark(state, model,
+      xnn_qs8_gemm_minmax_rndnu_ukernel_4x8__aarch32_neon_mlal_lane_prfm_cortex_a53,
+      xnn_qs8_igemm_minmax_rndnu_ukernel_4x8__aarch32_neon_mlal_lane_prfm_ld64,
+      xnn_qs8_gemm_minmax_rndnu_ukernel_1x8__neon_mlal_lane,
+      xnn_qs8_igemm_minmax_rndnu_ukernel_1x8__neon_mlal_lane,
+      xnn_init_qs8_conv_minmax_rndnu_neon_params,
+      4 /* mr */, 8 /* nr */, 0 /* log2_kr */, 0 /* log2_sr */,
+      benchmark::utils::CheckNEON);
   }
   static void qs8_gemm_4x8__aarch32_neon_mlal_lane_ld64(benchmark::State& state, models::ExecutionPlanFactory model) {
     GEMMEnd2EndBenchmark(state, model,
@@ -116,8 +136,10 @@ static void GEMMEnd2EndBenchmark(
       4 /* mr */, 8 /* nr */, 0 /* log2_kr */, 0 /* log2_sr */,
       benchmark::utils::CheckNEON);
   }
-  BENCHMARK_QS8_END2END(qs8_gemm_4x8c4__aarch32_neondot_ld64)
   BENCHMARK_QS8_END2END(qs8_gemm_4x8c4__aarch32_neondot_cortex_a55)
+  BENCHMARK_QS8_END2END(qs8_gemm_4x8c4__aarch32_neondot_ld64)
+  BENCHMARK_QS8_END2END(qs8_gemm_4x8__aarch32_neon_mlal_lane_prfm_cortex_a53)
+  BENCHMARK_QS8_END2END(qs8_gemm_4x8__aarch32_neon_mlal_lane_cortex_a53)
   BENCHMARK_QS8_END2END(qs8_gemm_4x8__aarch32_neon_mlal_lane_prfm_ld64)
   BENCHMARK_QS8_END2END(qs8_gemm_4x8__aarch32_neon_mlal_lane_ld64)
 #endif  // XNN_ARCH_ARM && XNN_ENABLE_ASSEMBLY
