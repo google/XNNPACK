802f650e05b4c4cc171ef4040fe0a2903680c99b
Frank Barchard <fbarchard@google.com>
2022-02-09 10:09:32 -0800

    Initialize Cortex A57 microkernels for AArch32
    - Use Cortex A53 microkernels with prefetch for Q8
    - Use Cortex A75 microkernels with prefetch for FP32
    
    PiperOrigin-RevId: 427502823

--- src/init.c
+++ src/init.c
@@ -149,6 +149,7 @@ static void init(void) {
               xnn_params.qc8.gemm.nr = 8;
               break;
             case cpuinfo_uarch_cortex_a53:
+            case cpuinfo_uarch_cortex_a57:
               xnn_params.qc8.gemm.minmax.gemm = xnn_init_hmp_gemm_ukernel((xnn_gemm_ukernel_function) xnn_qc8_gemm_minmax_fp32_ukernel_4x8__aarch32_neonv8_mlal_lane_prfm_cortex_a53);
               xnn_params.qc8.gemm.minmax.igemm = xnn_init_hmp_igemm_ukernel((xnn_igemm_ukernel_function) xnn_qc8_igemm_minmax_fp32_ukernel_4x8__aarch32_neonv8_mlal_lane_prfm_ld64);
               xnn_params.qc8.gemm.minmax.gemm1 = xnn_init_hmp_gemm_ukernel((xnn_gemm_ukernel_function) xnn_qc8_gemm_minmax_fp32_ukernel_1x8__neonv8_mlal_lane);
@@ -359,6 +360,7 @@ static void init(void) {
               xnn_params.qs8.gemm.nr = 8;
               break;
             case cpuinfo_uarch_cortex_a53:
+            case cpuinfo_uarch_cortex_a57:
               xnn_params.qs8.gemm.minmax.gemm = xnn_init_hmp_gemm_ukernel((xnn_gemm_ukernel_function) xnn_qs8_gemm_minmax_rndnu_ukernel_4x8__aarch32_neon_mlal_lane_prfm_cortex_a53);
               xnn_params.qs8.gemm.minmax.igemm = xnn_init_hmp_igemm_ukernel((xnn_igemm_ukernel_function) xnn_qs8_igemm_minmax_rndnu_ukernel_4x8__aarch32_neon_mlal_lane_prfm_ld64);
               xnn_params.qs8.gemm.minmax.gemm1 = xnn_init_hmp_gemm_ukernel((xnn_gemm_ukernel_function) xnn_qs8_gemm_minmax_rndnu_ukernel_1x8__neon_mlal_lane);
