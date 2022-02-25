4aaf90037c0255fa06ed807ffe071cd10733f37f
Zhi An Ng <zhin@google.com>
2022-02-25 09:13:36 -0800

    Addresses some comments from prior review on test/jit.cc
    PiperOrigin-RevId: 430961145

--- test/jit.cc
+++ test/jit.cc
@@ -21,7 +21,7 @@ TEST(JitMemory, AllocateAndReleaseJunkCode) {
   xnn_code_buffer b;
   ASSERT_EQ(xnn_status_success, xnn_allocate_code_memory(&b, XNN_DEFAULT_CODE_BUFFER_SIZE));
   std::string junk = "1234";
-  memcpy(b.code, junk.data(), junk.length());
+  std::memcpy(b.code, junk.data(), junk.length());
   b.size = junk.length();
 #if XNN_PLATFORM_JIT
   ASSERT_EQ(xnn_status_success, xnn_finalize_code_memory(&b));
@@ -42,20 +42,20 @@ TEST(JitMemory, GrowMemory) {
   xnn_code_buffer b;
   ASSERT_EQ(xnn_status_success, xnn_allocate_code_memory(&b, 8));
   std::string junk = "1234";
-  memcpy(b.code, junk.data(), junk.length());
+  std::memcpy(b.code, junk.data(), junk.length());
   b.size += junk.length();
   ASSERT_EQ(b.size, 4);
-  uintptr_t old_code = (uintptr_t) b.code;
+  const uintptr_t old_code = reinterpret_cast<uintptr_t>(b.code);
 
   // This should be a no-op, since we have enough space.
   ASSERT_EQ(xnn_status_success, xnn_grow_code_memory(&b, 4));
-  ASSERT_EQ(old_code, (uintptr_t) b.code);
+  ASSERT_EQ(old_code, reinterpret_cast<uintptr_t>(b.code));
 
   // Copy 4 more bytes, now we are full.
   memcpy(b.code, junk.data(), junk.length());
   b.size += junk.length();
 
-  size_t old_size = b.size;
+  const size_t old_size = b.size;
   ASSERT_EQ(xnn_status_success, xnn_grow_code_memory(&b, 4));
 
   // After growing, the new capacity should be bigger than the old one.
