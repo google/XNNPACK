31937ec7c0d04e86b2e0cc5b3d2946d0a40baa3c
Zhi An Ng <zhin@google.com>
2022-02-28 10:07:07 -0800

    Fix missing includes, breaks CMake builds
    PiperOrigin-RevId: 431463157

--- test/jit.cc
+++ test/jit.cc
@@ -3,6 +3,8 @@
 // This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree.
 
+#include <cstring>
+
 #include <xnnpack/allocator.h>
 #include <xnnpack/common.h>
 
