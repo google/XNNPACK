4c1530bd553e505528bcda799d602e39b35c3919
Frank Barchard <fbarchard@google.com>
2022-03-15 14:58:35 -0700

    Enable FP32 4x2 GEMM assembly microkernel for Cortex A75
    PiperOrigin-RevId: 434864702

--- src/init.c
+++ src/init.c
@@ -2816,7 +2816,7 @@ static void init(void) {
         }
       }
       #endif  // XNN_MAX_UARCH_TYPES > 1
-      xnn_params.f32.gemm2.minmax.gemm = xnn_init_hmp_gemm_ukernel((xnn_gemm_ukernel_function) xnn_f32_gemm_minmax_ukernel_4x2__aarch64_neonfma_ld64);
+      xnn_params.f32.gemm2.minmax.gemm = xnn_init_hmp_gemm_ukernel((xnn_gemm_ukernel_function) xnn_f32_gemm_minmax_ukernel_4x2__aarch64_neonfma_cortex_a75);
       xnn_params.f32.gemm2.minmax.igemm = xnn_init_hmp_igemm_ukernel((xnn_igemm_ukernel_function) xnn_f32_igemm_minmax_ukernel_4x2__aarch64_neonfma_ld64);
       xnn_params.f32.gemm2.init.f32 = xnn_init_f32_minmax_scalar_params;
       xnn_params.f32.gemm2.mr = 4;
@@ -2832,7 +2832,7 @@ static void init(void) {
         xnn_params.f32.gemm.mr = 6;
         xnn_params.f32.gemm.nr = 8;
 
-        xnn_params.f32.gemm2.minmax.gemm = xnn_init_hmp_gemm_ukernel((xnn_gemm_ukernel_function) xnn_f32_gemm_minmax_ukernel_4x2__aarch64_neonfma_ld64);
+        xnn_params.f32.gemm2.minmax.gemm = xnn_init_hmp_gemm_ukernel((xnn_gemm_ukernel_function) xnn_f32_gemm_minmax_ukernel_4x2__aarch64_neonfma_prfm_cortex_a75);
         xnn_params.f32.gemm2.minmax.igemm = xnn_init_hmp_igemm_ukernel((xnn_igemm_ukernel_function) xnn_f32_igemm_minmax_ukernel_4x2__aarch64_neonfma_ld64);
         xnn_params.f32.gemm2.init.f32 = xnn_init_f32_minmax_scalar_params;
         xnn_params.f32.gemm2.mr = 4;
