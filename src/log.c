b2631be7ac29f707474c15bd74d07ff3975c07bb
Marat Dukhan <maratek@google.com>
2022-06-05 15:25:21 -0700

    Re-implement logging primitives within XNNPACK
    Avoid dependency on clog for logging
    
    PiperOrigin-RevId: 453091946

---
+++ src/log.c
@@ -0,0 +1,145 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// All rights reserved.
+//
+// Copyright 2022 Google LLC
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+#include <assert.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#ifdef _WIN32
+  #include <windows.h>
+#else
+  #include <unistd.h>
+#endif
+
+#include <xnnpack/log.h>
+
+
+/* Messages up to this size are formatted entirely on-stack, and don't allocate heap memory */
+#define XNN_LOG_STACK_BUFFER_SIZE 1024
+
+#ifdef _WIN32
+  #define XNN_LOG_NEWLINE_LENGTH 2
+
+  #define XNN_LOG_STDERR STD_ERROR_HANDLE
+  #define XNN_LOG_STDOUT STD_OUTPUT_HANDLE
+#else
+  #define XNN_LOG_NEWLINE_LENGTH 1
+
+  #define XNN_LOG_STDERR STDERR_FILENO
+  #define XNN_LOG_STDOUT STDOUT_FILENO
+#endif
+
+static void xnn_vlog(int output_handle, const char* prefix, size_t prefix_length, const char* format, va_list args) {
+  char stack_buffer[XNN_LOG_STACK_BUFFER_SIZE];
+  char* heap_buffer = NULL;
+  char* out_buffer = &stack_buffer[0];
+
+  /* The first call to vsnprintf will clobber args, thus need a copy in case a second vsnprintf call is needed */
+  va_list args_copy;
+  va_copy(args_copy, args);
+
+  memcpy(stack_buffer, prefix, prefix_length * sizeof(char));
+  assert((prefix_length + XNN_LOG_NEWLINE_LENGTH) * sizeof(char) <= XNN_LOG_STACK_BUFFER_SIZE);
+
+  const int format_chars = vsnprintf(
+    &stack_buffer[prefix_length],
+    XNN_LOG_STACK_BUFFER_SIZE - (prefix_length + XNN_LOG_NEWLINE_LENGTH) * sizeof(char),
+    format,
+    args);
+  if (format_chars < 0) {
+    /* Format error in the message: silently ignore this particular message. */
+    goto cleanup;
+  }
+  const size_t format_length = (size_t) format_chars;
+  if ((prefix_length + format_length + XNN_LOG_NEWLINE_LENGTH) * sizeof(char) > XNN_LOG_STACK_BUFFER_SIZE) {
+    /* Allocate a buffer on heap, and vsnprintf to this buffer */
+    const size_t heap_buffer_size = (prefix_length + format_length + XNN_LOG_NEWLINE_LENGTH) * sizeof(char);
+    #if _WIN32
+      heap_buffer = HeapAlloc(GetProcessHeap(), 0, heap_buffer_size);
+    #else
+      heap_buffer = malloc(heap_buffer_size);
+    #endif
+    if (heap_buffer == NULL) {
+      goto cleanup;
+    }
+
+    /* Copy pre-formatted prefix into the on-heap buffer */
+    memcpy(heap_buffer, prefix, prefix_length * sizeof(char));
+    vsnprintf(&heap_buffer[prefix_length], (format_length + XNN_LOG_NEWLINE_LENGTH) * sizeof(char), format, args_copy);
+    out_buffer = heap_buffer;
+  }
+  #ifdef _WIN32
+    out_buffer[prefix_length + format_length] = '\r';
+    out_buffer[prefix_length + format_length + 1] = '\n';
+
+    DWORD bytes_written;
+    WriteFile(
+      GetStdHandle((DWORD) output_handle),
+      out_buffer, (prefix_length + format_length + XNN_LOG_NEWLINE_LENGTH) * sizeof(char),
+      &bytes_written, NULL);
+  #else
+    out_buffer[prefix_length + format_length] = '\n';
+
+    ssize_t bytes_written = write(output_handle, out_buffer, (prefix_length + format_length + XNN_LOG_NEWLINE_LENGTH) * sizeof(char));
+    (void) bytes_written;
+  #endif
+
+cleanup:
+  #ifdef _WIN32
+    HeapFree(GetProcessHeap(), 0, heap_buffer);
+  #else
+    free(heap_buffer);
+  #endif
+  va_end(args_copy);
+}
+
+#if XNN_LOG_LEVEL >= XNN_LOG_DEBUG
+  void xnn_vlog_debug(const char* format, va_list args) {
+    static const char debug_prefix[17] = {
+      'D', 'e', 'b', 'u', 'g', ' ', '(', 'X', 'N', 'N', 'P', 'A', 'C', 'K', ')', ':', ' '
+    };
+    xnn_vlog(XNN_LOG_STDOUT, debug_prefix, 17, format, args);
+  }
+#endif
+
+#if XNN_LOG_LEVEL >= XNN_LOG_INFO
+  void xnn_vlog_info(const char* format, va_list args) {
+    static const char info_prefix[16] = {
+      'N', 'o', 't', 'e', ' ', '(', 'X', 'N', 'N', 'P', 'A', 'C', 'K', ')', ':', ' '
+    };
+    xnn_vlog(XNN_LOG_STDOUT, info_prefix, 16, format, args);
+  }
+#endif
+
+#if XNN_LOG_LEVEL >= XNN_LOG_WARNING
+  void xnn_vlog_warning(const char* format, va_list args) {
+    static const char warning_prefix[20] = {
+      'W', 'a', 'r', 'n', 'i', 'n', 'g', ' ', 'i', 'n', ' ', 'X', 'N', 'N', 'P', 'A', 'C', 'K', ':', ' '
+    };
+    xnn_vlog(XNN_LOG_STDERR, warning_prefix, 20, format, args);
+  }
+#endif
+
+#if XNN_LOG_LEVEL >= XNN_LOG_ERROR
+  void xnn_vlog_error(const char* format, va_list args) {
+    static const char error_prefix[18] = {
+      'E', 'r', 'r', 'o', 'r', ' ', 'i', 'n', ' ', 'X', 'N', 'N', 'P', 'A', 'C', 'K', ':', ' '
+    };
+    xnn_vlog(XNN_LOG_STDERR, error_prefix, 18, format, args);
+  }
+#endif
+
+#if XNN_LOG_LEVEL >= XNN_LOG_FATAL
+  void xnn_vlog_fatal(const char* format, va_list args) {
+    static const char fatal_prefix[24] = {
+      'F', 'a', 't', 'a', 'l', ' ', 'e', 'r', 'r', 'o', 'r', ' ', 'i', 'n', ' ', 'X', 'N', 'N', 'P', 'A', 'C', 'K', ':', ' '
+    };
+    xnn_vlog(XNN_LOG_STDERR, fatal_prefix, 24, format, args);
+  }
+#endif
--- src/xnnpack/log.h
+++ src/xnnpack/log.h
@@ -9,8 +9,8 @@
 #pragma once
 
 #include <inttypes.h>
-
-#include <clog.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
 #include <xnnpack.h>
 #include <xnnpack/node-type.h>
@@ -21,17 +21,39 @@
   #error "Undefined XNN_LOG_LEVEL"
 #endif
 
-CLOG_DEFINE_LOG_DEBUG(xnn_log_debug, "XNNPACK", XNN_LOG_LEVEL);
-CLOG_DEFINE_LOG_INFO(xnn_log_info, "XNNPACK", XNN_LOG_LEVEL);
-CLOG_DEFINE_LOG_WARNING(xnn_log_warning, "XNNPACK", XNN_LOG_LEVEL);
-CLOG_DEFINE_LOG_ERROR(xnn_log_error, "XNNPACK", XNN_LOG_LEVEL);
-CLOG_DEFINE_LOG_FATAL(xnn_log_fatal, "XNNPACK", XNN_LOG_LEVEL);
+#define XNN_LOG_NONE 0
+#define XNN_LOG_FATAL 1
+#define XNN_LOG_ERROR 2
+#define XNN_LOG_WARNING 3
+#define XNN_LOG_INFO 4
+#define XNN_LOG_DEBUG 5
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#if XNN_LOG_LEVEL == 0
+#if XNN_LOG_LEVEL >= XNN_LOG_DEBUG
+  void xnn_vlog_debug(const char* format, va_list args);
+#endif
+
+#if XNN_LOG_LEVEL >= XNN_LOG_INFO
+  void xnn_vlog_info(const char* format, va_list args);
+#endif
+
+#if XNN_LOG_LEVEL >= XNN_LOG_WARNING
+  void xnn_vlog_warning(const char* format, va_list args);
+#endif
+
+#if XNN_LOG_LEVEL >= XNN_LOG_ERROR
+  void xnn_vlog_error(const char* format, va_list args);
+#endif
+
+#if XNN_LOG_LEVEL >= XNN_LOG_FATAL
+  void xnn_vlog_fatal(const char* format, va_list args);
+#endif
+
+#if XNN_LOG_LEVEL == XNN_LOG_NONE
   inline static const char* xnn_datatype_to_string(enum xnn_datatype type) {
     return "Unknown";
   }
@@ -49,3 +71,57 @@ const char* xnn_ukernel_type_to_string(enum xnn_ukernel_type type);
 #ifdef __cplusplus
 }  // extern "C"
 #endif
+
+#ifndef XNN_LOG_ARGUMENTS_FORMAT
+  #ifdef __GNUC__
+    #define XNN_LOG_ARGUMENTS_FORMAT __attribute__((__format__(__printf__, 1, 2)))
+  #else
+    #define XNN_LOG_ARGUMENTS_FORMAT
+  #endif
+#endif
+
+XNN_LOG_ARGUMENTS_FORMAT inline static void xnn_log_debug(const char* format, ...) {
+  #if XNN_LOG_LEVEL >= XNN_LOG_DEBUG
+    va_list args;
+    va_start(args, format);
+    xnn_vlog_debug(format, args);
+    va_end(args);
+  #endif
+}
+
+XNN_LOG_ARGUMENTS_FORMAT inline static void xnn_log_info(const char* format, ...) {
+  #if XNN_LOG_LEVEL >= XNN_LOG_INFO
+    va_list args;
+    va_start(args, format);
+    xnn_vlog_info(format, args);
+    va_end(args);
+  #endif
+}
+
+XNN_LOG_ARGUMENTS_FORMAT inline static void xnn_log_warning(const char* format, ...) {
+  #if XNN_LOG_LEVEL >= XNN_LOG_WARNING
+    va_list args;
+    va_start(args, format);
+    xnn_vlog_warning(format, args);
+    va_end(args);
+  #endif
+}
+
+XNN_LOG_ARGUMENTS_FORMAT inline static void xnn_log_error(const char* format, ...) {
+  #if XNN_LOG_LEVEL >= XNN_LOG_ERROR
+    va_list args;
+    va_start(args, format);
+    xnn_vlog_error(format, args);
+    va_end(args);
+  #endif
+}
+
+XNN_LOG_ARGUMENTS_FORMAT inline static void xnn_log_fatal(const char* format, ...) {
+  #if XNN_LOG_LEVEL >= XNN_LOG_FATAL
+    va_list args;
+    va_start(args, format);
+    xnn_vlog_fatal(format, args);
+    va_end(args);
+  #endif
+  abort();
+}
