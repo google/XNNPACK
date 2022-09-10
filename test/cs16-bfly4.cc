e2f795d9e2b96c244fdfac5b521fa6c0ff778f2e
Frank Barchard <fbarchard@google.com>
2022-09-10 10:35:06 -0700

    BFLY4 neon microkernel
    - first sample skips twiddle
    - processes a single sample per loop (x1)
    
    PiperOrigin-RevId: 473477322

--- test/cs16-bfly4.cc
+++ test/cs16-bfly4.cc
@@ -149,6 +149,74 @@
 #endif  // XNN_ARCH_ARM || XNN_ARCH_ARM64
 
 
+#if XNN_ARCH_ARM || XNN_ARCH_ARM64
+
+  TEST(CS16_BFLY4__NEON, samples_eq_1) {
+    TEST_REQUIRES_ARM_NEON;
+    BFly4MicrokernelTester()
+      .batch(1)
+      .samples(1)
+      .stride(64)
+      .Test(xnn_cs16_bfly4_ukernel__neon);
+  }
+
+  TEST(CS16_BFLY4__NEON, batch_eq_4) {
+    TEST_REQUIRES_ARM_NEON;
+    BFly4MicrokernelTester()
+      .batch(4)
+      .samples(1)
+      .stride(64)
+      .Test(xnn_cs16_bfly4_ukernel__neon);
+  }
+
+  TEST(CS16_BFLY4__NEON, batch_gt_1) {
+    TEST_REQUIRES_ARM_NEON;
+    for (size_t batch = 2; batch <= 16; batch++) {
+      BFly4MicrokernelTester()
+        .batch(batch)
+        .samples(1)
+        .stride(64)
+        .Test(xnn_cs16_bfly4_ukernel__neon);
+    }
+  }
+
+  TEST(CS16_BFLY4__NEON, samples_eq_4) {
+    TEST_REQUIRES_ARM_NEON;
+    BFly4MicrokernelTester()
+      .samples(4)
+      .stride(16)
+      .Test(xnn_cs16_bfly4_ukernel__neon);
+  }
+
+  TEST(CS16_BFLY4__NEON, samples_eq_4_batch_gt_1) {
+    TEST_REQUIRES_ARM_NEON;
+    for (size_t batch = 2; batch <= 4; batch++) {
+      BFly4MicrokernelTester()
+        .batch(batch)
+        .samples(4)
+        .stride(16)
+        .Test(xnn_cs16_bfly4_ukernel__neon);
+    }
+  }
+
+  TEST(CS16_BFLY4__NEON, samples_eq_16) {
+    TEST_REQUIRES_ARM_NEON;
+    BFly4MicrokernelTester()
+      .samples(16)
+      .stride(4)
+      .Test(xnn_cs16_bfly4_ukernel__neon);
+  }
+
+  TEST(CS16_BFLY4__NEON, samples_eq_64) {
+    TEST_REQUIRES_ARM_NEON;
+    BFly4MicrokernelTester()
+      .samples(64)
+      .stride(1)
+      .Test(xnn_cs16_bfly4_ukernel__neon);
+  }
+#endif  // XNN_ARCH_ARM || XNN_ARCH_ARM64
+
+
 
 TEST(CS16_BFLY4_SAMPLES1__SCALAR, samples_eq_1) {
   BFly4MicrokernelTester()
