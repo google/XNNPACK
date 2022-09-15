6c40223d447447214f575282f7dabb68e6c4cd42
Frank Barchard <fbarchard@google.com>
2022-09-15 15:39:25 -0700

    amalgamate-microkernels.py script use codecs class for file I/O.
    - Rerun template generator scripts to update source.
    
    PiperOrigin-RevId: 474674059

--- tools/amalgamate-microkernels.py
+++ tools/amalgamate-microkernels.py
@@ -103,14 +103,13 @@ def main(args):
   amalgam_text += "\n".join(amalgam_lines)
 
 
-
   txt_changed = True
   if os.path.exists(options.output):
-    with open(options.output, "r", encoding="utf-8") as amalgam_file:
+    with codecs.open(options.output, "r", encoding="utf-8") as amalgam_file:
       txt_changed = amalgam_file.read() != amalgam_text
 
   if txt_changed:
-    with open(options.output, "w", encoding="utf-8") as amalgam_file:
+    with codecs.open(options.output, "w", encoding="utf-8") as amalgam_file:
       amalgam_file.write(amalgam_text)
 
 
