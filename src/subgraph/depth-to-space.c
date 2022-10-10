480eaae5939cbed0212b33a27fb68da48a2ea5fe
Zhi An Ng <zhin@google.com>
2022-10-10 10:06:01 -0700

    Refactor subgraph checks for matching quantization params
    PiperOrigin-RevId: 480112000

--- src/subgraph/depth-to-space.c
+++ src/subgraph/depth-to-space.c
@@ -248,23 +248,10 @@ enum xnn_status xnn_define_depth_to_space(
   }
 
 #if !defined(XNN_NO_QU8_OPERATORS) || !defined(XNN_NO_QS8_OPERATORS)
-  if (compute_type == xnn_datatype_qint8 || compute_type == xnn_datatype_quint8) {
-    if (input_value->quantization.zero_point != output_value->quantization.zero_point) {
-      xnn_log_error(
-        "failed to define %s operator with input ID #%" PRIu32 " and output ID #%" PRIu32
-        ": mismatching zero point quantization parameter across input (%"PRId32") and output (%"PRId32")",
-        xnn_node_type_to_string(xnn_node_type_depth_to_space), input_id, output_id,
-        input_value->quantization.zero_point, output_value->quantization.zero_point);
-      return xnn_status_invalid_parameter;
-    }
-    if (input_value->quantization.scale != output_value->quantization.scale) {
-      xnn_log_error(
-        "failed to define %s operator with input ID #%" PRIu32 " and output ID #%" PRIu32
-        ": mismatching zero point quantization parameter across input (%.7g) and output (%.7g)",
-        xnn_node_type_to_string(xnn_node_type_depth_to_space), input_id, output_id,
-        input_value->quantization.scale, output_value->quantization.scale);
-      return xnn_status_invalid_parameter;
-    }
+  status = xnn_subgraph_check_quantization_parameter_matches(
+      xnn_node_type_depth_to_space, input_id, input_value, output_id, output_value);
+  if (status != xnn_status_success) {
+    return status;
   }
 #endif  // !defined(XNN_NO_QU8_OPERATORS) || !defined(XNN_NO_QS8_OPERATORS)
 
--- src/xnnpack/subgraph-validation.h
+++ src/xnnpack/subgraph-validation.h
@@ -49,6 +49,13 @@ enum xnn_status xnn_subgraph_check_datatype_matches_two_inputs(
   const struct xnn_value* output_value);
 enum xnn_status xnn_subgraph_check_output_min_max(enum xnn_node_type node_type, float output_min, float output_max);
 
+enum xnn_status xnn_subgraph_check_quantization_parameter_matches(
+  enum xnn_node_type node_type,
+  uint32_t input_id,
+  const struct xnn_value* input_value,
+  uint32_t output_id,
+  const struct xnn_value* output_value);
+
 #ifdef __cplusplus
 }  // extern "C"
 #endif
