161e8999753fcb2d8e1f6d79226dbf7d9e291e80
Frank Barchard <fbarchard@google.com>
2022-10-11 19:15:46 -0700

    F16 GAVGPOOL CW use single channel loop for AARch32
    - works around a code generation bug on AArch32
    - optimized single channel loop to use vectors
    
    PiperOrigin-RevId: 480508214

--- src/f16-gavgpool-cw/neonfp16arith-x8.c
+++ src/f16-gavgpool-cw/neonfp16arith-x8.c
@@ -25,16 +25,17 @@ void xnn_f16_gavgpool_cw_ukernel__neonfp16arith_x8(
 
   __fp16* o = (__fp16*) output;
   const __fp16* i0 = input;
-  const __fp16* i1 = (const __fp16*) ((uintptr_t) i0 + elements);
-  const __fp16* i2 = (const __fp16*) ((uintptr_t) i1 + elements);
-  const __fp16* i3 = (const __fp16*) ((uintptr_t) i2 + elements);
 
   const uint16x8_t vmask = vld1q_u16(params->neonfp16arith.mask);
   const float16x4_t vmultiplier = vreinterpret_f16_u16(vld1_dup_u16(&params->neonfp16arith.multiplier));
   const float16x4_t voutput_min = vreinterpret_f16_u16(vld1_dup_u16(&params->neonfp16arith.output_min));
   const float16x4_t voutput_max = vreinterpret_f16_u16(vld1_dup_u16(&params->neonfp16arith.output_max));
 
+#if XNN_ARCH_ARM64
   while (channels >= 4) {
+    const __fp16* i1 = (const __fp16*) ((uintptr_t) i0 + elements);
+    const __fp16* i2 = (const __fp16*) ((uintptr_t) i1 + elements);
+    const __fp16* i3 = (const __fp16*) ((uintptr_t) i2 + elements);
     float16x8_t vsum0 = vmovq_n_f16(0);
     float16x8_t vsum1 = vmovq_n_f16(0);
     float16x8_t vsum2 = vmovq_n_f16(0);
@@ -72,20 +73,10 @@ void xnn_f16_gavgpool_cw_ukernel__neonfp16arith_x8(
 
     // Having exactly 4 rows makes this work out nicely as we end up with
     // the 4 totals in 4 different lanes of the same vector.
-    #if XNN_ARCH_ARM64
-      const float16x8_t vsum01 = vpaddq_f16(vsum0, vsum1);
-      const float16x8_t vsum23 = vpaddq_f16(vsum2, vsum3);
-      const float16x8_t vsum0123 = vpaddq_f16(vsum01, vsum23);
-      const float16x4_t vsum = vpadd_f16(vget_low_f16(vsum0123), vget_high_f16(vsum0123));
-    #else
-      const float16x4_t vsum0_lo = vadd_f16(vget_low_f16(vsum0), vget_high_f16(vsum0));
-      const float16x4_t vsum1_lo = vadd_f16(vget_low_f16(vsum1), vget_high_f16(vsum1));
-      const float16x4_t vsum2_lo = vadd_f16(vget_low_f16(vsum2), vget_high_f16(vsum2));
-      const float16x4_t vsum3_lo = vadd_f16(vget_low_f16(vsum3), vget_high_f16(vsum3));
-      const float16x4_t vsum01_lo = vpadd_f16(vsum0_lo, vsum1_lo);
-      const float16x4_t vsum23_lo = vpadd_f16(vsum2_lo, vsum3_lo);
-      const float16x4_t vsum = vpadd_f16(vsum01_lo, vsum23_lo);
-    #endif
+    const float16x8_t vsum01 = vpaddq_f16(vsum0, vsum1);
+    const float16x8_t vsum23 = vpaddq_f16(vsum2, vsum3);
+    const float16x8_t vsum0123 = vpaddq_f16(vsum01, vsum23);
+    const float16x4_t vsum = vpadd_f16(vget_low_f16(vsum0123), vget_high_f16(vsum0123));
 
     float16x4_t vout = vmul_f16(vsum, vmultiplier);
 
@@ -95,20 +86,19 @@ void xnn_f16_gavgpool_cw_ukernel__neonfp16arith_x8(
     vst1_f16(o, vout); o += 4;
 
     i0 = i3;
-    i1 = (const __fp16*) ((uintptr_t) i0 + elements);
-    i2 = (const __fp16*) ((uintptr_t) i1 + elements);
-    i3 = (const __fp16*) ((uintptr_t) i2 + elements);
     channels -= 4;
   }
+#endif  // XNN_ARCH_ARM64
 
   while (channels != 0) {
     float16x8_t vsum0 = vmovq_n_f16(0);
     size_t n = elements;
-    while (n >= 8 * sizeof(__fp16)) {
-      const float16x8_t vi0 = vld1q_f16(i0); i0 += 8;
-
-      vsum0 = vaddq_f16(vsum0, vi0);
-      n -= 8 * sizeof(__fp16);
+    if (n >= 8 * sizeof(__fp16)) {
+      do {
+        const float16x8_t vi0 = vld1q_f16(i0); i0 += 8;
+        vsum0 = vaddq_f16(vsum0, vi0);
+        n -= 8 * sizeof(__fp16);
+      } while (n >= 8 * sizeof(__fp16));
     }
 
     if XNN_UNLIKELY(n != 0) {
@@ -119,11 +109,11 @@ void xnn_f16_gavgpool_cw_ukernel__neonfp16arith_x8(
       vsum0 = vaddq_f16(vsum0, vi0);
     }
 
-    float16x4_t vsum = vadd_f16(vget_low_f16(vsum0), vget_high_f16(vsum0));
-    vsum = vpadd_f16(vsum, vsum);
-    vsum = vpadd_f16(vsum, vsum);
+    const float16x4_t vout4 = vpadd_f16(vget_low_f16(vsum0), vget_high_f16(vsum0));
+    const float16x4_t vout2 = vpadd_f16(vout4, vout4);
+    const float16x4_t vout1 = vpadd_f16(vout2, vout2);
 
-    float16x4_t vout = vmul_f16(vsum, vmultiplier);
+    float16x4_t vout = vmul_f16(vout1, vmultiplier);
 
     vout = vmax_f16(vout, voutput_min);
     vout = vmin_f16(vout, voutput_max);
