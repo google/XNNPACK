93a62407c3c4a413ae129b111ff699cc114292b3
Frank Barchard <fbarchard@google.com>
2022-10-13 11:56:33 -0700

    AArch32 F16 GAVGPOOL_CW enable 4 rows with clang bug work around
    - vmultiply param was loaded incorrectly in clang 16.
    - change params[restrict XNN_MIN_ELEMENTS(1)]) XNN_OOB_READS to *params
    
    PiperOrigin-RevId: 480949814

--- src/f16-gavgpool-cw/neonfp16arith-x8.c
+++ src/f16-gavgpool-cw/neonfp16arith-x8.c
@@ -17,7 +17,7 @@ void xnn_f16_gavgpool_cw_ukernel__neonfp16arith_x8(
     size_t channels,
     const void* input,
     void* output,
-    const union xnn_f16_gavgpool_params params[restrict XNN_MIN_ELEMENTS(1)]) XNN_OOB_READS
+    const union xnn_f16_gavgpool_params* params) XNN_OOB_READS
 {
   assert(elements != 0);
   assert(elements % sizeof(__fp16) == 0);
@@ -31,7 +31,6 @@ void xnn_f16_gavgpool_cw_ukernel__neonfp16arith_x8(
   const float16x4_t voutput_min = vreinterpret_f16_u16(vld1_dup_u16(&params->neonfp16arith.output_min));
   const float16x4_t voutput_max = vreinterpret_f16_u16(vld1_dup_u16(&params->neonfp16arith.output_max));
 
-#if XNN_ARCH_ARM64
   while (channels >= 4) {
     const __fp16* i1 = (const __fp16*) ((uintptr_t) i0 + elements);
     const __fp16* i2 = (const __fp16*) ((uintptr_t) i1 + elements);
@@ -73,13 +72,22 @@ void xnn_f16_gavgpool_cw_ukernel__neonfp16arith_x8(
 
     // Having exactly 4 rows makes this work out nicely as we end up with
     // the 4 totals in 4 different lanes of the same vector.
-    const float16x8_t vsum01 = vpaddq_f16(vsum0, vsum1);
-    const float16x8_t vsum23 = vpaddq_f16(vsum2, vsum3);
-    const float16x8_t vsum0123 = vpaddq_f16(vsum01, vsum23);
-    const float16x4_t vsum = vpadd_f16(vget_low_f16(vsum0123), vget_high_f16(vsum0123));
+#if XNN_ARCH_ARM64
+    const float16x8_t vout01 = vpaddq_f16(vsum0, vsum1);
+    const float16x8_t vout23 = vpaddq_f16(vsum2, vsum3);
+    const float16x8_t vout0123 = vpaddq_f16(vout01, vout23);
+    float16x4_t vout = vget_low_f16(vpaddq_f16(vout0123, vout0123));
+#else  // XNN_ARCH_ARM
+    const float16x4_t vout0 = vadd_f16(vget_low_f16(vsum0), vget_high_f16(vsum0));
+    const float16x4_t vout1 = vadd_f16(vget_low_f16(vsum1), vget_high_f16(vsum1));
+    const float16x4_t vout2 = vadd_f16(vget_low_f16(vsum2), vget_high_f16(vsum2));
+    const float16x4_t vout3 = vadd_f16(vget_low_f16(vsum3), vget_high_f16(vsum3));
+    const float16x4_t vout01 = vpadd_f16(vout0, vout1);
+    const float16x4_t vout23 = vpadd_f16(vout2, vout3);
+    float16x4_t vout = vpadd_f16(vout01, vout23);
+#endif
 
-    float16x4_t vout = vmul_f16(vsum, vmultiplier);
-
+    vout = vmul_f16(vout, vmultiplier);
     vout = vmax_f16(vout, voutput_min);
     vout = vmin_f16(vout, voutput_max);
 
@@ -88,7 +96,6 @@ void xnn_f16_gavgpool_cw_ukernel__neonfp16arith_x8(
     i0 = i3;
     channels -= 4;
   }
-#endif  // XNN_ARCH_ARM64
 
   while (channels != 0) {
     float16x8_t vsum0 = vmovq_n_f16(0);
