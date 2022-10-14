5794c7fff938940e0e30ad6f1ce3b883378a5e41
Frank Barchard <fbarchard@google.com>
2022-10-14 00:09:21 -0700

    f16 GAVGPOOL-CW add 4 elements horizontally instead of 4 rows
    PiperOrigin-RevId: 481073670

--- src/f16-gavgpool-cw/neonfp16arith-x8.c
+++ src/f16-gavgpool-cw/neonfp16arith-x8.c
@@ -7,7 +7,6 @@
 
 #include <arm_neon.h>
 
-#include <xnnpack/common.h>
 #include <xnnpack/gavgpool.h>
 #include <xnnpack/math.h>
 
@@ -31,81 +30,31 @@ void xnn_f16_gavgpool_cw_ukernel__neonfp16arith_x8(
   const float16x4_t voutput_min = vreinterpret_f16_u16(vld1_dup_u16(&params->neonfp16arith.output_min));
   const float16x4_t voutput_max = vreinterpret_f16_u16(vld1_dup_u16(&params->neonfp16arith.output_max));
 
-  while (channels >= 4) {
-    const __fp16* i1 = (const __fp16*) ((uintptr_t) i0 + elements);
-    const __fp16* i2 = (const __fp16*) ((uintptr_t) i1 + elements);
-    const __fp16* i3 = (const __fp16*) ((uintptr_t) i2 + elements);
-    float16x8_t vsum0 = vmovq_n_f16(0);
-    float16x8_t vsum1 = vmovq_n_f16(0);
-    float16x8_t vsum2 = vmovq_n_f16(0);
-    float16x8_t vsum3 = vmovq_n_f16(0);
-    size_t n = elements;
-    while (n >= 8 * sizeof(__fp16)) {
-      const float16x8_t vi0 = vld1q_f16(i0); i0 += 8;
-      const float16x8_t vi1 = vld1q_f16(i1); i1 += 8;
-      const float16x8_t vi2 = vld1q_f16(i2); i2 += 8;
-      const float16x8_t vi3 = vld1q_f16(i3); i3 += 8;
-
-      vsum0 = vaddq_f16(vsum0, vi0);
-      vsum1 = vaddq_f16(vsum1, vi1);
-      vsum2 = vaddq_f16(vsum2, vi2);
-      vsum3 = vaddq_f16(vsum3, vi3);
-      n -= 8 * sizeof(__fp16);
-    }
-
-    if XNN_UNLIKELY(n != 0) {
-      float16x8_t vi0 = vld1q_f16(i0); i0 = (const __fp16*) ((uintptr_t) i0 + n);
-      float16x8_t vi1 = vld1q_f16(i1); i1 = (const __fp16*) ((uintptr_t) i1 + n);
-      float16x8_t vi2 = vld1q_f16(i2); i2 = (const __fp16*) ((uintptr_t) i2 + n);
-      float16x8_t vi3 = vld1q_f16(i3); i3 = (const __fp16*) ((uintptr_t) i3 + n);
-
-      vi0 = vreinterpretq_f16_u16(vandq_u16(vmask, vreinterpretq_u16_f16(vi0)));
-      vi1 = vreinterpretq_f16_u16(vandq_u16(vmask, vreinterpretq_u16_f16(vi1)));
-      vi2 = vreinterpretq_f16_u16(vandq_u16(vmask, vreinterpretq_u16_f16(vi2)));
-      vi3 = vreinterpretq_f16_u16(vandq_u16(vmask, vreinterpretq_u16_f16(vi3)));
-
-      vsum0 = vaddq_f16(vsum0, vi0);
-      vsum1 = vaddq_f16(vsum1, vi1);
-      vsum2 = vaddq_f16(vsum2, vi2);
-      vsum3 = vaddq_f16(vsum3, vi3);
-    }
-
-    // Having exactly 4 rows makes this work out nicely as we end up with
-    // the 4 totals in 4 different lanes of the same vector.
-#if XNN_ARCH_ARM64
-    const float16x8_t vout01 = vpaddq_f16(vsum0, vsum1);
-    const float16x8_t vout23 = vpaddq_f16(vsum2, vsum3);
-    const float16x8_t vout0123 = vpaddq_f16(vout01, vout23);
-    float16x4_t vout = vget_low_f16(vpaddq_f16(vout0123, vout0123));
-#else  // XNN_ARCH_ARM
-    const float16x4_t vout0 = vadd_f16(vget_low_f16(vsum0), vget_high_f16(vsum0));
-    const float16x4_t vout1 = vadd_f16(vget_low_f16(vsum1), vget_high_f16(vsum1));
-    const float16x4_t vout2 = vadd_f16(vget_low_f16(vsum2), vget_high_f16(vsum2));
-    const float16x4_t vout3 = vadd_f16(vget_low_f16(vsum3), vget_high_f16(vsum3));
-    const float16x4_t vout01 = vpadd_f16(vout0, vout1);
-    const float16x4_t vout23 = vpadd_f16(vout2, vout3);
-    float16x4_t vout = vpadd_f16(vout01, vout23);
-#endif
-
-    vout = vmul_f16(vout, vmultiplier);
-    vout = vmax_f16(vout, voutput_min);
-    vout = vmin_f16(vout, voutput_max);
-
-    vst1_f16(o, vout); o += 4;
-
-    i0 = i3;
-    channels -= 4;
-  }
-
-  while (channels != 0) {
+  do {
     float16x8_t vsum0 = vmovq_n_f16(0);
     size_t n = elements;
-    if (n >= 8 * sizeof(__fp16)) {
+    if (n >= 32 * sizeof(__fp16)) {
+      float16x8_t vsum1 = vmovq_n_f16(0);
       do {
-        const float16x8_t vi0 = vld1q_f16(i0); i0 += 8;
-        vsum0 = vaddq_f16(vsum0, vi0);
-        n -= 8 * sizeof(__fp16);
-      } while (n >= 8 * sizeof(__fp16));
+        const float16x8_t vi0 = vld1q_f16(i0);
+        const float16x8_t vi1 = vld1q_f16(i0 + 8);
+        const float16x8_t vi2 = vld1q_f16(i0 + 16);
+        const float16x8_t vi3 = vld1q_f16(i0 + 24);
+        i0 += 32;
+        const float16x8_t acc0 = vaddq_f16(vi0, vi1);
+        const float16x8_t acc1 = vaddq_f16(vi2, vi3);
+        vsum0 = vaddq_f16(vsum0, acc0);
+        vsum1 = vaddq_f16(vsum1, acc1);
+        n -= 32 * sizeof(__fp16);
+      } while (n >= 32 * sizeof(__fp16));
+      vsum0 = vaddq_f16(vsum0, vsum1);
+    }
+
+    while (n >= 8 * sizeof(__fp16)) {
+      const float16x8_t vi0 = vld1q_f16(i0);
+      i0 += 8;
+      vsum0 = vaddq_f16(vsum0, vi0);
+      n -= 8 * sizeof(__fp16);
     }
 
     if XNN_UNLIKELY(n != 0) {
@@ -126,6 +75,5 @@ void xnn_f16_gavgpool_cw_ukernel__neonfp16arith_x8(
     vout = vmin_f16(vout, voutput_max);
 
     vst1_lane_f16(o, vout, 0); o += 1;
-    channels -= 1;
-  }
+  } while (--channels != 0);
 }
