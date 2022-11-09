76d8f142222c9bf24fde5ffbdd9f9b399be4ac99
Marat Dukhan <maratek@google.com>
2022-11-09 09:47:03 -0800

    Use globally unique filenames for FFT, ZIP, PAKCX, UNPOOL, Requantization microkernels
    PiperOrigin-RevId: 487271604

--- scripts/generate-cs16-fftr.sh
+++ scripts/generate-cs16-fftr.sh
@@ -5,9 +5,9 @@
 # LICENSE file in the root directory of this source tree.
 
 ################################### SCALAR ###################################
-tools/xngen src/cs16-fftr/scalar.c.in -D SAMPLE_TILE=1 -o src/cs16-fftr/gen/scalar-x1.c &
-tools/xngen src/cs16-fftr/scalar.c.in -D SAMPLE_TILE=2 -o src/cs16-fftr/gen/scalar-x2.c &
-tools/xngen src/cs16-fftr/scalar.c.in -D SAMPLE_TILE=4 -o src/cs16-fftr/gen/scalar-x4.c &
+tools/xngen src/cs16-fftr/scalar.c.in -D SAMPLE_TILE=1 -o src/cs16-fftr/gen/cs16-fftr-scalar-x1.c &
+tools/xngen src/cs16-fftr/scalar.c.in -D SAMPLE_TILE=2 -o src/cs16-fftr/gen/cs16-fftr-scalar-x2.c &
+tools/xngen src/cs16-fftr/scalar.c.in -D SAMPLE_TILE=4 -o src/cs16-fftr/gen/cs16-fftr-scalar-x4.c &
 
 ################################## Unit tests #################################
 tools/generate-fftr-test.py --spec test/cs16-fftr.yaml --output test/cs16-fftr.cc &
