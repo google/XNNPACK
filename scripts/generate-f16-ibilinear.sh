679b93439f8a7301b3e09b92a6133e27569a7534
Marat Dukhan <maratek@google.com>
2022-11-09 14:08:21 -0800

    Use globally unique filenames for IBILINEAR microkernels
    PiperOrigin-RevId: 487342459

--- scripts/generate-f16-ibilinear.sh
+++ scripts/generate-f16-ibilinear.sh
@@ -5,12 +5,12 @@
 # LICENSE file in the root directory of this source tree.
 
 ################################### ARM NEON ##################################
-tools/xngen src/f16-ibilinear/neonfp16arith.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -o src/f16-ibilinear/gen/neonfp16arith-c8.c &
-tools/xngen src/f16-ibilinear/neonfp16arith.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -o src/f16-ibilinear/gen/neonfp16arith-c16.c &
+tools/xngen src/f16-ibilinear/neonfp16arith.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -o src/f16-ibilinear/gen/f16-ibilinear-neonfp16arith-c8.c &
+tools/xngen src/f16-ibilinear/neonfp16arith.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -o src/f16-ibilinear/gen/f16-ibilinear-neonfp16arith-c16.c &
 
 ################################### x86 FMA3 ###################################
-tools/xngen src/f16-ibilinear/fma3.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -o src/f16-ibilinear/gen/fma3-c8.c &
-tools/xngen src/f16-ibilinear/fma3.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -o src/f16-ibilinear/gen/fma3-c16.c &
+tools/xngen src/f16-ibilinear/fma3.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -o src/f16-ibilinear/gen/f16-ibilinear-fma3-c8.c &
+tools/xngen src/f16-ibilinear/fma3.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -o src/f16-ibilinear/gen/f16-ibilinear-fma3-c16.c &
 
 ################################## Unit tests #################################
 tools/generate-ibilinear-test.py --spec test/f16-ibilinear.yaml --output test/f16-ibilinear.cc &
--- scripts/generate-s8-ibilinear.sh
+++ scripts/generate-s8-ibilinear.sh
@@ -5,46 +5,46 @@
 # LICENSE file in the root directory of this source tree.
 
 #################################### Scalar ###################################
-tools/xngen src/s8-ibilinear/scalar.c.in -D CHANNEL_TILE=1 -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/scalar-c1.c &
-tools/xngen src/s8-ibilinear/scalar.c.in -D CHANNEL_TILE=2 -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/scalar-c2.c &
-tools/xngen src/s8-ibilinear/scalar.c.in -D CHANNEL_TILE=4 -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/scalar-c4.c &
+tools/xngen src/s8-ibilinear/scalar.c.in -D CHANNEL_TILE=1 -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/s8-ibilinear-scalar-c1.c &
+tools/xngen src/s8-ibilinear/scalar.c.in -D CHANNEL_TILE=2 -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/s8-ibilinear-scalar-c2.c &
+tools/xngen src/s8-ibilinear/scalar.c.in -D CHANNEL_TILE=4 -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/s8-ibilinear-scalar-c4.c &
 
-tools/xngen src/s8-ibilinear/scalar.c.in -D CHANNEL_TILE=1 -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/scalar-c1.c &
-tools/xngen src/s8-ibilinear/scalar.c.in -D CHANNEL_TILE=2 -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/scalar-c2.c &
-tools/xngen src/s8-ibilinear/scalar.c.in -D CHANNEL_TILE=4 -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/scalar-c4.c &
+tools/xngen src/s8-ibilinear/scalar.c.in -D CHANNEL_TILE=1 -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/u8-ibilinear-scalar-c1.c &
+tools/xngen src/s8-ibilinear/scalar.c.in -D CHANNEL_TILE=2 -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/u8-ibilinear-scalar-c2.c &
+tools/xngen src/s8-ibilinear/scalar.c.in -D CHANNEL_TILE=4 -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/u8-ibilinear-scalar-c4.c &
 
 ################################## WAsm SIMD ##################################
-tools/xngen src/s8-ibilinear/wasmsimd-dot16x2.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/wasmsimd-dot16x2-c8.c &
-tools/xngen src/s8-ibilinear/wasmsimd-dot16x2.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/wasmsimd-dot16x2-c16.c &
+tools/xngen src/s8-ibilinear/wasmsimd-dot16x2.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/s8-ibilinear-wasmsimd-dot16x2-c8.c &
+tools/xngen src/s8-ibilinear/wasmsimd-dot16x2.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/s8-ibilinear-wasmsimd-dot16x2-c16.c &
 
-tools/xngen src/s8-ibilinear/wasmsimd-mul32.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/wasmsimd-mul32-c8.c &
-tools/xngen src/s8-ibilinear/wasmsimd-mul32.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/wasmsimd-mul32-c16.c &
+tools/xngen src/s8-ibilinear/wasmsimd-mul32.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/s8-ibilinear-wasmsimd-mul32-c8.c &
+tools/xngen src/s8-ibilinear/wasmsimd-mul32.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/s8-ibilinear-wasmsimd-mul32-c16.c &
 
-tools/xngen src/s8-ibilinear/wasmsimd-dot16x2.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/wasmsimd-dot16x2-c8.c &
-tools/xngen src/s8-ibilinear/wasmsimd-dot16x2.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/wasmsimd-dot16x2-c16.c &
+tools/xngen src/s8-ibilinear/wasmsimd-dot16x2.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/u8-ibilinear-wasmsimd-dot16x2-c8.c &
+tools/xngen src/s8-ibilinear/wasmsimd-dot16x2.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/u8-ibilinear-wasmsimd-dot16x2-c16.c &
 
-tools/xngen src/s8-ibilinear/wasmsimd-mul32.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/wasmsimd-mul32-c8.c &
-tools/xngen src/s8-ibilinear/wasmsimd-mul32.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/wasmsimd-mul32-c16.c &
+tools/xngen src/s8-ibilinear/wasmsimd-mul32.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/u8-ibilinear-wasmsimd-mul32-c8.c &
+tools/xngen src/s8-ibilinear/wasmsimd-mul32.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/u8-ibilinear-wasmsimd-mul32-c16.c &
 
 ################################### ARM NEON ##################################
-tools/xngen src/s8-ibilinear/neon.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/neon-c8.c &
-tools/xngen src/s8-ibilinear/neon.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/neon-c16.c &
+tools/xngen src/s8-ibilinear/neon.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/s8-ibilinear-neon-c8.c &
+tools/xngen src/s8-ibilinear/neon.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=S8 -o src/s8-ibilinear/gen/s8-ibilinear-neon-c16.c &
 
-tools/xngen src/s8-ibilinear/neon.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/neon-c8.c &
-tools/xngen src/s8-ibilinear/neon.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/neon-c16.c &
+tools/xngen src/s8-ibilinear/neon.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/u8-ibilinear-neon-c8.c &
+tools/xngen src/s8-ibilinear/neon.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=U8 -o src/u8-ibilinear/gen/u8-ibilinear-neon-c16.c &
 
 ################################### x86 SSE ###################################
-tools/xngen src/s8-ibilinear/sse.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=S8 -D SSE=2 -D AVX=0 -D XOP=0 -o src/s8-ibilinear/gen/sse2-c8.c &
-tools/xngen src/s8-ibilinear/sse.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=S8 -D SSE=2 -D AVX=0 -D XOP=0 -o src/s8-ibilinear/gen/sse2-c16.c &
+tools/xngen src/s8-ibilinear/sse.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=S8 -D SSE=2 -D AVX=0 -D XOP=0 -o src/s8-ibilinear/gen/s8-ibilinear-sse2-c8.c &
+tools/xngen src/s8-ibilinear/sse.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=S8 -D SSE=2 -D AVX=0 -D XOP=0 -o src/s8-ibilinear/gen/s8-ibilinear-sse2-c16.c &
 
-tools/xngen src/s8-ibilinear/sse.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=S8 -D SSE=4 -D AVX=0 -D XOP=0 -o src/s8-ibilinear/gen/sse41-c8.c &
-tools/xngen src/s8-ibilinear/sse.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=S8 -D SSE=4 -D AVX=0 -D XOP=0 -o src/s8-ibilinear/gen/sse41-c16.c &
+tools/xngen src/s8-ibilinear/sse.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=S8 -D SSE=4 -D AVX=0 -D XOP=0 -o src/s8-ibilinear/gen/s8-ibilinear-sse41-c8.c &
+tools/xngen src/s8-ibilinear/sse.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=S8 -D SSE=4 -D AVX=0 -D XOP=0 -o src/s8-ibilinear/gen/s8-ibilinear-sse41-c16.c &
 
-tools/xngen src/s8-ibilinear/sse.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=U8 -D SSE=2 -D AVX=0 -D XOP=0 -o src/u8-ibilinear/gen/sse2-c8.c &
-tools/xngen src/s8-ibilinear/sse.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=U8 -D SSE=2 -D AVX=0 -D XOP=0 -o src/u8-ibilinear/gen/sse2-c16.c &
+tools/xngen src/s8-ibilinear/sse.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=U8 -D SSE=2 -D AVX=0 -D XOP=0 -o src/u8-ibilinear/gen/u8-ibilinear-sse2-c8.c &
+tools/xngen src/s8-ibilinear/sse.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=U8 -D SSE=2 -D AVX=0 -D XOP=0 -o src/u8-ibilinear/gen/u8-ibilinear-sse2-c16.c &
 
-tools/xngen src/s8-ibilinear/sse.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=U8 -D SSE=4 -D AVX=0 -D XOP=0 -o src/u8-ibilinear/gen/sse41-c8.c &
-tools/xngen src/s8-ibilinear/sse.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=U8 -D SSE=4 -D AVX=0 -D XOP=0 -o src/u8-ibilinear/gen/sse41-c16.c &
+tools/xngen src/s8-ibilinear/sse.c.in -D CHANNEL_TILE=8  -D PIXEL_TILE=1 -D DATATYPE=U8 -D SSE=4 -D AVX=0 -D XOP=0 -o src/u8-ibilinear/gen/u8-ibilinear-sse41-c8.c &
+tools/xngen src/s8-ibilinear/sse.c.in -D CHANNEL_TILE=16 -D PIXEL_TILE=1 -D DATATYPE=U8 -D SSE=4 -D AVX=0 -D XOP=0 -o src/u8-ibilinear/gen/u8-ibilinear-sse41-c16.c &
 
 ################################## Unit tests #################################
 tools/generate-ibilinear-test.py --spec test/s8-ibilinear.yaml --output test/s8-ibilinear.cc &
