68a1bf956e8b7a619856d4524c2e1a812d1cb001
Zhi An Ng <zhin@google.com>
2022-11-14 15:23:37 -0800

    Convert F32 IGEMM AArch32 LD64 JIT microkernel to support max_mr
    PiperOrigin-RevId: 488483975

--- src/f32-igemm/f32-igemm-4x8-aarch32-neon-ld64.cc
+++ src/f32-igemm/f32-igemm-4x8-aarch32-neon-ld64.cc
@@ -3,22 +3,25 @@
 // This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree.
 
-
 #include <cassert>
+#include <cstddef>
+#include <limits>
 
 #include <xnnpack.h>
 #include <xnnpack/aarch32-assembler.h>
 #include <xnnpack/igemm.h>
 #include <xnnpack/memory.h>
-
+#include <xnnpack/microparams.h>
+#include <xnnpack/post-operation.h>
 
 namespace xnnpack {
 namespace aarch32 {
 namespace {
 class Generator : public MacroAssembler {
   using MacroAssembler::MacroAssembler;
+
  public:
-  void generate(size_t max_mr, size_t nc_mod_nr, size_t kc, const void* params);
+  void generate(size_t max_mr, size_t nc_mod_nr, size_t kc, size_t ks, const jit_gemm_params* jit_gemm_params);
 };
 
 
@@ -51,15 +54,23 @@ class Generator : public MacroAssembler {
 // C3   r6 d28-d29 q14  d30-d31 q15
 // clamp  (r5) d4 d5 d6 d7
 
-// Converted from: src/f32-igemm/gen/4x8-minmax-aarch32-neon-ld64.S
-void Generator::generate(size_t max_mr, size_t nc_mod_nr, size_t kc, const void* params)
+// Converted from: src/f32-igemm/gen/f32-igemm-4x8-minmax-aarch32-neon-ld64.S
+void Generator::generate(size_t max_mr, size_t nc_mod_nr, size_t kc, size_t ks, const jit_gemm_params* jit_gemm_params)
 {
+  assert(max_mr <= 4);
   assert(nc_mod_nr < 8);
   assert(kc != 0);
   assert(kc % sizeof(float) == 0);
+  assert(ks != 0);
 
   Label l0, l1, l2, l3, l4, l5, l6, l7, l8;
-
+  const size_t num_post_operations = jit_gemm_params->num_post_operations;
+  (void) num_post_operations;  // Silence unused warning.
+  const float min = jit_gemm_params->f32_minmax.min;
+  const float max = jit_gemm_params->f32_minmax.max;
+  const bool clamp_min = min != -std::numeric_limits<float>::infinity();
+  const bool clamp_max = max != +std::numeric_limits<float>::infinity();
+  assert(num_post_operations == 0 || (!clamp_min && !clamp_max));
   // Push 112 bytes
   // r2 will be reloaded in outer loop.  r3 is ks
   push({r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, lr}); // +44
@@ -74,36 +85,56 @@ void Generator::generate(size_t max_mr, size_t nc_mod_nr, size_t kc, const void*
   mov(r14, r3); // p = ks
 
   // Clamp C pointers
-  cmp(r0, 2); // if mr >= 2
-  add(r4, r11, r6); //   c1 = c0 + cm_stride
-  movlo(r4, r11); // c1
-  // if mr > 2
-  add(r8, r4, r6); //   c2 = c1 + cm_stride
-  movls(r8, r4); // c2
-  cmp(r0, 4); // if mr >=4
-  add(r6, r8, r6); //   c3 = c2 + cm_stride
-  movlo(r6, r8); // c3
+  if (max_mr > 1) {
+    cmp(r0, 2); // if mr >= 2
+    add(r4, r11, r6); //   c1 = c0 + cm_stride
+    movlo(r4, r11); // c1
+  }
+  if (max_mr > 2) {
+    // if mr > 2
+    add(r8, r4, r6); //   c2 = c1 + cm_stride
+    movls(r8, r4); // c2
+  }
+  if (max_mr > 3) {
+    cmp(r0, 4); // if mr >=4
+    add(r6, r8, r6); //   c3 = c2 + cm_stride
+    movlo(r6, r8); // c3
+  }
 
   // Load min/max values
-  vld1r_32({d4, d5}, mem[r5]++);
-  vld1r_32({d6, d7}, mem[r5]);
+  if (clamp_min || clamp_max) {
+    vld1r_32({d4, d5}, mem[r5]++);
+    vld1r_32({d6, d7}, mem[r5]);
+  }
 
   bind(l0);
   // Load initial bias from w into accumulators
   vldm(mem[r9]++, {d16-d19}); // Bias
-  vmov(q10, q8);
-  vmov(q11, q9);
-  vmov(q12, q8);
-  vmov(q13, q9);
-  vmov(q14, q8);
-  vmov(q15, q9);
+  if (max_mr > 1) {
+    vmov(q10, q8);
+    vmov(q11, q9);
+  }
+  if (max_mr > 2) {
+    vmov(q12, q8);
+    vmov(q13, q9);
+  }
+  if (max_mr > 3) {
+    vmov(q14, q8);
+    vmov(q15, q9);
+  }
 
   bind(l1);
   // Load next 4 A pointers
   ldr(r3, mem[r2, 0]);
-  ldr(r12, mem[r2, 4]);
-  ldr(r10, mem[r2, 8]);
-  ldr(r0, mem[r2, 12]);
+  if (max_mr > 1) {
+    ldr(r12, mem[r2, 4]);
+  }
+  if (max_mr > 2) {
+    ldr(r10, mem[r2, 8]);
+  }
+  if (max_mr > 3) {
+    ldr(r0, mem[r2, 12]);
+  }
   add(r2, r2, 16);
 
   // Add a_offset
@@ -112,16 +143,24 @@ void Generator::generate(size_t max_mr, size_t nc_mod_nr, size_t kc, const void*
   cmp(r3, r7); // if a0 == zero
   add(r3, r3, r5); // a0 += a_offset
   moveq(r3, r7); //   a0 = zero, else += a0 + a_offset
-  cmp(r12, r7); // if a1 == zero
-  add(r12, r12, r5); // a1 += a_offset
-  moveq(r12, r7); //   a1 = zero, else += a1 + a_offset
-  cmp(r10, r7); // if a2 == zero
-  add(r10, r10, r5); // a2 += a_offset
-  moveq(r10, r7); //   a2 = zero, else += a2 + a_offset
-  cmp(r0, r7); // if a3 == zero
-  add(r0, r0, r5); // a3 += a_offset
+  if (max_mr > 1) {
+    cmp(r12, r7); // if a1 == zero
+    add(r12, r12, r5); // a1 += a_offset
+    moveq(r12, r7); //   a1 = zero, else += a1 + a_offset
+  }
+  if (max_mr > 2) {
+    cmp(r10, r7); // if a2 == zero
+    add(r10, r10, r5); // a2 += a_offset
+    moveq(r10, r7); //   a2 = zero, else += a2 + a_offset
+  }
+  if (max_mr > 3) {
+    cmp(r0, r7); // if a3 == zero
+    add(r0, r0, r5); // a3 += a_offset
+  }
   ldr(r5, mem[sp, 68]); // kc
-  moveq(r0, r7); //   a3 = zero, else += a3 + a_offset
+  if (max_mr > 3) {
+    moveq(r0, r7); //   a3 = zero, else += a3 + a_offset
+  }
 
 
   subs(r5, r5, 8); // kc - 8
@@ -131,28 +170,46 @@ void Generator::generate(size_t max_mr, size_t nc_mod_nr, size_t kc, const void*
   bind(l2);
   vld1_32({d0}, mem[r3]++); // A0
   vldm(mem[r9]++, {d8-d11}); // B0
-  vld1_32({d1}, mem[r12]++); // A1
-  vld1_32({d2}, mem[r10]++); // A2
-  vld1_32({d3}, mem[r0]++); // A3
+  if (max_mr > 1) {
+    vld1_32({d1}, mem[r12]++); // A1
+  }
+  if (max_mr > 2) {
+    vld1_32({d2}, mem[r10]++); // A2
+  }
+  if (max_mr > 3) {
+    vld1_32({d3}, mem[r0]++); // A3
+  }
   vldm(mem[r9]++, {d12-d15}); // B1
 
   vmla_f32(q8, q4, d0[0]);
   vmla_f32(q9, q5, d0[0]);
-  vmla_f32(q10, q4, d1[0]);
-  vmla_f32(q11, q5, d1[0]);
-  vmla_f32(q12, q4, d2[0]);
-  vmla_f32(q13, q5, d2[0]);
-  vmla_f32(q14, q4, d3[0]);
-  vmla_f32(q15, q5, d3[0]);
+  if (max_mr > 1) {
+    vmla_f32(q10, q4, d1[0]);
+    vmla_f32(q11, q5, d1[0]);
+  }
+  if (max_mr > 2) {
+    vmla_f32(q12, q4, d2[0]);
+    vmla_f32(q13, q5, d2[0]);
+  }
+  if (max_mr > 3) {
+    vmla_f32(q14, q4, d3[0]);
+    vmla_f32(q15, q5, d3[0]);
+  }
   vmla_f32(q8, q6, d0[1]);
   vmla_f32(q9, q7, d0[1]);
-  vmla_f32(q10, q6, d1[1]);
-  vmla_f32(q11, q7, d1[1]);
+  if (max_mr > 1) {
+    vmla_f32(q10, q6, d1[1]);
+    vmla_f32(q11, q7, d1[1]);
+  }
   subs(r5, r5, 8);
-  vmla_f32(q12, q6, d2[1]);
-  vmla_f32(q13, q7, d2[1]);
-  vmla_f32(q14, q6, d3[1]);
-  vmla_f32(q15, q7, d3[1]);
+  if (max_mr > 2) {
+    vmla_f32(q12, q6, d2[1]);
+    vmla_f32(q13, q7, d2[1]);
+  }
+  if (max_mr > 3) {
+    vmla_f32(q14, q6, d3[1]);
+    vmla_f32(q15, q7, d3[1]);
+  }
   bhs(l2);
 
   // Is there a remainder?- 1 float of A (4 bytes)
@@ -161,36 +218,60 @@ void Generator::generate(size_t max_mr, size_t nc_mod_nr, size_t kc, const void*
 
   bind(l3);
   // ks loop
-  subs(r14, r14, 16); // ks -= MR * sizeof(void*)
+  subs(r14, r14, max_mr * sizeof(void*)); // ks -= MR * sizeof(void*)
   bhi(l1);
 
   ldr(r7, mem[sp, 128]); // cn_stride
   ldr(r14, mem[sp, 72]); // p = ks
 
   // Clamp
-  vmax_f32(q8, q8, q2);
+  if (clamp_min) {
+    vmax_f32(q8, q8, q2);
+  }
   subs(r1, r1, 8);
-  vmax_f32(q9, q9, q2);
-  vmax_f32(q10, q10, q2);
-  vmax_f32(q11, q11, q2);
-  vmax_f32(q12, q12, q2);
-  vmax_f32(q13, q13, q2);
-  vmax_f32(q14, q14, q2);
-  vmax_f32(q15, q15, q2);
-  vmin_f32(q8, q8, q3);
-  vmin_f32(q9, q9, q3);
-  vmin_f32(q10, q10, q3);
-  vmin_f32(q11, q11, q3);
-  vmin_f32(q12, q12, q3);
-  vmin_f32(q13, q13, q3);
-  vmin_f32(q14, q14, q3);
-  vmin_f32(q15, q15, q3);
+  if (clamp_min) {
+    vmax_f32(q9, q9, q2);
+    if (max_mr > 1) {
+      vmax_f32(q10, q10, q2);
+      vmax_f32(q11, q11, q2);
+    }
+    if (max_mr > 2) {
+      vmax_f32(q12, q12, q2);
+      vmax_f32(q13, q13, q2);
+    }
+    if (max_mr > 3) {
+      vmax_f32(q14, q14, q2);
+      vmax_f32(q15, q15, q2);
+    }
+  }
+  if (clamp_max) {
+    vmin_f32(q8, q8, q3);
+    vmin_f32(q9, q9, q3);
+    if (max_mr > 1) {
+      vmin_f32(q10, q10, q3);
+      vmin_f32(q11, q11, q3);
+    }
+    if (max_mr > 2) {
+      vmin_f32(q12, q12, q3);
+      vmin_f32(q13, q13, q3);
+    }
+    if (max_mr > 3) {
+      vmin_f32(q14, q14, q3);
+      vmin_f32(q15, q15, q3);
+    }
+  }
 
   // Store full 4 x 8
   blo(l5);
-  vst1_32({d28-d31}, mem[r6], r7);
-  vst1_32({d24-d27}, mem[r8], r7);
-  vst1_32({d20-d23}, mem[r4], r7);
+  if (max_mr > 3) {
+    vst1_32({d28-d31}, mem[r6], r7);
+  }
+  if (max_mr > 2) {
+    vst1_32({d24-d27}, mem[r8], r7);
+  }
+  if (max_mr > 1) {
+    vst1_32({d20-d23}, mem[r4], r7);
+  }
   vst1_32({d16-d19}, mem[r11], r7);
   sub(r2, r2, r14); // a -= ks
   bhi(l0);
@@ -208,61 +289,99 @@ void Generator::generate(size_t max_mr, size_t nc_mod_nr, size_t kc, const void*
   vldm(mem[r0]++, {s6}); // A3
   vmla_f32(q8, q4, d0[0]);
   vmla_f32(q9, q5, d0[0]);
-  vmla_f32(q10, q4, d1[0]);
-  vmla_f32(q11, q5, d1[0]);
-  vmla_f32(q12, q4, d2[0]);
-  vmla_f32(q13, q5, d2[0]);
-  vmla_f32(q14, q4, d3[0]);
-  vmla_f32(q15, q5, d3[0]);
+  if (max_mr > 1) {
+    vmla_f32(q10, q4, d1[0]);
+    vmla_f32(q11, q5, d1[0]);
+  }
+  if (max_mr > 2) {
+    vmla_f32(q12, q4, d2[0]);
+    vmla_f32(q13, q5, d2[0]);
+  }
+  if (max_mr > 3) {
+    vmla_f32(q14, q4, d3[0]);
+    vmla_f32(q15, q5, d3[0]);
+  }
   b(l3);
 
   // Store odd width
   bind(l5);
   tst(r1, 4);
   beq(l6);
-  vst1_32({d28-d29}, mem[r6]++);
-  vst1_32({d24-d25}, mem[r8]++);
-  vmov(q14, q15);
-  vmov(q12, q13);
-  vst1_32({d20-d21}, mem[r4]++);
+  if (max_mr > 3) {
+    vst1_32({d28-d29}, mem[r6]++);
+  }
+  if (max_mr > 2) {
+    vst1_32({d24-d25}, mem[r8]++);
+  }
+  if (max_mr > 3) {
+    vmov(q14, q15);
+  }
+  if (max_mr > 2) {
+    vmov(q12, q13);
+  }
+  if (max_mr > 1) {
+    vst1_32({d20-d21}, mem[r4]++);
+  }
   vst1_32({d16-d17}, mem[r11]++);
-  vmov(q10, q11);
+  if (max_mr > 1) {
+    vmov(q10, q11);
+  }
   vmov(q8, q9);
 
   bind(l6);
   tst(r1, 2);
   beq(l7);
-  vst1_32({d28}, mem[r6]++);
-  vst1_32({d24}, mem[r8]++);
-  vmov(d28, d29);
-  vmov(d24, d25);
-  vst1_32({d20}, mem[r4]++);
+  if (max_mr > 3) {
+    vst1_32({d28}, mem[r6]++);
+  }
+  if (max_mr > 2) {
+    vst1_32({d24}, mem[r8]++);
+  }
+  if (max_mr > 3) {
+    vmov(d28, d29);
+  }
+  if (max_mr > 2) {
+    vmov(d24, d25);
+  }
+  if (max_mr > 1) {
+    vst1_32({d20}, mem[r4]++);
+  }
   vst1_32({d16}, mem[r11]++);
-  vmov(d20, d21);
+  if (max_mr > 1) {
+    vmov(d20, d21);
+  }
   vmov(d16, d17);
 
   bind(l7);
   tst(r1, 1);
   beq(l8);
-  vst1_32({d28[0]}, mem[r6]++);
-  vst1_32({d24[0]}, mem[r8]++);
-  vst1_32({d20[0]}, mem[r4]++);
+  if (max_mr > 3) {
+    vst1_32({d28[0]}, mem[r6]++);
+  }
+  if (max_mr > 2) {
+    vst1_32({d24[0]}, mem[r8]++);
+  }
+  if (max_mr > 1) {
+    vst1_32({d20[0]}, mem[r4]++);
+  }
   vst1_32({d16[0]}, mem[r11]++);
 
   bind(l8);
   vpop({d8-d15});
   add(sp, sp, 12); // skip pad, r2, r3
   pop({r4, r5, r6, r7, r8, r9, r10, r11, pc});
+
+  align(16);
 }
 }  // namespace
-}  // aarch32
-}  // xnnpack
+}  // namespace aarch32
+}  // namespace xnnpack
 
 xnn_status_t xnn_generate_f32_igemm_ukernel_4x8__aarch32_neon_ld64(xnn_code_buffer* code, size_t max_mr, size_t nc_mod_nr, size_t kc, size_t ks, const void* params) {
   using namespace xnnpack::aarch32;
   Generator g(code);
   assert(params != nullptr);
-  g.generate(max_mr, nc_mod_nr, kc, nullptr);
+  g.generate(max_mr, nc_mod_nr, kc, ks, static_cast<const jit_gemm_params*>(params));
   g.finalize();
   if (g.error() != xnnpack::Error::kNoError) {
     return xnn_status_invalid_state;
