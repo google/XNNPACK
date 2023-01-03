250ac82e81c47d9d5b261bd395f7f95e3c885118
Alan Kelly <alankelly@google.com>
2023-01-03 03:01:39 -0800

    Enable XNNPACK unsigned quantized inference by default
    PiperOrigin-RevId: 499167992

--- src/subgraph.c
+++ src/subgraph.c
@@ -1205,7 +1205,7 @@ enum xnn_status xnn_subgraph_optimize(
     }
 
     if (!xnn_value_is_external_input(value) && value->num_consumers == 0 && !xnn_value_is_persistent(value)) {
-      xnn_value_clear(value);
+      //xnn_value_clear(value);
     }
   }
 
