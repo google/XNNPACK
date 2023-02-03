19a66c46f57d498c6cf64b72a7a4cfcccdd18d2c
Zhi An Ng <zhin@google.com>
2023-02-03 14:21:57 -0800

    Allow XNN_PLATFORM_JIT to be specified explicitly
    Use --define=xnn_platform_jit={true,false} to overwrite the XNN_PLATFORM_JIT, which is usually set based on other (platform-specific) constants.
    
    PiperOrigin-RevId: 506993236

--- src/xnnpack/common.h
+++ src/xnnpack/common.h
@@ -134,11 +134,13 @@
   #define XNN_PLATFORM_QURT 0
 #endif
 
-#if (XNN_ARCH_ARM || XNN_ARCH_ARM64) && !XNN_PLATFORM_IOS && !XNN_PLATFORM_FUCHSIA
-  #define XNN_PLATFORM_JIT 1
-#else
-  #define XNN_PLATFORM_JIT 0
-#endif
+#ifndef XNN_PLATFORM_JIT
+  #if (XNN_ARCH_ARM || XNN_ARCH_ARM64) && !XNN_PLATFORM_IOS && !XNN_PLATFORM_FUCHSIA
+    #define XNN_PLATFORM_JIT 1
+  #else
+    #define XNN_PLATFORM_JIT 0
+  #endif
+#endif  // XNN_PLATFORM_JIT
 
 // Define compile identification macros
 
