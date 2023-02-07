adcaa51c1003fd0d538ef4f637757bea5ac2439a
Frank Barchard <fbarchard@google.com>
2023-02-07 02:18:14 -0800

    QS8 VCVT SSE/AVX replace _mm_storeu_si32 with unaligned_store_u32
    PiperOrigin-RevId: 507717545

--- src/qs8-vcvt/gen/qs8-vcvt-sse41-x32.c
+++ src/qs8-vcvt/gen/qs8-vcvt-sse41-x32.c
@@ -88,17 +88,18 @@ void xnn_qs8_vcvt_ukernel__sse41_x32(
 
     __m128i vy = _mm_packs_epi16(vacc, vacc);
     if (batch & (4 * sizeof(int8_t))) {
-      _mm_storeu_si32(output, vy);
+      unaligned_store_u32(output, (uint32_t) _mm_cvtsi128_si32(vy));
       vy = _mm_srli_epi64(vy, 32);
       output += 4;
     }
+    uint32_t vy_lo = (uint32_t) _mm_cvtsi128_si32(vy);
     if (batch & (2 * sizeof(int8_t))) {
-      _mm_storeu_si16(output, vy);
-      vy = _mm_srli_epi32(vy, 16);
+      unaligned_store_u16(output, (uint16_t) vy_lo);
+      vy_lo >>= 16;
       output += 2;
     }
     if (batch & (1 * sizeof(int8_t))) {
-      *output = (int8_t) _mm_extract_epi8(vy, 0);
+      *output = (int8_t) vy_lo;
     }
   }
 }
