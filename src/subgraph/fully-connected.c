be2d732fcb14a2fe3486d18a000dce2fcd1bc5c8
Marat Dukhan <maratek@google.com>
2023-04-06 20:40:21 -0700

    Remove erroneous checks for static bias in Fully Connected node
    PiperOrigin-RevId: 522510417

--- src/subgraph/fully-connected.c
+++ src/subgraph/fully-connected.c
@@ -434,11 +434,18 @@ enum xnn_status xnn_define_fully_connected(
       return xnn_status_invalid_parameter;
     }
 
-    if (bias_value->data == NULL) {
-      xnn_log_error(
-        "failed to define %s operator with bias ID #%" PRIu32 ": non-static Value",
-        xnn_node_type_to_string(xnn_node_type_fully_connected), bias_id);
-      return xnn_status_invalid_parameter;
+    // Non-static bias is supported, but only for some data types
+    switch (bias_value->datatype) {
+      case xnn_datatype_fp32:
+        break;  // non-static bias is supported
+      default:
+        if (bias_value->data == NULL) {
+          xnn_log_error(
+            "failed to define %s operator with bias ID #%" PRIu32 ": non-static Value",
+            xnn_node_type_to_string(xnn_node_type_fully_connected), bias_id);
+          return xnn_status_invalid_parameter;
+        }
+        break;
     }
 
     switch (bias_value->datatype) {
