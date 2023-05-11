a95d6955a9279e5829fca4344f915b819c8ae108
Zhi An Ng <zhin@google.com>
2023-05-11 13:39:35 -0700

    Separate f32dist for denominators
    PiperOrigin-RevId: 531294245

--- test/vbinary-microkernel-tester.h
+++ test/vbinary-microkernel-tester.h
@@ -299,17 +299,19 @@ class VBinaryMicrokernelTester {
   void Test(xnn_f32_vbinary_relu_ukernel_fn vbinary_relu, OpType op_type) const {
     std::random_device random_device;
     auto rng = std::mt19937(random_device());
-    std::uniform_real_distribution<float> f32dist(-1.0f, 1.0f);
+    std::uniform_real_distribution<float> lhs_f32dist(-1.0f, 1.0f);
+    // For denominator, avoid 0 so we don't get Infinity as the result.
+    std::uniform_real_distribution<float> rhs_f32dist(0.1, 1.0f);
 
     std::vector<float> a(batch_size() + XNN_EXTRA_BYTES / sizeof(float));
     std::vector<float> b(batch_size() + XNN_EXTRA_BYTES / sizeof(float));
     std::vector<float> y(batch_size() + (inplace_a() || inplace_b() ? XNN_EXTRA_BYTES / sizeof(float) : 0));
     std::vector<float> y_ref(batch_size());
     for (size_t iteration = 0; iteration < iterations(); iteration++) {
-      std::generate(a.begin(), a.end(), [&]() { return f32dist(rng); });
-      std::generate(b.begin(), b.end(), [&]() { return f32dist(rng); });
+      std::generate(a.begin(), a.end(), [&]() { return lhs_f32dist(rng); });
+      std::generate(b.begin(), b.end(), [&]() { return rhs_f32dist(rng); });
       if (inplace_a() || inplace_b()) {
-        std::generate(y.begin(), y.end(), [&]() { return f32dist(rng); });
+        std::generate(y.begin(), y.end(), [&]() { return lhs_f32dist(rng); });
       } else {
         std::fill(y.begin(), y.end(), nanf(""));
       }
@@ -354,10 +356,6 @@ class VBinaryMicrokernelTester {
 
       // Verify results.
       for (size_t i = 0; i < batch_size(); i++) {
-        // Special case for Div when denominator is 0.
-        if (y[i] == std::numeric_limits<float>::infinity() || y[i] == -std::numeric_limits<float>::infinity()) {
-          EXPECT_EQ(y[i], y_ref[i]) << "at " << i << " / " << batch_size();
-        }
         EXPECT_GE(y[i], 0.0f)
           << "at " << i << " / " << batch_size();
         EXPECT_NEAR(y[i], y_ref[i], std::abs(y_ref[i]) * 1.0e-6f)
