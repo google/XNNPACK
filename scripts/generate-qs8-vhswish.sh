93a5e9eedb9a9e46a40d600bca02adb99a3cf875
Frank Barchard <fbarchard@google.com>
2023-05-19 13:49:51 -0700

    Move F32=QC8W version GEMM microkernels into f32-gemm/gen/ for consistency with qs8-qc8w
    PiperOrigin-RevId: 533542388

--- scripts/generate-qs8-vhswish.sh
+++ scripts/generate-qs8-vhswish.sh
@@ -13,6 +13,15 @@ tools/xngen src/qs8-vhswish/neon.c.in -D BATCH_TILE=8  -D DATATYPE=QU8 -o src/qu
 tools/xngen src/qs8-vhswish/neon.c.in -D BATCH_TILE=16 -D DATATYPE=QU8 -o src/qu8-vhswish/gen/qu8-vhswish-neon-x16.c &
 tools/xngen src/qs8-vhswish/neon.c.in -D BATCH_TILE=32 -D DATATYPE=QU8 -o src/qu8-vhswish/gen/qu8-vhswish-neon-x32.c &
 
+#################################### Scalar ###################################
+tools/xngen src/qs8-vhswish/scalar.c.in -D BATCH_TILE=1 -D DATATYPE=QS8 -o src/qs8-vhswish/gen/qs8-vhswish-scalar-x1.c &
+tools/xngen src/qs8-vhswish/scalar.c.in -D BATCH_TILE=2 -D DATATYPE=QS8 -o src/qs8-vhswish/gen/qs8-vhswish-scalar-x2.c &
+tools/xngen src/qs8-vhswish/scalar.c.in -D BATCH_TILE=4 -D DATATYPE=QS8 -o src/qs8-vhswish/gen/qs8-vhswish-scalar-x4.c &
+
+tools/xngen src/qs8-vhswish/scalar.c.in -D BATCH_TILE=1 -D DATATYPE=QU8 -o src/qu8-vhswish/gen/qu8-vhswish-scalar-x1.c &
+tools/xngen src/qs8-vhswish/scalar.c.in -D BATCH_TILE=2 -D DATATYPE=QU8 -o src/qu8-vhswish/gen/qu8-vhswish-scalar-x2.c &
+tools/xngen src/qs8-vhswish/scalar.c.in -D BATCH_TILE=4 -D DATATYPE=QU8 -o src/qu8-vhswish/gen/qu8-vhswish-scalar-x4.c &
+
 ################################## Unit tests #################################
 tools/generate-vhswish-test.py --spec test/qs8-vhswish.yaml --output test/qs8-vhswish.cc &
 tools/generate-vhswish-test.py --spec test/qu8-vhswish.yaml --output test/qu8-vhswish.cc &
