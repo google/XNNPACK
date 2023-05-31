675fee8affa5b64912c787306b5fdbb22c722579
Zhi An Ng <zhin@google.com>
2023-05-30 17:20:57 -0700

    Add GEMM2 support to fully connected operator
    PiperOrigin-RevId: 536552337

--- src/operators/fully-connected-nc.c
+++ src/operators/fully-connected-nc.c
@@ -412,6 +412,20 @@ enum xnn_status xnn_create_fully_connected_nc_f32(
     return xnn_status_unsupported_hardware;
   }
 
+  const struct xnn_gemm_config* gemm2_config = xnn_init_f32_gemm2_config();
+  if (gemm_config == NULL) {
+    xnn_log_error("failed to create %s operator: unsupported hardware configuration",
+                  xnn_operator_type_to_string(xnn_operator_type_convolution_nhwc_f32));
+    return xnn_status_unsupported_hardware;
+  }
+
+  if (gemm_config->nr > output_channels) {
+    // Default microkernel is suboptimal, use a microkernel that better supports less output channels.
+    if (gemm2_config->minmax.gemm[gemm2_config->mr-1].function[XNN_UARCH_DEFAULT] != NULL) {
+      gemm_config = gemm2_config;
+    }
+  }
+
   const struct gemm_fused_ukernels* gemm_ukernels = &gemm_config->minmax;
   const bool linear_activation = (output_max == INFINITY) && (output_min == -output_max);
   if (linear_activation && gemm_config->linear.gemm[gemm_config->mr-1].function[XNN_UARCH_DEFAULT] != NULL) {
