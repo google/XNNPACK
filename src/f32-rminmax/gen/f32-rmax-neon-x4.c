3045368a98e295ee293eaa023923a4c8c9c5be73
Alan Kelly <alankelly@google.com>
2023-06-12 10:56:55 -0700

    Internal config change
    PiperOrigin-RevId: 539703008

--- src/f32-rminmax/gen/f32-rmax-neon-x4.c
+++ src/f32-rminmax/gen/f32-rmax-neon-x4.c
@@ -26,20 +26,20 @@ void xnn_f32_rmax_ukernel__neon_x4(
   assert(input != NULL);
   assert(output != NULL);
 
-  float32x4_t vacc0 = vld1q_dup_f32(input);
+  float32x4_t vmax0 = vld1q_dup_f32(input);
   for (; batch >= 4 * sizeof(float); batch -= 4 * sizeof(float)) {
     const float32x4_t vt = vld1q_f32(input); input += 4;
-    vacc0 = vmaxq_f32(vacc0, vt);
+    vmax0 = vmaxq_f32(vmax0, vt);
   }
-  float32x2_t vacc = vmax_f32(vget_low_f32(vacc0), vget_high_f32(vacc0));
+  float32x2_t vmax = vmax_f32(vget_low_f32(vmax0), vget_high_f32(vmax0));
   if XNN_UNLIKELY(batch & (2 * sizeof(float))) {
     const float32x2_t vt = vld1_f32(input); input += 2;
-    vacc = vmax_f32(vacc, vt);
+    vmax = vmax_f32(vmax, vt);
   }
-  vacc = vpmax_f32(vacc, vacc);
+  vmax = vpmax_f32(vmax, vmax);
   if XNN_UNLIKELY(batch & (1 * sizeof(float))) {
     const float32x2_t vt = vld1_dup_f32(input);
-    vacc = vmax_f32(vacc, vt);
+    vmax = vmax_f32(vmax, vt);
   }
-  vst1_lane_f32(output, vacc, 0);
+  vst1_lane_f32(output, vmax, 0);
 }
--- src/f32-rminmax/gen/f32-rminmax-neon-x8-acc2.c
+++ src/f32-rminmax/gen/f32-rminmax-neon-x8-acc2.c
@@ -15,7 +15,7 @@
 #include <xnnpack/reduce.h>
 
 
-void xnn_f32_rmin_ukernel__neon_x16_acc4(
+void xnn_f32_rminmax_ukernel__neon_x8_acc2(
     size_t batch,
     const float* input,
     float* output,
@@ -27,36 +27,39 @@ void xnn_f32_rmin_ukernel__neon_x16_acc4(
   assert(output != NULL);
 
   float32x4_t vmin0 = vld1q_dup_f32(input);
+  float32x4_t vmax0 = vmin0;
   float32x4_t vmin1 = vmin0;
-  float32x4_t vmin2 = vmin0;
-  float32x4_t vmin3 = vmin0;
-  for (; batch >= 16 * sizeof(float); batch -= 16 * sizeof(float)) {
+  float32x4_t vmax1 = vmax0;
+  for (; batch >= 8 * sizeof(float); batch -= 8 * sizeof(float)) {
     const float32x4_t vt0 = vld1q_f32(input); input += 4;
     const float32x4_t vt1 = vld1q_f32(input); input += 4;
-    const float32x4_t vt2 = vld1q_f32(input); input += 4;
-    const float32x4_t vt3 = vld1q_f32(input); input += 4;
 
     vmin0 = vminq_f32(vmin0, vt0);
+    vmax0 = vmaxq_f32(vmax0, vt0);
     vmin1 = vminq_f32(vmin1, vt1);
-    vmin2 = vminq_f32(vmin2, vt2);
-    vmin3 = vminq_f32(vmin3, vt3);
+    vmax1 = vmaxq_f32(vmax1, vt1);
   }
   vmin0 = vminq_f32(vmin0, vmin1);
-  vmin2 = vminq_f32(vmin2, vmin3);
-  vmin0 = vminq_f32(vmin0, vmin2);
+  vmax0 = vmaxq_f32(vmax0, vmax1);
   for (; batch >= 4 * sizeof(float); batch -= 4 * sizeof(float)) {
     const float32x4_t vt = vld1q_f32(input); input += 4;
     vmin0 = vminq_f32(vmin0, vt);
+    vmax0 = vmaxq_f32(vmax0, vt);
   }
   float32x2_t vmin = vmin_f32(vget_low_f32(vmin0), vget_high_f32(vmin0));
+  float32x2_t vmax = vmax_f32(vget_low_f32(vmax0), vget_high_f32(vmax0));
   if XNN_UNLIKELY(batch & (2 * sizeof(float))) {
     const float32x2_t vt = vld1_f32(input); input += 2;
     vmin = vmin_f32(vmin, vt);
+    vmax = vmax_f32(vmax, vt);
   }
   vmin = vpmin_f32(vmin, vmin);
+  vmax = vpmax_f32(vmax, vmax);
   if XNN_UNLIKELY(batch & (1 * sizeof(float))) {
     const float32x2_t vt = vld1_dup_f32(input);
     vmin = vmin_f32(vmin, vt);
+    vmax = vmax_f32(vmax, vt);
   }
   vst1_lane_f32(output, vmin, 0);
+  vst1_lane_f32(output + 1, vmax, 0);
 }
