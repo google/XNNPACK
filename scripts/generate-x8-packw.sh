070a20f866ce603bd00d5730d71f22fd10315ed4
Frank Barchard <fbarchard@google.com>
2023-06-21 06:04:53 -0700

    Move packw microkernels to expected file names
    PiperOrigin-RevId: 542231196

--- scripts/generate-x8-packw.sh
+++ scripts/generate-x8-packw.sh
@@ -6,10 +6,10 @@
 
 #################################### Scalar ###################################
 ### Generic C micro-kernels
-tools/xngen src/x8-packw/scalar.c.in -D NR=2  -D KBLOCK=4 -D TYPE=int8_t -o src/x8-packw/gen/x8-packw-x2-scalar-int-x4.c &
-tools/xngen src/x8-packw/scalar.c.in -D NR=4  -D KBLOCK=4 -D TYPE=int8_t -o src/x8-packw/gen/x8-packw-x4-scalar-int-x4.c &
-tools/xngen src/x8-packw/scalar.c.in -D NR=8  -D KBLOCK=4 -D TYPE=int8_t -o src/x8-packw/gen/x8-packw-x8-scalar-int-x4.c &
-tools/xngen src/x8-packw/scalar.c.in -D NR=16 -D KBLOCK=4 -D TYPE=int8_t -o src/x8-packw/gen/x8-packw-x16-scalar-int-x4.c &
+tools/xngen src/x8-packw/scalar.c.in -D NR=2  -D KBLOCK=4 -D TYPE=int8_t -o src/x8-packw/gen/x8-packw-x2-gemm-goi-scalar-int-x4.c &
+tools/xngen src/x8-packw/scalar.c.in -D NR=4  -D KBLOCK=4 -D TYPE=int8_t -o src/x8-packw/gen/x8-packw-x4-gemm-goi-scalar-int-x4.c &
+tools/xngen src/x8-packw/scalar.c.in -D NR=8  -D KBLOCK=4 -D TYPE=int8_t -o src/x8-packw/gen/x8-packw-x8-gemm-goi-scalar-int-x4.c &
+tools/xngen src/x8-packw/scalar.c.in -D NR=16 -D KBLOCK=4 -D TYPE=int8_t -o src/x8-packw/gen/x8-packw-x16-gemm-goi-scalar-int-x4.c &
 
 ################################## Unit tests #################################
 tools/generate-packw-test.py --spec test/x8-packw.yaml --output test/x8-packw.cc &
