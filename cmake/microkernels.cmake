df12900b4545d642c9e53e2bec7373d008df5b23
Frank Barchard <fbarchard@google.com>
2023-06-29 21:56:06 -0700

    Move qd8-f32-qs8w-gemm microkernels to expected file names
    PiperOrigin-RevId: 544550088

--- cmake/microkernels.cmake
+++ cmake/microkernels.cmake
@@ -3427,12 +3427,12 @@ SET(ALL_NEONBF16_MICROKERNEL_SRCS
 SET(ALL_NEONBF16_AARCH64_MICROKERNEL_SRCS)
 
 SET(ALL_NEONDOT_MICROKERNEL_SRCS
-  src/qd8-f32-qs8w-gemm/gen/qd8-f32-qs8w-gemm-1x8c4-neondot.c
-  src/qd8-f32-qs8w-gemm/gen/qd8-f32-qs8w-gemm-1x16c4-neondot.c
-  src/qd8-f32-qs8w-gemm/gen/qd8-f32-qs8w-gemm-2x8c4-neondot.c
-  src/qd8-f32-qs8w-gemm/gen/qd8-f32-qs8w-gemm-2x16c4-neondot.c
-  src/qd8-f32-qs8w-gemm/gen/qd8-f32-qs8w-gemm-3x16c4-neondot.c
-  src/qd8-f32-qs8w-gemm/gen/qd8-f32-qs8w-gemm-4x16c4-neondot.c
+  src/qd8-f32-qs8w-gemm/gen/qd8-f32-qs8w-gemm-1x8c4-minmax-neondot.c
+  src/qd8-f32-qs8w-gemm/gen/qd8-f32-qs8w-gemm-1x16c4-minmax-neondot.c
+  src/qd8-f32-qs8w-gemm/gen/qd8-f32-qs8w-gemm-2x8c4-minmax-neondot.c
+  src/qd8-f32-qs8w-gemm/gen/qd8-f32-qs8w-gemm-2x16c4-minmax-neondot.c
+  src/qd8-f32-qs8w-gemm/gen/qd8-f32-qs8w-gemm-3x16c4-minmax-neondot.c
+  src/qd8-f32-qs8w-gemm/gen/qd8-f32-qs8w-gemm-4x16c4-minmax-neondot.c
   src/qs8-gemm/gen/qs8-gemm-1x8c4-minmax-fp32-neondot.c
   src/qs8-gemm/gen/qs8-gemm-1x8c4-minmax-rndnu-neondot.c
   src/qs8-gemm/gen/qs8-gemm-1x16c4-minmax-rndnu-neondot.c
