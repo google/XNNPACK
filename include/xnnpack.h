CodeLinaro-mirror/le_XNNPACK

f32->qd8 convert operator
PiperOrigin-RevId: 545632971

include/xnnpack.h
@@ -5050,6 +5050,29 @@ enum xnn_status xnn_run_transpose_nd_x8(
     uint32_t flags,
     pthreadpool_t threadpool);
 
+struct xnn_qd8_quantization_params {
+  int32_t zero_point;
+  float scale;
+};
+
+enum xnn_status xnn_create_convert_nc_f32_qd8(
+  size_t channels,
+  size_t input_stride,
+  size_t output_stride,
+  uint32_t flags,
+  xnn_operator_t* convert_op_out);
+
+enum xnn_status xnn_setup_convert_nc_f32_qd8(
+  xnn_operator_t convert_op,
+  const float* input,
+  int8_t* output,
+  struct xnn_qd8_quantization_params* quantization_params);
+
+enum xnn_status xnn_reshape_convert_nc_f32_qd8(
+  xnn_operator_t convert_op,
+  size_t batch_size,
+  pthreadpool_t threadpool);
+
 enum xnn_status xnn_create_convert_nc_f16_f32(
   size_t channels,
   size_t input_stride,

src/reduce-config.c
@@ -19,13 +19,16 @@
 
 
 static struct xnn_reduce_config f16_f32acc_rsum_config = {0};
+static struct xnn_reduce_config f32_rminmax_config = {0};
 static struct xnn_reduce_config f32_rsum_config = {0};
 
 #if XNN_PLATFORM_WINDOWS
   static INIT_ONCE init_guard_f16_f32acc_rsum = INIT_ONCE_STATIC_INIT;
+  static INIT_ONCE init_guard_f32_rminmax = INIT_ONCE_STATIC_INIT;
   static INIT_ONCE init_guard_f32_rsum = INIT_ONCE_STATIC_INIT;
 #else
   static pthread_once_t init_guard_f16_f32acc_rsum = PTHREAD_ONCE_INIT;
+  static pthread_once_t init_guard_f32_rminmax = PTHREAD_ONCE_INIT;
   static pthread_once_t init_guard_f32_rsum = PTHREAD_ONCE_INIT;
 #endif
 
@@ -60,6 +63,56 @@ static void init_f16_f32acc_rsum_config(void) {
         .element_tile = 32,
       };
     }
+  #endif
+}
+
+static void init_f32_rminmax_config(void) {
+  #if XNN_ARCH_ARM
+    const struct xnn_hardware_config* hardware_config = xnn_init_hardware_config();
+    assert(hardware_config != NULL);
+    if (hardware_config->use_arm_neon) {
+      f32_rminmax_config = (struct xnn_reduce_config) {
+        .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__neon_x16_acc4,
+        .init.f32_scale = xnn_init_f32_scale_scalar_params,
+        .element_tile = 16,
+      };
+    } else if (!XNN_PLATFORM_MOBILE) {
+      f32_rminmax_config = (struct xnn_reduce_config) {
+        .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__scalar_x4_acc4,
+        .init.f32_scale = xnn_init_f32_scale_scalar_params,
+        .element_tile = 4,
+      };
+    }
+  #elif XNN_ARCH_ARM64
+    f32_rminmax_config = (struct xnn_reduce_config) {
+      .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__neon_x16_acc4,
+      .init.f32_scale = xnn_init_f32_scale_scalar_params,
+      .element_tile = 16,
+    };
+  #elif XNN_ARCH_X86 || XNN_ARCH_X86_64
+    f32_rminmax_config = (struct xnn_reduce_config) {
+      .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__sse_x16_acc4,
+      .init.f32_scale = xnn_init_f32_scale_scalar_params,
+      .element_tile = 16,
+    };
+  #elif XNN_ARCH_WASMSIMD || XNN_ARCH_WASMRELAXEDSIMD
+    f32_rminmax_config = (struct xnn_reduce_config) {
+      .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__wasmsimd_minmax_x16_acc4,
+      .init.f32_scale = xnn_init_f32_scale_scalar_params,
+      .element_tile = 16,
+    };
+  #elif XNN_ARCH_WASM
+    f32_rminmax_config = (struct xnn_reduce_config) {
+      .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__wasm_x4_acc4,
+      .init.f32_scale = xnn_init_f32_scale_scalar_params,
+      .element_tile = 4,
+    };
+  #elif XNN_ARCH_RISCV
+    f32_rminmax_config = (struct xnn_reduce_config) {
+      .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__scalar_x4_acc4,
+      .init.f32_scale = xnn_init_f32_scale_scalar_params,
+      .element_tile = 4,
+    };
   #endif
 }
 
@@ -124,6 +177,11 @@ static void init_f32_rsum_config(void) {
 }
 
 #if XNN_PLATFORM_WINDOWS
+  static BOOL CALLBACK init_f16_f32acc_rminmax_config_windows(PINIT_ONCE init_once, PVOID parameter, PVOID* context) {
+    init_f16_f32acc_rminmax_config();
+    return TRUE;
+  }
+
   static BOOL CALLBACK init_f16_f32acc_rsum_config_windows(PINIT_ONCE init_once, PVOID parameter, PVOID* context) {
     init_f16_f32acc_rsum_config();
     return TRUE;
@@ -146,6 +204,19 @@ const struct xnn_reduce_config* xnn_init_f16_f32acc_rsum_config() {
     pthread_once(&init_guard_f16_f32acc_rsum, &init_f16_f32acc_rsum_config);
   #endif
   return &f16_f32acc_rsum_config;
+}
+
+const struct xnn_reduce_config* xnn_init_f32_rminmax_config() {
+  const struct xnn_hardware_config* hardware_config = xnn_init_hardware_config();
+  if (hardware_config == NULL) {
+    return NULL;
+  }
+  #if XNN_PLATFORM_WINDOWS
+    InitOnceExecuteOnce(&init_guard_f32_rminmax, &init_f32_rminmax_config_windows, NULL, NULL);
+  #else
+    pthread_once(&init_guard_f32_rminmax, &init_f32_rminmax_config);
+  #endif
+  return &f32_rminmax_config;
 }
 
 const struct xnn_reduce_config* xnn_init_f32_rsum_config() {

test/convert-operator-tester.h
@@ -19,6 +19,7 @@
 #include <fp16/fp16.h>
 
 #include <xnnpack.h>
+#include <xnnpack/prequantization.h>
 
 class ConvertOperatorTester {
  public:
@@ -218,6 +219,57 @@ class ConvertOperatorTester {
       for (size_t i = 0; i < batch_size(); i++) {
         for (size_t c = 0; c < channels(); c++) {
           EXPECT_EQ(output_ref[i * channels() + c], output[i * output_stride() + c])
+            << "at batch " << i << " / " << batch_size() << ", channel " << c << " / " << channels();
+        }
+      }
+    }
+  }
+
+  void TestF32toQD8() const {
+    std::random_device random_device;
+    auto rng = std::mt19937(random_device());
+
+    std::vector<float> input(XNN_EXTRA_BYTES / sizeof(float) +
+      (batch_size() - 1) * input_stride() + channels());
+    std::vector<int8_t> output((batch_size() - 1) * output_stride() + channels());
+    std::vector<xnn_qd8_quantization_params> quantization_params(batch_size());
+    std::uniform_real_distribution<float> range_dist(-100000, 100000);
+    for (size_t iteration = 0; iteration < iterations(); iteration++) {
+      const float first_val = range_dist(rng);
+      const float second_val = range_dist(rng);
+      std::uniform_real_distribution<float> f32dist(std::min(first_val, second_val), std::max(first_val, second_val));
+      std::generate(input.begin(), input.end(), [&]() { return f32dist(rng); });
+      std::fill(output.begin(), output.end(), INT8_C(0xA5));
+
+      // Create, setup, run, and destroy Convert operator.
+      ASSERT_EQ(xnn_status_success, xnn_initialize(nullptr /* allocator */));
+      xnn_operator_t convert_op = nullptr;
+
+      ASSERT_EQ(xnn_status_success,
+        xnn_create_convert_nc_f32_qd8(
+          channels(), input_stride(), output_stride(),
+          0, &convert_op));
+      ASSERT_NE(nullptr, convert_op);
+
+      // Smart pointer to automatically delete convert op.
+      std::unique_ptr<xnn_operator, decltype(&xnn_delete_operator)> auto_convert_op(convert_op, xnn_delete_operator);
+
+      ASSERT_EQ(xnn_status_success, xnn_reshape_convert_nc_f32_qd8(convert_op, batch_size(), /*threadpool=*/nullptr));
+      ASSERT_EQ(xnn_status_success, xnn_setup_convert_nc_f32_qd8(convert_op, input.data(), output.data(), quantization_params.data()));
+      ASSERT_EQ(xnn_status_success, xnn_run_operator(convert_op, /*threadpool=*/nullptr));
+
+      // Verify results.
+      for (size_t i = 0; i < batch_size(); i++) {
+        const float* input_ptr = &input[i * input_stride()];
+        const auto minmax = std::minmax_element(input_ptr, input_ptr + channels());
+        const float rmin = math_min_f32(0.0f, *minmax.first);
+        const float rmax = math_max_f32(0.0f, *minmax.second);
+        const float max_acceptable_error = 0.5001f * (rmax - rmin) / std::numeric_limits<uint8_t>::max();
+        for (size_t c = 0; c < channels(); c++) {
+          float expected = input[i * input_stride() + c];
+          int8_t quantized_val = output[i * output_stride() + c];
+          float dequantized_val = float(quantized_val - quantization_params[i].zero_point) / quantization_params[i].scale;
+          EXPECT_NEAR(expected, dequantized_val, max_acceptable_error)
             << "at batch " << i << " / " << batch_size() << ", channel " << c << " / " << channels();
         }
       }

google/XNNPACK

include/xnnpack.h
@@ -5050,6 +5050,29 @@ enum xnn_status xnn_run_transpose_nd_x8(
     uint32_t flags,
     pthreadpool_t threadpool);
 
+struct xnn_dynamic_quantization_params {
+  int32_t zero_point;
+  float scale;
+};
+
+enum xnn_status xnn_create_convert_nc_f32_qd8(
+  size_t channels,
+  size_t input_stride,
+  size_t output_stride,
+  uint32_t flags,
+  xnn_operator_t* convert_op_out);
+
+enum xnn_status xnn_setup_convert_nc_f32_qd8(
+  xnn_operator_t convert_op,
+  const float* input,
+  int8_t* output,
+  struct xnn_dynamic_quantization_params* quantization_params);
+
+enum xnn_status xnn_reshape_convert_nc_f32_qd8(
+  xnn_operator_t convert_op,
+  size_t batch_size,
+  pthreadpool_t threadpool);
+
 enum xnn_status xnn_create_convert_nc_f16_f32(
   size_t channels,
   size_t input_stride,

src/reduce-config.c
@@ -19,13 +19,16 @@
 
 
 static struct xnn_reduce_config f16_f32acc_rsum_config = {0};
+static struct xnn_reduce_config f32_rminmax_config = {0};
 static struct xnn_reduce_config f32_rsum_config = {0};
 
 #if XNN_PLATFORM_WINDOWS
   static INIT_ONCE init_guard_f16_f32acc_rsum = INIT_ONCE_STATIC_INIT;
+  static INIT_ONCE init_guard_f32_rminmax = INIT_ONCE_STATIC_INIT;
   static INIT_ONCE init_guard_f32_rsum = INIT_ONCE_STATIC_INIT;
 #else
   static pthread_once_t init_guard_f16_f32acc_rsum = PTHREAD_ONCE_INIT;
+  static pthread_once_t init_guard_f32_rminmax = PTHREAD_ONCE_INIT;
   static pthread_once_t init_guard_f32_rsum = PTHREAD_ONCE_INIT;
 #endif
 
@@ -60,6 +63,56 @@ static void init_f16_f32acc_rsum_config(void) {
         .element_tile = 32,
       };
     }
+  #endif
+}
+
+static void init_f32_rminmax_config(void) {
+  #if XNN_ARCH_ARM
+    const struct xnn_hardware_config* hardware_config = xnn_init_hardware_config();
+    assert(hardware_config != NULL);
+    if (hardware_config->use_arm_neon) {
+      f32_rminmax_config = (struct xnn_reduce_config) {
+        .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__neon_x16_acc4,
+        .init.f32_scale = xnn_init_f32_scale_scalar_params,
+        .element_tile = 16,
+      };
+    } else if (!XNN_PLATFORM_MOBILE) {
+      f32_rminmax_config = (struct xnn_reduce_config) {
+        .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__scalar_x4_acc4,
+        .init.f32_scale = xnn_init_f32_scale_scalar_params,
+        .element_tile = 4,
+      };
+    }
+  #elif XNN_ARCH_ARM64
+    f32_rminmax_config = (struct xnn_reduce_config) {
+      .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__neon_x16_acc4,
+      .init.f32_scale = xnn_init_f32_scale_scalar_params,
+      .element_tile = 16,
+    };
+  #elif XNN_ARCH_X86 || XNN_ARCH_X86_64
+    f32_rminmax_config = (struct xnn_reduce_config) {
+      .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__sse_x16_acc4,
+      .init.f32_scale = xnn_init_f32_scale_scalar_params,
+      .element_tile = 16,
+    };
+  #elif XNN_ARCH_WASMSIMD || XNN_ARCH_WASMRELAXEDSIMD
+    f32_rminmax_config = (struct xnn_reduce_config) {
+      .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__wasmsimd_minmax_x16_acc4,
+      .init.f32_scale = xnn_init_f32_scale_scalar_params,
+      .element_tile = 16,
+    };
+  #elif XNN_ARCH_WASM
+    f32_rminmax_config = (struct xnn_reduce_config) {
+      .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__wasm_x4_acc4,
+      .init.f32_scale = xnn_init_f32_scale_scalar_params,
+      .element_tile = 4,
+    };
+  #elif XNN_ARCH_RISCV
+    f32_rminmax_config = (struct xnn_reduce_config) {
+      .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__scalar_x4_acc4,
+      .init.f32_scale = xnn_init_f32_scale_scalar_params,
+      .element_tile = 4,
+    };
   #endif
 }
 
@@ -129,6 +182,11 @@ static void init_f32_rsum_config(void) {
     return TRUE;
   }
 
+  static BOOL CALLBACK init_f32_rminmax_config_windows(PINIT_ONCE init_once, PVOID parameter, PVOID* context) {
+    init_f32_rminmax_config();
+    return TRUE;
+  }
+
   static BOOL CALLBACK init_f32_rsum_config_windows(PINIT_ONCE init_once, PVOID parameter, PVOID* context) {
     init_f32_rsum_config();
     return TRUE;
@@ -146,6 +204,19 @@ const struct xnn_reduce_config* xnn_init_f16_f32acc_rsum_config() {
     pthread_once(&init_guard_f16_f32acc_rsum, &init_f16_f32acc_rsum_config);
   #endif
   return &f16_f32acc_rsum_config;
+}
+
+const struct xnn_reduce_config* xnn_init_f32_rminmax_config() {
+  const struct xnn_hardware_config* hardware_config = xnn_init_hardware_config();
+  if (hardware_config == NULL) {
+    return NULL;
+  }
+  #if XNN_PLATFORM_WINDOWS
+    InitOnceExecuteOnce(&init_guard_f32_rminmax, &init_f32_rminmax_config_windows, NULL, NULL);
+  #else
+    pthread_once(&init_guard_f32_rminmax, &init_f32_rminmax_config);
+  #endif
+  return &f32_rminmax_config;
 }
 
 const struct xnn_reduce_config* xnn_init_f32_rsum_config() {

test/convert-operator-tester.h
@@ -19,6 +19,7 @@
 #include <fp16/fp16.h>
 
 #include <xnnpack.h>
+#include <xnnpack/prequantization.h>
 
 class ConvertOperatorTester {
  public:
@@ -218,6 +219,57 @@ class ConvertOperatorTester {
       for (size_t i = 0; i < batch_size(); i++) {
         for (size_t c = 0; c < channels(); c++) {
           EXPECT_EQ(output_ref[i * channels() + c], output[i * output_stride() + c])
+            << "at batch " << i << " / " << batch_size() << ", channel " << c << " / " << channels();
+        }
+      }
+    }
+  }
+
+  void TestF32toQD8() const {
+    std::random_device random_device;
+    auto rng = std::mt19937(random_device());
+
+    std::vector<float> input(XNN_EXTRA_BYTES / sizeof(float) +
+      (batch_size() - 1) * input_stride() + channels());
+    std::vector<int8_t> output((batch_size() - 1) * output_stride() + channels());
+    std::vector<xnn_dynamic_quantization_params> quantization_params(batch_size());
+    std::uniform_real_distribution<float> range_dist(-100000, 100000);
+    for (size_t iteration = 0; iteration < iterations(); iteration++) {
+      const float first_val = range_dist(rng);
+      const float second_val = range_dist(rng);
+      std::uniform_real_distribution<float> f32dist(std::min(first_val, second_val), std::max(first_val, second_val));
+      std::generate(input.begin(), input.end(), [&]() { return f32dist(rng); });
+      std::fill(output.begin(), output.end(), INT8_C(0xA5));
+
+      // Create, setup, run, and destroy Convert operator.
+      ASSERT_EQ(xnn_status_success, xnn_initialize(nullptr /* allocator */));
+      xnn_operator_t convert_op = nullptr;
+
+      ASSERT_EQ(xnn_status_success,
+        xnn_create_convert_nc_f32_qd8(
+          channels(), input_stride(), output_stride(),
+          0, &convert_op));
+      ASSERT_NE(nullptr, convert_op);
+
+      // Smart pointer to automatically delete convert op.
+      std::unique_ptr<xnn_operator, decltype(&xnn_delete_operator)> auto_convert_op(convert_op, xnn_delete_operator);
+
+      ASSERT_EQ(xnn_status_success, xnn_reshape_convert_nc_f32_qd8(convert_op, batch_size(), /*threadpool=*/nullptr));
+      ASSERT_EQ(xnn_status_success, xnn_setup_convert_nc_f32_qd8(convert_op, input.data(), output.data(), quantization_params.data()));
+      ASSERT_EQ(xnn_status_success, xnn_run_operator(convert_op, /*threadpool=*/nullptr));
+
+      // Verify results.
+      for (size_t i = 0; i < batch_size(); i++) {
+        const float* input_ptr = &input[i * input_stride()];
+        const auto minmax = std::minmax_element(input_ptr, input_ptr + channels());
+        const float rmin = math_min_f32(0.0f, *minmax.first);
+        const float rmax = math_max_f32(0.0f, *minmax.second);
+        const float max_acceptable_error = 0.5001f * (rmax - rmin) / std::numeric_limits<uint8_t>::max();
+        for (size_t c = 0; c < channels(); c++) {
+          float expected = input[i * input_stride() + c];
+          int8_t quantized_val = output[i * output_stride() + c];
+          float dequantized_val = float(quantized_val - quantization_params[i].zero_point) / quantization_params[i].scale;
+          EXPECT_NEAR(expected, dequantized_val, max_acceptable_error)
             << "at batch " << i << " / " << batch_size() << ", channel " << c << " / " << channels();
         }
       }