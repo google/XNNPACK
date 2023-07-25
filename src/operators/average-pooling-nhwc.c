5695c8deb5add47472fc7f35284bd173e7b72b5e
Valeriy Avanesov <avanesov@google.com>
2023-07-25 09:37:36 -0700

    Use goto error idiom to deduplicate cleanup code
    PiperOrigin-RevId: 550911348

--- src/operators/average-pooling-nhwc.c
+++ src/operators/average-pooling-nhwc.c
@@ -416,33 +416,29 @@ enum xnn_status xnn_create_average_pooling2d_nhwc_f16(
 
   average_pooling_op->type = xnn_operator_type_average_pooling_nhwc_f16;
 
+  status = xnn_status_unsupported_hardware;
+
   const struct xnn_avgpool_config* avgpool_config = xnn_init_f16_avgpool_config();
   if (avgpool_config == NULL) {
-    xnn_release_simd_memory(zero_buffer);
-    xnn_release_simd_memory(average_pooling_op);
     xnn_log_error("failed to create %s operator: unsupported hardware configuration",
                   xnn_operator_type_to_string(xnn_operator_type_average_pooling_nhwc_f16));
-    return xnn_status_unsupported_hardware;
+    goto error;
   }
   average_pooling_op->avgpool_config = avgpool_config;
 
   const struct xnn_pavgpool_config* pavgpool_config = xnn_init_f16_pavgpool_config();
   if (pavgpool_config == NULL) {
-    xnn_release_simd_memory(zero_buffer);
-    xnn_release_simd_memory(average_pooling_op);
     xnn_log_error("failed to create %s operator: unsupported hardware configuration",
                   xnn_operator_type_to_string(xnn_operator_type_average_pooling_nhwc_f16));
-    return xnn_status_unsupported_hardware;
+    goto error;
   }
   average_pooling_op->pavgpool_config = pavgpool_config;
 
   const struct xnn_gavgpool_config* gavgpool_config = xnn_init_f16_gavgpool_config();
   if (gavgpool_config == NULL) {
-    xnn_release_simd_memory(zero_buffer);
-    xnn_release_simd_memory(average_pooling_op);
     xnn_log_error("failed to create %s operator: unsupported hardware configuration",
                   xnn_operator_type_to_string(xnn_operator_type_average_pooling_nhwc_f16));
-    return xnn_status_unsupported_hardware;
+    goto error;
   }
   average_pooling_op->gavgpool_config = gavgpool_config;
 
