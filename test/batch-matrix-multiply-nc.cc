6a8bd623b0d9ca9bc08a03828b7f8d6865f8a973
Zhi An Ng <zhin@google.com>
2023-08-01 22:12:36 -0700

    Remove some overlapping test cases for Batch Matrix Multiply NC
    PiperOrigin-RevId: 553026109

--- test/batch-matrix-multiply-nc.cc
+++ test/batch-matrix-multiply-nc.cc
@@ -21,15 +21,13 @@ TEST(BATCH_MATRIX_MULTIPLY_NC_F32, unit_batch) {
 }
 
 TEST(BATCH_MATRIX_MULTIPLY_NC_F32, small_batch) {
-  for (size_t batch_size = 2; batch_size < 13; batch_size++) {
-    BatchMatMulOperatorTester()
-      .batch_size(batch_size)
-      .m(17)
-      .k(23)
-      .n(19)
-      .iterations(3)
-      .TestF32();
-  }
+  BatchMatMulOperatorTester()
+    .batch_size(5)
+    .m(17)
+    .k(23)
+    .n(19)
+    .iterations(3)
+    .TestF32();
 }
 
 TEST(BATCH_MATRIX_MULTIPLY_NC_F32, unit_batch_bigger_matrices) {
@@ -43,33 +41,13 @@ TEST(BATCH_MATRIX_MULTIPLY_NC_F32, unit_batch_bigger_matrices) {
 }
 
 TEST(BATCH_MATRIX_MULTIPLY_NC_F32, small_batch_bigger_matrices) {
-  for (size_t batch_size = 2; batch_size < 13; batch_size++) {
-    BatchMatMulOperatorTester()
-      .batch_size(batch_size)
-      .m(37)
-      .k(101)
-      .n(71)
-      .iterations(3)
-      .TestF32();
-  }
-}
-
-TEST(BATCH_MATRIX_MULTIPLY_NC_F32, various_batch_and_small_matrices) {
-  for (size_t b = 1; b < 11; b++) {
-    for (size_t m = 3; m < 27; m += 3) {
-      for (size_t k = 5; k < 30; k += 5) {
-        for (size_t n = 7; n < 49; n += 7) {
-          BatchMatMulOperatorTester()
-            .batch_size(b)
-            .m(m)
-            .k(k)
-            .n(n)
-            .iterations(3)
-            .TestF32();
-        }
-      }
-    }
-  }
+  BatchMatMulOperatorTester()
+    .batch_size(5)
+    .m(37)
+    .k(101)
+    .n(71)
+    .iterations(3)
+    .TestF32();
 }
 
 TEST(BATCH_MATRIX_MULTIPLY_NC_F32, unit_batch_transpose_b) {
@@ -84,16 +62,14 @@ TEST(BATCH_MATRIX_MULTIPLY_NC_F32, unit_batch_transpose_b) {
 }
 
 TEST(BATCH_MATRIX_MULTIPLY_NC_F32, small_batch_tranpose_b) {
-  for (size_t batch_size = 2; batch_size < 13; batch_size++) {
-    BatchMatMulOperatorTester()
-      .transpose_b(true)
-      .batch_size(batch_size)
-      .m(17)
-      .k(23)
-      .n(19)
-      .iterations(3)
-      .TestF32();
-  }
+  BatchMatMulOperatorTester()
+    .transpose_b(true)
+    .batch_size(5)
+    .m(17)
+    .k(23)
+    .n(19)
+    .iterations(3)
+    .TestF32();
 }
 
 TEST(BATCH_MATRIX_MULTIPLY_NC_F32, unit_batch_bigger_matrices_transpose_b) {
@@ -108,33 +84,12 @@ TEST(BATCH_MATRIX_MULTIPLY_NC_F32, unit_batch_bigger_matrices_transpose_b) {
 }
 
 TEST(BATCH_MATRIX_MULTIPLY_NC_F32, small_batch_bigger_matrices_transpose_b) {
-  for (size_t batch_size = 2; batch_size < 13; batch_size++) {
-    BatchMatMulOperatorTester()
-      .transpose_b(true)
-      .batch_size(batch_size)
-      .m(37)
-      .k(101)
-      .n(71)
-      .iterations(3)
-      .TestF32();
-  }
-}
-
-TEST(BATCH_MATRIX_MULTIPLY_NC_F32, various_batch_and_small_matrices_transpose_b) {
-  for (size_t b = 1; b < 11; b++) {
-    for (size_t m = 3; m < 27; m += 3) {
-      for (size_t k = 5; k < 30; k += 5) {
-        for (size_t n = 7; n < 49; n += 7) {
-          BatchMatMulOperatorTester()
-            .transpose_b(true)
-            .batch_size(b)
-            .m(m)
-            .k(k)
-            .n(n)
-            .iterations(3)
-            .TestF32();
-        }
-      }
-    }
-  }
+  BatchMatMulOperatorTester()
+    .transpose_b(true)
+    .batch_size(5)
+    .m(37)
+    .k(101)
+    .n(71)
+    .iterations(3)
+    .TestF32();
 }
