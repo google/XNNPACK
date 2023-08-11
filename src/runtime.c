3a1cc5161582d9f1cd9f777b16828e763e1406a5
Zhi An Ng <zhin@google.com>
2023-08-10 22:25:07 -0700

    Change workspace to be zero-allocated
    This eliminates a bunch of msan warnings due to reading uninitialized bias values. E.g. if NC < NR, then we will only pack NC bias, and skip NR-NC bytes ahead in packed weights. In GEMM microkernel we will read NR bias, and thus touch uninitialized values.
    
    In packed static weights, we use mmap with MMAP_ANON, which zero- initializes the packed weights workspace. So this is aligning both static/dynamic weights workspace.
    
    PiperOrigin-RevId: 555798230

--- src/runtime.c
+++ src/runtime.c
@@ -168,7 +168,7 @@ static enum xnn_status initialize_workspace_values(
   // Allocates larger workspace here if needed.
   if (runtime->workspace->size < mem_arena_size) {
     void* old_workspace_data = runtime->workspace->data;
-    void* new_workspace_data = xnn_allocate_simd_memory(mem_arena_size);
+    void* new_workspace_data = xnn_allocate_zero_simd_memory(mem_arena_size);
     if (new_workspace_data == NULL) {
       xnn_log_error("failed to allocate %zu bytes for runtime workspace", mem_arena_size);
       return xnn_status_out_of_memory;
