7d5c4e6c18cce5058de8b674121334782112c298
Marat Dukhan <maratek@google.com>
2023-08-25 19:13:24 -0700

    Enable AVX & AVX512F F32 RMINMAX microkernels
    PiperOrigin-RevId: 560251040

--- src/configs/reduce-config.c
+++ src/configs/reduce-config.c
@@ -87,10 +87,25 @@ static void init_f32_rminmax_config(void) {
       .element_tile = 16,
     };
   #elif XNN_ARCH_X86 || XNN_ARCH_X86_64
-    f32_rminmax_config = (struct xnn_reduce_config) {
-      .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__sse_u16_acc4,
-      .element_tile = 16,
-    };
+    const struct xnn_hardware_config* hardware_config = xnn_init_hardware_config();
+    assert(hardware_config != NULL);
+    if (!XNN_PLATFORM_MOBILE && hardware_config->use_x86_avx512f) {
+      f32_rminmax_config = (struct xnn_reduce_config) {
+        .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__avx512f_u64_acc4,
+        .element_tile = 64,
+      };
+    } else if (hardware_config->use_x86_avx) {
+      f32_rminmax_config = (struct xnn_reduce_config) {
+        .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__avx_u32_acc4,
+        .init.f32_default = xnn_init_f32_default_avx_params,
+        .element_tile = 32,
+      };
+    } else {
+      f32_rminmax_config = (struct xnn_reduce_config) {
+        .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__sse_u16_acc4,
+        .element_tile = 16,
+      };
+    }
   #elif XNN_ARCH_WASMSIMD || XNN_ARCH_WASMRELAXEDSIMD
     f32_rminmax_config = (struct xnn_reduce_config) {
       .ukernel = (xnn_reduce_ukernel_fn) xnn_f32_rminmax_ukernel__wasmsimd_minmax_u16_acc4,
