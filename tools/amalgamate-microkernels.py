e279de111b2e3bff631e6bd6eee8ef4d504f2a42
Marat Dukhan <maratek@google.com>
2023-09-12 16:42:37 -0700

    Refactor amalgamate-microkernels.py script
    - Make the amalgamation code re-usable
    - Prepare for further refactoring
    
    PiperOrigin-RevId: 564870820

--- tools/amalgamate-microkernels.py
+++ tools/amalgamate-microkernels.py
@@ -26,40 +26,25 @@ parser.add_argument("-o", "--output", metavar="FILE", required=True,
                     help='Output (C source) file')
 
 
-def main(args):
-  options = parser.parse_args(args)
+AMALGAMATION_HEADER = """\
+// Copyright 2021 Google LLC
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
 
-  build_path = os.path.join(ROOT_DIR, "..", "BUILD.bazel")
+"""
 
-  with codecs.open(build_path, "r", encoding="utf-8") as build_file:
-    build_text = build_file.read()
 
-  pattern = r"\b" + options.set + r"\b\s*=\s*\["
-  match = re.search(pattern, build_text)
-  if not match:
-    raise ValueError(
-      "Failed to find file set %s (regex \"%s\") inside the BUILD.bazel file" %
-        (options.set, pattern))
-
-  start_pos = match.end()
-  end_pos = build_text.find("]", start_pos)
-
-  fileset = [filename.strip()[1:-1] for filename in
-             build_text[start_pos:end_pos].split(",")]
-
+def amalgamate_microkernel_sources(source_paths, include_header):
   amalgam_lines = list()
   amalgam_includes = set()
-  for filename in sorted(fileset):
-    if not filename:
-      continue
-
-    filepath = os.path.join(ROOT_DIR, "..", filename)
-    with codecs.open(filepath, "r", encoding="utf-8") as file:
-      filelines = file.read().splitlines()
+  for filepath in sorted(source_paths):
+    with open(filepath, "r", encoding="utf-8") as file:
+      source_lines = file.read().splitlines()
 
     consumed_license = False
     consumed_includes = False
-    for line in filelines:
+    for line in source_lines:
       if line.startswith("//"):
         if not consumed_license:
           # Skip and generate a standard license header for amalgamated file
@@ -100,18 +85,12 @@ def main(args):
   amalgam_includes.discard("#include <xmmintrin.h>")
   amalgam_includes.discard("#include <wasm_simd128.h>")
 
-  amalgam_text = """\
-// Copyright 2021 Google LLC
-//
-// This source code is licensed under the BSD-style license found in the
-// LICENSE file in the root directory of this source tree.
-
-"""
+  amalgam_text = AMALGAMATION_HEADER
 
   amalgam_text += "\n".join(sorted(inc for inc in amalgam_includes if
                                    not inc.startswith("#include <xnnpack/")))
-  if options.include:
-    if options.include == "xopintrin.h":
+  if include_header:
+    if include_header == "xopintrin.h":
       amalgam_text += "\n\n"
       amalgam_text += "#ifdef _MSC_VER\n"
       amalgam_text += "  #include <intrin.h>\n"
@@ -119,7 +98,7 @@ def main(args):
       amalgam_text += "  #include <x86intrin.h>\n"
       amalgam_text += "#endif\n\n"
     else:
-      amalgam_text += "\n\n#include <%s>\n\n" % options.include
+      amalgam_text += "\n\n#include <%s>\n\n" % include_header
   else:
     amalgam_text += "\n\n"
   amalgam_text += "\n".join(sorted(inc for inc in amalgam_includes if
@@ -127,6 +106,33 @@ def main(args):
   amalgam_text += "\n\n\n"
   amalgam_text += "\n".join(amalgam_lines)
 
+  return amalgam_text
+
+def main(args):
+  options = parser.parse_args(args)
+
+  build_path = os.path.join(ROOT_DIR, "..", "BUILD.bazel")
+
+  with codecs.open(build_path, "r", encoding="utf-8") as build_file:
+    build_text = build_file.read()
+
+  pattern = r"\b" + options.set + r"\b\s*=\s*\["
+  match = re.search(pattern, build_text)
+  if not match:
+    raise ValueError(
+      "Failed to find file set %s (regex \"%s\") inside the BUILD.bazel file" %
+        (options.set, pattern))
+
+  start_pos = match.end()
+  end_pos = build_text.find("]", start_pos)
+
+  filenames = [filename.strip()[1:-1] for filename in
+               build_text[start_pos:end_pos].split(",")]
+
+  filepaths = [os.path.join(ROOT_DIR, "..", filename)
+               for filename in filenames if filename]
+
+  amalgam_text = amalgamate_microkernel_sources(filepaths, options.include)
 
   txt_changed = True
   if os.path.exists(options.output):
