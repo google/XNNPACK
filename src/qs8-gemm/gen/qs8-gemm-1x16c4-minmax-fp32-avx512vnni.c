8ca24dca8755d14d97d7d581357602a2e6b05e04
Quentin Khan <qkhan@google.com>
2023-10-02 06:37:37 -0700

    Fix loading of shuffle_control_mask in AVX512 VNNI QS8 GEMM microkernels.
    PiperOrigin-RevId: 570049668

--- src/qs8-gemm/gen/qs8-gemm-1x16c4-minmax-fp32-avx512vnni.c
+++ src/qs8-gemm/gen/qs8-gemm-1x16c4-minmax-fp32-avx512vnni.c
@@ -47,6 +47,7 @@ void xnn_qs8_gemm_minmax_fp32_ukernel_1x16c4__avx512vnni(
   const __m512 voutput_max_less_zero_point = _mm512_load_ps(params->fp32_avx512vnni.output_max_less_zero_point);
   const __m256i voutput_zero_point = _mm256_load_si256((const __m256i*) params->fp32_avx512vnni.output_zero_point);
   const __m128i voutput_min = _mm_load_si128((const __m128i*) params->fp32_avx512vnni.output_min);
+  const __m128i vshuffle_control_mask = _mm_loadu_si128((const __m128i*) params->fp32_avx512vnni.shuffle_control_mask);
   do {
     __m512i vacc0x0123456789ABCDEF = _mm512_load_epi32(w);
     w = (const int32_t*) w + 16;
@@ -81,7 +82,7 @@ void xnn_qs8_gemm_minmax_fp32_ukernel_1x16c4__avx512vnni(
 
     const __m128i vout0x012389AB4567CDEF = _mm_packs_epi16(_mm256_castsi256_si128(vacc0x012389AB4567CDEF), _mm256_extracti128_si256(vacc0x012389AB4567CDEF, 1));
 
-    __m128i vout0x0123456789ABCDEF = _mm_shuffle_epi8(vout0x012389AB4567CDEF, _mm_loadu_epi8(params->fp32_avx512vnni.shuffle_control_mask));
+    __m128i vout0x0123456789ABCDEF = _mm_shuffle_epi8(vout0x012389AB4567CDEF, vshuffle_control_mask);
 
     vout0x0123456789ABCDEF = _mm_max_epi8(vout0x0123456789ABCDEF, voutput_min);
 
