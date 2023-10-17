bdf237fe9c3d7510d755b77ddd6ad2fd3ab9f075
Frank Barchard <fbarchard@google.com>
2023-10-16 18:02:50 -0700

    Disable flakey test
    PiperOrigin-RevId: 573983300

--- test/fully-connected.cc
+++ test/fully-connected.cc
@@ -2145,9 +2145,7 @@ TEST_F(FullyConnectedTestQD8F32QC4W, define)
   ASSERT_EQ(node->flags, 0);
 }
 
-// DISABLED_ due to increased flakiness, likely due to legacy/tester requiring rounding
-// TODO: b/304931463 - Remove round up input/output channels and reenable
-TEST_F(FullyConnectedTestQD8F32QC4W, DISABLED_internally_allocated_dynamic_quantization_parameters)
+TEST_F(FullyConnectedTestQD8F32QC4W, internally_allocated_dynamic_quantization_parameters)
 {
   xnn_subgraph_t subgraph = nullptr;
   ASSERT_EQ(xnn_status_success, xnn_create_subgraph(/*external_value_ids=*/4, /*flags=*/0, &subgraph));
