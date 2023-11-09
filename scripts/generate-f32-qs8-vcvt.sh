1881b6cb2b160f1507f60bc79b6933e13f77e7e2
Alan Kelly <alankelly@google.com>
2023-11-09 03:59:15 -0800

    Merge generate-f16-qs8 and generate-f32-qs8 as they use common input files
    PiperOrigin-RevId: 580852251

--- scripts/generate-f32-qs8-vcvt.sh
+++ scripts/generate-f32-qs8-vcvt.sh
@@ -5,6 +5,12 @@
 # LICENSE file in the root directory of this source tree.
 
 ################################## ARM NEON ###################################
+tools/xngen src/f16-qs8-vcvt/neonfp16arith.c.in -D BATCH_TILE=8  -D DATATYPE=QS8 -o src/f16-qs8-vcvt/gen/f16-qs8-vcvt-neonfp16arith-u8.c &
+tools/xngen src/f16-qs8-vcvt/neonfp16arith.c.in -D BATCH_TILE=16 -D DATATYPE=QS8 -o src/f16-qs8-vcvt/gen/f16-qs8-vcvt-neonfp16arith-u16.c &
+tools/xngen src/f16-qs8-vcvt/neonfp16arith.c.in -D BATCH_TILE=24 -D DATATYPE=QS8 -o src/f16-qs8-vcvt/gen/f16-qs8-vcvt-neonfp16arith-u24.c &
+tools/xngen src/f16-qs8-vcvt/neonfp16arith.c.in -D BATCH_TILE=32 -D DATATYPE=QS8 -o src/f16-qs8-vcvt/gen/f16-qs8-vcvt-neonfp16arith-u32.c &
+tools/xngen src/f16-qs8-vcvt/neonfp16arith.c.in -D BATCH_TILE=64 -D DATATYPE=QS8 -o src/f16-qs8-vcvt/gen/f16-qs8-vcvt-neonfp16arith-u64.c &
+
 tools/xngen src/f32-qs8-vcvt/neon.c.in -D BATCH_TILE=8  -D DATATYPE=QS8 -o src/f32-qs8-vcvt/gen/f32-qs8-vcvt-neon-u8.c &
 tools/xngen src/f32-qs8-vcvt/neon.c.in -D BATCH_TILE=16 -D DATATYPE=QS8 -o src/f32-qs8-vcvt/gen/f32-qs8-vcvt-neon-u16.c &
 tools/xngen src/f32-qs8-vcvt/neon.c.in -D BATCH_TILE=24 -D DATATYPE=QS8 -o src/f32-qs8-vcvt/gen/f32-qs8-vcvt-neon-u24.c &
@@ -106,6 +112,11 @@ tools/xngen src/f32-qs8-vcvt/scalar-fmagic.c.in -D BATCH_TILE=3 -D IDATATYPE=F32
 tools/xngen src/f32-qs8-vcvt/scalar-fmagic.c.in -D BATCH_TILE=4 -D IDATATYPE=F32 -D ODATATYPE=QU8 -D WASM=1 -o src/f32-qu8-vcvt/gen/f32-qu8-vcvt-wasm-fmagic-u4.c &
 
 #################################### Scalar ###################################
+tools/xngen src/f32-qs8-vcvt/scalar-fmagic.c.in -D BATCH_TILE=1 -D IDATATYPE=F16 -D ODATATYPE=QS8 -D WASM=0 -o src/f16-qs8-vcvt/gen/f16-qs8-vcvt-scalar-fmagic-u1.c &
+tools/xngen src/f32-qs8-vcvt/scalar-fmagic.c.in -D BATCH_TILE=2 -D IDATATYPE=F16 -D ODATATYPE=QS8 -D WASM=0 -o src/f16-qs8-vcvt/gen/f16-qs8-vcvt-scalar-fmagic-u2.c &
+tools/xngen src/f32-qs8-vcvt/scalar-fmagic.c.in -D BATCH_TILE=3 -D IDATATYPE=F16 -D ODATATYPE=QS8 -D WASM=0 -o src/f16-qs8-vcvt/gen/f16-qs8-vcvt-scalar-fmagic-u3.c &
+tools/xngen src/f32-qs8-vcvt/scalar-fmagic.c.in -D BATCH_TILE=4 -D IDATATYPE=F16 -D ODATATYPE=QS8 -D WASM=0 -o src/f16-qs8-vcvt/gen/f16-qs8-vcvt-scalar-fmagic-u4.c &
+
 tools/xngen src/f32-qs8-vcvt/scalar-fmagic.c.in -D BATCH_TILE=1 -D IDATATYPE=F32 -D ODATATYPE=QS8 -D WASM=0 -o src/f32-qs8-vcvt/gen/f32-qs8-vcvt-scalar-fmagic-u1.c &
 tools/xngen src/f32-qs8-vcvt/scalar-fmagic.c.in -D BATCH_TILE=2 -D IDATATYPE=F32 -D ODATATYPE=QS8 -D WASM=0 -o src/f32-qs8-vcvt/gen/f32-qs8-vcvt-scalar-fmagic-u2.c &
 tools/xngen src/f32-qs8-vcvt/scalar-fmagic.c.in -D BATCH_TILE=3 -D IDATATYPE=F32 -D ODATATYPE=QS8 -D WASM=0 -o src/f32-qs8-vcvt/gen/f32-qs8-vcvt-scalar-fmagic-u3.c &
@@ -115,6 +126,11 @@ tools/xngen src/f32-qs8-vcvt/scalar-fmagic.c.in -D BATCH_TILE=1 -D IDATATYPE=F32
 tools/xngen src/f32-qs8-vcvt/scalar-fmagic.c.in -D BATCH_TILE=2 -D IDATATYPE=F32 -D ODATATYPE=QU8 -D WASM=0 -o src/f32-qu8-vcvt/gen/f32-qu8-vcvt-scalar-fmagic-u2.c &
 tools/xngen src/f32-qs8-vcvt/scalar-fmagic.c.in -D BATCH_TILE=3 -D IDATATYPE=F32 -D ODATATYPE=QU8 -D WASM=0 -o src/f32-qu8-vcvt/gen/f32-qu8-vcvt-scalar-fmagic-u3.c &
 tools/xngen src/f32-qs8-vcvt/scalar-fmagic.c.in -D BATCH_TILE=4 -D IDATATYPE=F32 -D ODATATYPE=QU8 -D WASM=0 -o src/f32-qu8-vcvt/gen/f32-qu8-vcvt-scalar-fmagic-u4.c &
+
+tools/xngen src/f32-qs8-vcvt/scalar-imagic.c.in -D BATCH_TILE=1 -D IDATATYPE=F16 -D ODATATYPE=QS8 -D WASM=0 -o src/f16-qs8-vcvt/gen/f16-qs8-vcvt-scalar-imagic-u1.c &
+tools/xngen src/f32-qs8-vcvt/scalar-imagic.c.in -D BATCH_TILE=2 -D IDATATYPE=F16 -D ODATATYPE=QS8 -D WASM=0 -o src/f16-qs8-vcvt/gen/f16-qs8-vcvt-scalar-imagic-u2.c &
+tools/xngen src/f32-qs8-vcvt/scalar-imagic.c.in -D BATCH_TILE=3 -D IDATATYPE=F16 -D ODATATYPE=QS8 -D WASM=0 -o src/f16-qs8-vcvt/gen/f16-qs8-vcvt-scalar-imagic-u3.c &
+tools/xngen src/f32-qs8-vcvt/scalar-imagic.c.in -D BATCH_TILE=4 -D IDATATYPE=F16 -D ODATATYPE=QS8 -D WASM=0 -o src/f16-qs8-vcvt/gen/f16-qs8-vcvt-scalar-imagic-u4.c &
 
 tools/xngen src/f32-qs8-vcvt/scalar-imagic.c.in -D BATCH_TILE=1 -D DATATYPE=QS8 -o src/f32-qs8-vcvt/gen/f32-qs8-vcvt-scalar-imagic-u1.c &
 tools/xngen src/f32-qs8-vcvt/scalar-imagic.c.in -D BATCH_TILE=2 -D DATATYPE=QS8 -o src/f32-qs8-vcvt/gen/f32-qs8-vcvt-scalar-imagic-u2.c &
