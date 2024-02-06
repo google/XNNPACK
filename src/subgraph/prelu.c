1941d731d25cc7cfa2fc17b3e9847211d50aae17
Gregory Comer <gregoryjcomer@gmail.com>
2024-02-06 05:50:28 -0800

    Add fp16 support for prelu nodes

--- src/subgraph/prelu.c
+++ src/subgraph/prelu.c
@@ -176,6 +176,7 @@ enum xnn_status xnn_define_prelu(
   }
 
   switch (input_value->datatype) {
+    case xnn_datatype_fp16:
     case xnn_datatype_fp32:
       break;
     default:
@@ -230,8 +231,13 @@ enum xnn_status xnn_define_prelu(
     return status;
   }
 
+  enum xnn_compute_type compute_type = xnn_compute_type_invalid;
   switch (output_value->datatype) {
+    case xnn_datatype_fp16:
+      compute_type = xnn_compute_type_fp16;
+      break;
     case xnn_datatype_fp32:
+      compute_type = xnn_compute_type_fp32;
       break;
     default:
       xnn_log_error(
@@ -247,7 +253,7 @@ enum xnn_status xnn_define_prelu(
   }
 
   node->type = xnn_node_type_prelu;
-  node->compute_type = xnn_compute_type_fp32;
+  node->compute_type = compute_type;
   node->num_inputs = 2;
   node->inputs[0] = input_id;
   node->inputs[1] = slope_id;
