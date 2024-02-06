a4e717d6a2ed1f3f08c74747065c3bd8849a7f1c
Gregory Comer <gregoryjcomer@gmail.com>
2024-02-06 05:55:19 -0800

    Add fp16 support for space-to-depth-2d nodes

--- test/space-to-depth-2d.cc
+++ test/space-to-depth-2d.cc
@@ -12,6 +12,7 @@
 #include <random>
 #include <vector>
 
+#include <fp16/fp16.h>
 #include <gtest/gtest.h>
 
 #include <xnnpack.h>
@@ -23,6 +24,7 @@
 
 using SpaceToDepth2DTestQS8 = UnaryTest<int8_t, int8_t, /*min_dim=*/4, /*max_dim=*/4>;
 using SpaceToDepth2DTestQU8 = UnaryTest<uint8_t, uint8_t, /*min_dim=*/4, /*max_dim=*/4>;
+using SpaceToDepth2DTestF16 = UnaryTest<uint16_t, uint16_t, /*min_dim=*/4, /*max_dim=*/4>;
 using SpaceToDepth2DTestF32 = UnaryTest<float, float, /*min_dim=*/4, /*max_dim=*/4>;
 
 TEST_F(SpaceToDepth2DTestQS8, define)
@@ -113,6 +115,49 @@ TEST_F(SpaceToDepth2DTestQU8, define)
   const struct xnn_node* node = &subgraph->nodes[0];
   ASSERT_EQ(node->type, xnn_node_type_space_to_depth_2d);
   ASSERT_EQ(node->compute_type, xnn_compute_type_qu8);
+  ASSERT_EQ(node->params.space_to_depth_2d.block_size, block_size);
+  ASSERT_EQ(node->num_inputs, 1);
+  ASSERT_EQ(node->inputs[0], input_id);
+  ASSERT_EQ(node->num_outputs, 1);
+  ASSERT_EQ(node->outputs[0], output_id);
+  ASSERT_EQ(node->flags, 0);
+}
+
+TEST_F(SpaceToDepth2DTestF16, define)
+{
+  const size_t block_size = 2 + (u8dist(rng) % 13);
+  std::vector<size_t> output_dims = dims;
+  output_dims[3] *= block_size * block_size;
+  dims[1] *= block_size;
+  dims[2] *= block_size;
+
+  ASSERT_EQ(xnn_status_success, xnn_initialize(/*allocator=*/nullptr));
+  xnn_subgraph_t subgraph = nullptr;
+  ASSERT_EQ(xnn_status_success, xnn_create_subgraph(/*external_value_ids=*/2, /*flags=*/0, &subgraph));
+  std::unique_ptr<xnn_subgraph, decltype(&xnn_delete_subgraph)> auto_subgraph(subgraph, xnn_delete_subgraph);
+
+  input_id = XNN_INVALID_NODE_ID;
+  ASSERT_EQ(
+    xnn_status_success, xnn_define_tensor_value(
+                          subgraph, xnn_datatype_fp16, dims.size(), dims.data(), nullptr, 0,
+                          /*flags=*/XNN_VALUE_FLAG_EXTERNAL_INPUT, &input_id));
+  ASSERT_NE(input_id, XNN_INVALID_NODE_ID);
+
+  output_id = XNN_INVALID_NODE_ID;
+  ASSERT_EQ(
+    xnn_status_success, xnn_define_tensor_value(
+                          subgraph, xnn_datatype_fp16, output_dims.size(), output_dims.data(), nullptr, 1,
+                          /*flags=*/XNN_VALUE_FLAG_EXTERNAL_OUTPUT, &output_id));
+  ASSERT_NE(output_id, XNN_INVALID_NODE_ID);
+
+  ASSERT_EQ(
+    xnn_status_success,
+    xnn_define_space_to_depth_2d(subgraph, block_size, input_id, output_id, /*flags=*/0));
+
+  ASSERT_EQ(subgraph->num_nodes, 1);
+  const struct xnn_node* node = &subgraph->nodes[0];
+  ASSERT_EQ(node->type, xnn_node_type_space_to_depth_2d);
+  ASSERT_EQ(node->compute_type, xnn_compute_type_fp16);
   ASSERT_EQ(node->params.space_to_depth_2d.block_size, block_size);
   ASSERT_EQ(node->num_inputs, 1);
   ASSERT_EQ(node->inputs[0], input_id);
@@ -303,6 +348,79 @@ TEST_F(SpaceToDepth2DTestQU8, matches_operator_api)
                           subgraph, xnn_datatype_quint8, output_zero_point, output_scale, dims.size(),
                           output_dims.data(), nullptr, /*external_id=*/1, /*flags=*/XNN_VALUE_FLAG_EXTERNAL_OUTPUT,
                           &output_id));
+  ASSERT_NE(output_id, XNN_INVALID_NODE_ID);
+
+  ASSERT_EQ(
+    xnn_status_success,
+    xnn_define_space_to_depth_2d(subgraph, block_size, input_id, output_id, /*flags=*/0));
+
+  xnn_runtime_t runtime = nullptr;
+  ASSERT_EQ(xnn_status_success, xnn_create_runtime_v3(subgraph, nullptr, nullptr, /*flags=*/0, &runtime));
+  ASSERT_NE(nullptr, runtime);
+  std::unique_ptr<xnn_runtime, decltype(&xnn_delete_runtime)> auto_runtime(runtime, xnn_delete_runtime);
+
+  std::array<xnn_external_value, 2> external = {
+    xnn_external_value{input_id, input.data()}, xnn_external_value{output_id, subgraph_output.data()}};
+  ASSERT_EQ(xnn_status_success, xnn_setup_runtime(runtime, external.size(), external.data()));
+  ASSERT_EQ(xnn_status_success, xnn_invoke_runtime(runtime));
+
+  EXPECT_EQ(subgraph_output, operator_output);
+}
+
+TEST_F(SpaceToDepth2DTestF16, matches_operator_api)
+{
+  std::vector<size_t> output_dims = dims;
+  const size_t block_size = 2 + (u8dist(rng) % 11);
+  output_dims[3] *= block_size * block_size;
+  dims[1] *= block_size;
+  dims[2] *= block_size;
+  AllocateInputsAndOutputs();
+
+  size_t input_channels = dims[3];
+
+  size_t input_height = dims[1];
+  size_t input_width = dims[2];
+  size_t batch_size = dims[0];
+
+  std::generate(input.begin(), input.end(), [&]() { return fp16_ieee_from_fp32_value(f32dist(rng)); });
+  std::fill(operator_output.begin(), operator_output.end(), fp16_ieee_from_fp32_value(nanf("")));
+  std::fill(subgraph_output.begin(), subgraph_output.end(), fp16_ieee_from_fp32_value(nanf("")));
+
+  // Call operator API.
+  ASSERT_EQ(xnn_status_success, xnn_initialize(/*allocator=*/nullptr));
+  xnn_operator_t op = nullptr;
+  const xnn_status status = xnn_create_space_to_depth_nhwc_x16(block_size, /*flags=*/0, &op);
+  if (status == xnn_status_unsupported_hardware) {
+    GTEST_SKIP();
+  }
+  ASSERT_EQ(xnn_status_success, status);
+  ASSERT_NE(nullptr, op);
+  std::unique_ptr<xnn_operator, decltype(&xnn_delete_operator)> auto_op(op, xnn_delete_operator);
+
+  ASSERT_EQ(
+    xnn_status_success, xnn_reshape_space_to_depth_nhwc_x16(
+                          op, batch_size, input_height, input_width, input_channels,
+                          /*output_height_out=*/nullptr, /*output_width_out=*/nullptr, /*output_channels_out=*/nullptr,
+                          /*threadpool=*/nullptr));
+  ASSERT_EQ(xnn_status_success, xnn_setup_space_to_depth_nhwc_x16(op, input.data(), operator_output.data()));
+  ASSERT_EQ(xnn_status_success, xnn_run_operator(op, /*threadpool=*/nullptr));
+
+  // Call subgraph API.
+  xnn_subgraph_t subgraph = nullptr;
+  ASSERT_EQ(xnn_status_success, xnn_create_subgraph(/*external_value_ids=*/2, /*flags=*/0, &subgraph));
+  std::unique_ptr<xnn_subgraph, decltype(&xnn_delete_subgraph)> auto_subgraph(subgraph, xnn_delete_subgraph);
+  input_id = XNN_INVALID_NODE_ID;
+  ASSERT_EQ(
+    xnn_status_success, xnn_define_tensor_value(
+                          subgraph, xnn_datatype_fp16, dims.size(), dims.data(),
+                          nullptr, /*external_id=*/0, /*flags=*/XNN_VALUE_FLAG_EXTERNAL_INPUT, &input_id));
+  ASSERT_NE(input_id, XNN_INVALID_NODE_ID);
+
+  output_id = XNN_INVALID_NODE_ID;
+  ASSERT_EQ(
+    xnn_status_success, xnn_define_tensor_value(
+                          subgraph, xnn_datatype_fp16, dims.size(), output_dims.data(),
+                          nullptr, /*external_id=*/1, /*flags=*/XNN_VALUE_FLAG_EXTERNAL_OUTPUT, &output_id));
   ASSERT_NE(output_id, XNN_INVALID_NODE_ID);
 
   ASSERT_EQ(
