dc6614d0dd8c351ba8f9a5806cecaf6be11dadcf
Gregory Comer <gregoryjcomer@gmail.com>
2024-02-06 05:22:09 -0800

    Add fp16 support for depth-to-space-2d nodes

--- src/subgraph/depth-to-space-2d.c
+++ src/subgraph/depth-to-space-2d.c
@@ -256,6 +256,7 @@ enum xnn_status xnn_define_depth_to_space_2d(
   }
 
   switch (input_value->datatype) {
+    case xnn_datatype_fp16:
     case xnn_datatype_fp32:
     case xnn_datatype_qint8:
     case xnn_datatype_quint8:
@@ -281,6 +282,9 @@ enum xnn_status xnn_define_depth_to_space_2d(
 
   enum xnn_compute_type compute_type = xnn_compute_type_invalid;
   switch (output_value->datatype) {
+    case xnn_datatype_fp16:
+      compute_type = xnn_compute_type_fp16;
+      break;
     case xnn_datatype_fp32:
       compute_type = xnn_compute_type_fp32;
       break;
