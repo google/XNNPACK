2c58fe5c4997925019d6f2616897f3b7fdcfa0ab
Linkun Chen <lkchen@google.com>
2024-02-09 08:08:18 -0800

    Fix xnn_define_static_mean comment
    PiperOrigin-RevId: 605632620

--- include/xnnpack.h
+++ include/xnnpack.h
@@ -1156,8 +1156,9 @@ enum xnn_status xnn_define_static_constant_pad(
 /// @param input_id - Value ID for the input tensor. The input tensor must be a dense tensor with at least
 ///                   @a num_reduction_axes dimensions defined in the @a subgraph.
 /// @param output_id - Value ID for the output tensor. The output tensor must be a dense tensor defined in the
-///                    @a subgraph with @a num_reduction_axes fewer dimensions than the input tensor.
-/// @param flags - binary features of the Mean Node. No supported flags are currently defined.
+///                    @a subgraph with @a num_reduction_axes fewer dimensions than the input tensor (if
+///                    XNN_FLAG_KEEP_DIMS is not specified), or has same dimension rank but the dimension at
+///                    @a reduction_axes reduced to 1 (if XNN_FLAG_KEEP_DIMS is specified).
 /// @param flags - binary features of the Mean Node. The only currently supported value is XNN_FLAG_KEEP_DIMS
 enum xnn_status xnn_define_static_mean(
   xnn_subgraph_t subgraph,
