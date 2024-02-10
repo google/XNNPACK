23f8961eb534491e42b73da96ec37950243cbde9
Max Ren <maxren@meta.com>
2024-02-09 16:24:20 -0800

    Reshape Conv2d Output

--- src/subgraph/convolution-2d.c
+++ src/subgraph/convolution-2d.c
@@ -345,108 +345,143 @@ static enum xnn_status reshape_convolution_operator(
 {
   const uint32_t input_id = opdata->inputs[0];
   assert(input_id < num_values);
+
+  const uint32_t output_id = opdata->outputs[0];
+  assert(output_id < num_values);
+
   const size_t batch_size = values[input_id].shape.dim[0];
   const size_t input_height = values[input_id].shape.dim[1];
   const size_t input_width = values[input_id].shape.dim[2];
+
+  size_t output_height, output_width;
+  enum xnn_status status = xnn_status_invalid_state;
+  const size_t old_workspace_size = opdata->workspace_size;
   switch (opdata->operator_objects[0]->type) {
     case xnn_operator_type_convolution_nchw_f16:
-      return xnn_reshape_convolution2d_nchw_f16(
+      status = xnn_reshape_convolution2d_nchw_f16(
         opdata->operator_objects[0],
         batch_size,
         input_height,
         input_width,
-        /*output_height_out=*/NULL, /*output_width_out=*/NULL,
+        &output_height,
+        &output_width,
         threadpool);
       break;
     case xnn_operator_type_convolution_nchw_f32:
-      return xnn_reshape_convolution2d_nchw_f32(
+      status = xnn_reshape_convolution2d_nchw_f32(
         opdata->operator_objects[0],
         batch_size,
         input_height,
         input_width,
-        /*output_height_out=*/NULL, /*output_width_out=*/NULL,
+        &output_height,
+        &output_width,
         threadpool);
       break;
     case xnn_operator_type_convolution_nhwc_f32:
-      return xnn_reshape_convolution2d_nhwc_f32(
+      status = xnn_reshape_convolution2d_nhwc_f32(
         opdata->operator_objects[0],
         batch_size,
         input_height,
         input_width,
         &opdata->workspace_size,
         &opdata->workspace_alignment,
-        /*output_height_out=*/NULL, /*output_width_out=*/NULL,
+        &output_height,
+        &output_width,
         threadpool);
       break;
     case xnn_operator_type_convolution_nhwc_f16:
-      return xnn_reshape_convolution2d_nhwc_f16(
+      status = xnn_reshape_convolution2d_nhwc_f16(
         opdata->operator_objects[0],
         batch_size,
         input_height,
         input_width,
         &opdata->workspace_size,
         &opdata->workspace_alignment,
-        /*output_height_out=*/NULL, /*output_width_out=*/NULL,
+        &output_height,
+        &output_width,
         threadpool);
       break;
     case xnn_operator_type_convolution_nhwc_qd8_f16_qc8w:
-      return xnn_reshape_convolution2d_nhwc_qd8_f16_qc8w(
+      status = xnn_reshape_convolution2d_nhwc_qd8_f16_qc8w(
         opdata->operator_objects[0],
         batch_size,
         input_height,
         input_width,
         &opdata->workspace_size,
         &opdata->workspace_alignment,
-        /*output_height_out=*/NULL, /*output_width_out=*/NULL,
+        &output_height,
+        &output_width,
         threadpool);
       break;
     case xnn_operator_type_convolution_nhwc_qd8_f32_qc8w:
-      return xnn_reshape_convolution2d_nhwc_qd8_f32_qc8w(
+      status = xnn_reshape_convolution2d_nhwc_qd8_f32_qc8w(
         opdata->operator_objects[0],
         batch_size,
         input_height,
         input_width,
         &opdata->workspace_size,
         &opdata->workspace_alignment,
-        /*output_height_out=*/NULL, /*output_width_out=*/NULL,
+        &output_height,
+        &output_width,
         threadpool);
       break;
     case xnn_operator_type_convolution_nhwc_qc8:
-      return xnn_reshape_convolution2d_nhwc_qs8_qc8w(
+      status = xnn_reshape_convolution2d_nhwc_qs8_qc8w(
         opdata->operator_objects[0],
         batch_size,
         input_height,
         input_width,
         &opdata->workspace_size,
         &opdata->workspace_alignment,
-        /*output_height_out=*/NULL, /*output_width_out=*/NULL,
+        &output_height,
+        &output_width,
         threadpool);
       break;
     case xnn_operator_type_convolution_nhwc_qs8:
-      return xnn_reshape_convolution2d_nhwc_qs8(
+      status = xnn_reshape_convolution2d_nhwc_qs8(
         opdata->operator_objects[0],
         batch_size,
         input_height,
         input_width,
         &opdata->workspace_size,
         &opdata->workspace_alignment,
-        /*output_height_out=*/NULL, /*output_width_out=*/NULL,
+        &output_height,
+        &output_width,
         threadpool);
       break;
     case xnn_operator_type_convolution_nhwc_qu8:
-      return xnn_reshape_convolution2d_nhwc_qu8(
+      status = xnn_reshape_convolution2d_nhwc_qu8(
         opdata->operator_objects[0],
         batch_size,
         input_height,
         input_width,
         &opdata->workspace_size,
         &opdata->workspace_alignment,
-        /*output_height_out=*/NULL, /*output_width_out=*/NULL,
+        &output_height,
+        &output_width,
         threadpool);
       break;
     default:
       XNN_UNREACHABLE;
   }
+
+  if (status != xnn_status_success) {
+    return status;
+  }
+
+  const size_t output_pixel_stride = opdata->operator_objects[0]->output_pixel_stride;
+  struct xnn_value* output_value = values + output_id;
+  output_value->shape.dim[0] = batch_size;
+  output_value->shape.dim[1] = output_height;
+  output_value->shape.dim[2] = output_width;
+  output_value->shape.dim[3] = output_pixel_stride;
+
+  const size_t new_size = xnn_tensor_get_size(output_value);
+  if (new_size > output_value->size || old_workspace_size > opdata->workspace_size) {
+    output_value->size = new_size;
+    return xnn_status_reallocation_required;
+  }
+  return xnn_status_success;
 }
 
 static enum xnn_status setup_convolution_operator(
