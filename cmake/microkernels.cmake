3eaa7ce0092d3d1527489a5d13378686d86e0d4e
Frank Barchard <fbarchard@google.com>
2024-02-23 10:50:26 -0800

    Remove source that is not generated
    - These source files are in the gen/ folder but not generated.
    
    PiperOrigin-RevId: 609775513

--- cmake/microkernels.cmake
+++ cmake/microkernels.cmake
@@ -1081,11 +1081,8 @@ SET(ALL_AVX2_MICROKERNEL_SRCS
   src/qs8-qc8w-dwconv/gen/qs8-qc8w-dwconv-25p32c-minmax-fp32-avx2-mul16-vpunpck.c
   src/qs8-qc8w-dwconv/gen/qs8-qc8w-dwconv-25p32c-minmax-fp32-avx2-mul32.c
   src/qs8-qc8w-gemm/gen/qs8-qc8w-gemm-1x8c8-minmax-fp32-avx2.c
-  src/qs8-qc8w-gemm/gen/qs8-qc8w-gemm-1x8c8-xw-minmax-fp32-avx2.c
   src/qs8-qc8w-gemm/gen/qs8-qc8w-gemm-2x8c8-minmax-fp32-avx2.c
-  src/qs8-qc8w-gemm/gen/qs8-qc8w-gemm-2x8c8-xw-minmax-fp32-avx2.c
   src/qs8-qc8w-gemm/gen/qs8-qc8w-gemm-3x8c8-minmax-fp32-avx2.c
-  src/qs8-qc8w-gemm/gen/qs8-qc8w-gemm-3x8c8-xw-minmax-fp32-avx2.c
   src/qs8-qc8w-gemm/gen/qs8-qc8w-gemm-4x8c8-minmax-fp32-avx2.c
   src/qs8-qc8w-igemm/gen/qs8-qc8w-igemm-1x8c8-minmax-fp32-avx2.c
   src/qs8-qc8w-igemm/gen/qs8-qc8w-igemm-2x8c8-minmax-fp32-avx2.c
@@ -2921,12 +2918,6 @@ SET(ALL_NEON_MICROKERNEL_SRCS
   src/f32-dwconv2d-chw/gen/f32-dwconv2d-chw-5x5s2p2-minmax-neon-2x4.c
   src/f32-dwconv2d-chw/gen/f32-dwconv2d-chw-5x5s2p2-minmax-neon-3x4-acc2.c
   src/f32-dwconv2d-chw/gen/f32-dwconv2d-chw-5x5s2p2-minmax-neon-3x4.c
-  src/f32-dwconv/gen/f32-dwconv-2f2m2l4c4s4r-minmax-neon-acc2.c
-  src/f32-dwconv/gen/f32-dwconv-2f2m2l4c4s4r-minmax-neon.c
-  src/f32-dwconv/gen/f32-dwconv-2f2m2l8c4s4r-minmax-neon-acc2.c
-  src/f32-dwconv/gen/f32-dwconv-2f2m2l8c4s4r-minmax-neon.c
-  src/f32-dwconv/gen/f32-dwconv-2f2m2l16c4s4r-minmax-neon-acc2.c
-  src/f32-dwconv/gen/f32-dwconv-2f2m2l16c4s4r-minmax-neon.c
   src/f32-dwconv/gen/f32-dwconv-3p4c-minmax-neon-acc2.c
   src/f32-dwconv/gen/f32-dwconv-3p4c-minmax-neon.c
   src/f32-dwconv/gen/f32-dwconv-3p8c-minmax-neon-acc2.c
@@ -4018,12 +4009,6 @@ SET(ALL_NEONFMA_MICROKERNEL_SRCS
   src/bf16-gemm/gen/bf16-gemm-4x4c8-minmax-neonfma-zip.c
   src/bf16-gemm/gen/bf16-gemm-5x4c8-minmax-neonfma-shland.c
   src/bf16-gemm/gen/bf16-gemm-5x4c8-minmax-neonfma-zip.c
-  src/f32-dwconv/gen/f32-dwconv-2f2m2l4c4s4r-minmax-neonfma-acc2.c
-  src/f32-dwconv/gen/f32-dwconv-2f2m2l4c4s4r-minmax-neonfma.c
-  src/f32-dwconv/gen/f32-dwconv-2f2m2l8c4s4r-minmax-neonfma-acc2.c
-  src/f32-dwconv/gen/f32-dwconv-2f2m2l8c4s4r-minmax-neonfma.c
-  src/f32-dwconv/gen/f32-dwconv-2f2m2l16c4s4r-minmax-neonfma-acc2.c
-  src/f32-dwconv/gen/f32-dwconv-2f2m2l16c4s4r-minmax-neonfma.c
   src/f32-dwconv/gen/f32-dwconv-3p4c-minmax-neonfma-acc2.c
   src/f32-dwconv/gen/f32-dwconv-3p4c-minmax-neonfma.c
   src/f32-dwconv/gen/f32-dwconv-3p8c-minmax-neonfma-acc2.c
