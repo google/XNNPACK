0f5a2d973bccfd0df6c081f404be49bd0f3f6bf1
Frank Barchard <fbarchard@google.com>
2024-04-09 13:45:15 -0700

    Enable AVX512 and AVX2 F32_RADDSTOREEXPMINUSMAX microkernels
    - Use AVX2/AVX512 kernels from the f32-softmax benchmark
    
    PiperOrigin-RevId: 623269579

--- src/amalgam/gen/sse2.c
+++ src/amalgam/gen/sse2.c
@@ -1623,7 +1623,7 @@ void xnn_f32_qu8_vcvt_ukernel__sse2_u32(
   }
 }
 
-void xnn_f32_raddstoreexpminusmax_ukernel__sse2_rr2_p5_u20_acc2(
+void xnn_f32_raddstoreexpminusmax_ukernel__sse2_rr2_p5_u16(
     size_t batch,
     const float* input,
     const float* max,
@@ -1651,29 +1651,25 @@ void xnn_f32_raddstoreexpminusmax_ukernel__sse2_rr2_p5_u20_acc2(
   const __m128 vdenorm_cutoff = _mm_load_ps(params->sse2_rr2_p5.denorm_cutoff);
 
   __m128 vacc0 = _mm_setzero_ps();
-  __m128 vacc1 = _mm_setzero_ps();
-  for (; batch >= 20 * sizeof(float); batch -= 20 * sizeof(float)) {
-    // Load 20 (5x4) inputs at a time.
+  for (; batch >= 16 * sizeof(float); batch -= 16 * sizeof(float)) {
+    // Load 16 (4x4) inputs at a time.
     const __m128 vi0123 = _mm_loadu_ps(input);
     const __m128 vi4567 = _mm_loadu_ps(input + 4);
     const __m128 vi89AB = _mm_loadu_ps(input + 8);
     const __m128 viCDEF = _mm_loadu_ps(input + 12);
-    const __m128 viGHIJ = _mm_loadu_ps(input + 16);
-    input += 20;
+    input += 16;
 
     // Subtract maximum input x := i - i_max. This implies x <= 0.
     const __m128 vx0123 = _mm_sub_ps(vi0123, vi_max);
     const __m128 vx4567 = _mm_sub_ps(vi4567, vi_max);
     const __m128 vx89AB = _mm_sub_ps(vi89AB, vi_max);
     const __m128 vxCDEF = _mm_sub_ps(viCDEF, vi_max);
-    const __m128 vxGHIJ = _mm_sub_ps(viGHIJ, vi_max);
 
     // Compute reduced argument batch := round(x / log(2)).
     __m128 vn0123 = _mm_add_ps(_mm_mul_ps(vx0123, vlog2e), vmagic_bias);
     __m128 vn4567 = _mm_add_ps(_mm_mul_ps(vx4567, vlog2e), vmagic_bias);
     __m128 vn89AB = _mm_add_ps(_mm_mul_ps(vx89AB, vlog2e), vmagic_bias);
     __m128 vnCDEF = _mm_add_ps(_mm_mul_ps(vxCDEF, vlog2e), vmagic_bias);
-    __m128 vnGHIJ = _mm_add_ps(_mm_mul_ps(vxGHIJ, vlog2e), vmagic_bias);
 
     // Create a floating-point number s (scale) such that s == 2**batch for inputs which don't cause underflow, i.e.
     // -87.33642 <= x <= 0.0, and -126 <= batch <= 0 accordingly.
@@ -1681,14 +1677,12 @@ void xnn_f32_raddstoreexpminusmax_ukernel__sse2_rr2_p5_u20_acc2(
     const __m128 vs4567 = _mm_castsi128_ps(_mm_slli_epi32(_mm_castps_si128(vn4567), 23));
     const __m128 vs89AB = _mm_castsi128_ps(_mm_slli_epi32(_mm_castps_si128(vn89AB), 23));
     const __m128 vsCDEF = _mm_castsi128_ps(_mm_slli_epi32(_mm_castps_si128(vnCDEF), 23));
-    const __m128 vsGHIJ = _mm_castsi128_ps(_mm_slli_epi32(_mm_castps_si128(vnGHIJ), 23));
 
     // Subtract the large number back to get final batch := round(x / log(2)).
     vn0123 = _mm_sub_ps(vn0123, vmagic_bias);
     vn4567 = _mm_sub_ps(vn4567, vmagic_bias);
     vn89AB = _mm_sub_ps(vn89AB, vmagic_bias);
     vnCDEF = _mm_sub_ps(vnCDEF, vmagic_bias);
-    vnGHIJ = _mm_sub_ps(vnGHIJ, vmagic_bias);
 
     // Compute reduced argument t := x - batch * log(2).
     // Use Cody-Waite range reduction method (note two constants to represent log(2)) to improve accuracy.
@@ -1696,38 +1690,32 @@ void xnn_f32_raddstoreexpminusmax_ukernel__sse2_rr2_p5_u20_acc2(
     __m128 vt4567 = _mm_add_ps(_mm_mul_ps(vn4567, vminus_ln2_hi), vx4567);
     __m128 vt89AB = _mm_add_ps(_mm_mul_ps(vn89AB, vminus_ln2_hi), vx89AB);
     __m128 vtCDEF = _mm_add_ps(_mm_mul_ps(vnCDEF, vminus_ln2_hi), vxCDEF);
-    __m128 vtGHIJ = _mm_add_ps(_mm_mul_ps(vnGHIJ, vminus_ln2_hi), vxGHIJ);
 
     vt0123 = _mm_add_ps(_mm_mul_ps(vn0123, vminus_ln2_lo), vt0123);
     vt4567 = _mm_add_ps(_mm_mul_ps(vn4567, vminus_ln2_lo), vt4567);
     vt89AB = _mm_add_ps(_mm_mul_ps(vn89AB, vminus_ln2_lo), vt89AB);
     vtCDEF = _mm_add_ps(_mm_mul_ps(vnCDEF, vminus_ln2_lo), vtCDEF);
-    vtGHIJ = _mm_add_ps(_mm_mul_ps(vnGHIJ, vminus_ln2_lo), vtGHIJ);
 
     // Compute degree-5 polynomial approximation for exp(t) on [-log(2)/2, log(2)/2].
     __m128 vp0123 = _mm_add_ps(_mm_mul_ps(vc5, vt0123), vc4);
     __m128 vp4567 = _mm_add_ps(_mm_mul_ps(vc5, vt4567), vc4);
     __m128 vp89AB = _mm_add_ps(_mm_mul_ps(vc5, vt89AB), vc4);
     __m128 vpCDEF = _mm_add_ps(_mm_mul_ps(vc5, vtCDEF), vc4);
-    __m128 vpGHIJ = _mm_add_ps(_mm_mul_ps(vc5, vtGHIJ), vc4);
 
     vp0123 = _mm_add_ps(_mm_mul_ps(vp0123, vt0123), vc3);
     vp4567 = _mm_add_ps(_mm_mul_ps(vp4567, vt4567), vc3);
     vp89AB = _mm_add_ps(_mm_mul_ps(vp89AB, vt89AB), vc3);
     vpCDEF = _mm_add_ps(_mm_mul_ps(vpCDEF, vtCDEF), vc3);
-    vpGHIJ = _mm_add_ps(_mm_mul_ps(vpGHIJ, vtGHIJ), vc3);
 
     vp0123 = _mm_add_ps(_mm_mul_ps(vp0123, vt0123), vc2);
     vp4567 = _mm_add_ps(_mm_mul_ps(vp4567, vt4567), vc2);
     vp89AB = _mm_add_ps(_mm_mul_ps(vp89AB, vt89AB), vc2);
     vpCDEF = _mm_add_ps(_mm_mul_ps(vpCDEF, vtCDEF), vc2);
-    vpGHIJ = _mm_add_ps(_mm_mul_ps(vpGHIJ, vtGHIJ), vc2);
 
     vp0123 = _mm_add_ps(_mm_mul_ps(vp0123, vt0123), vc1);
     vp4567 = _mm_add_ps(_mm_mul_ps(vp4567, vt4567), vc1);
     vp89AB = _mm_add_ps(_mm_mul_ps(vp89AB, vt89AB), vc1);
     vpCDEF = _mm_add_ps(_mm_mul_ps(vpCDEF, vtCDEF), vc1);
-    vpGHIJ = _mm_add_ps(_mm_mul_ps(vpGHIJ, vtGHIJ), vc1);
 
     // Reconstruct the final f value:
     //   f = s * (1 + t * (c1 + t * (c2 + t * (c3 + t * (c4 + t * c5)))))
@@ -1737,13 +1725,11 @@ void xnn_f32_raddstoreexpminusmax_ukernel__sse2_rr2_p5_u20_acc2(
     vt4567 = _mm_mul_ps(vt4567, vs4567);
     vt89AB = _mm_mul_ps(vt89AB, vs89AB);
     vtCDEF = _mm_mul_ps(vtCDEF, vsCDEF);
-    vtGHIJ = _mm_mul_ps(vtGHIJ, vsGHIJ);
 
     __m128 vf0123 = _mm_add_ps(_mm_mul_ps(vt0123, vp0123), vs0123);
     __m128 vf4567 = _mm_add_ps(_mm_mul_ps(vt4567, vp4567), vs4567);
     __m128 vf89AB = _mm_add_ps(_mm_mul_ps(vt89AB, vp89AB), vs89AB);
     __m128 vfCDEF = _mm_add_ps(_mm_mul_ps(vtCDEF, vpCDEF), vsCDEF);
-    __m128 vfGHIJ = _mm_add_ps(_mm_mul_ps(vtGHIJ, vpGHIJ), vsGHIJ);
 
     // For inputs below zero cutoff, replace output with +0.0f.
     // Note that for NaN inputs, comparison result is false, and outputs are left unchanged.
@@ -1751,25 +1737,20 @@ void xnn_f32_raddstoreexpminusmax_ukernel__sse2_rr2_p5_u20_acc2(
     vf4567 = _mm_andnot_ps(_mm_cmplt_ps(vx4567, vdenorm_cutoff), vf4567);
     vf89AB = _mm_andnot_ps(_mm_cmplt_ps(vx89AB, vdenorm_cutoff), vf89AB);
     vfCDEF = _mm_andnot_ps(_mm_cmplt_ps(vxCDEF, vdenorm_cutoff), vfCDEF);
-    vfGHIJ = _mm_andnot_ps(_mm_cmplt_ps(vxGHIJ, vdenorm_cutoff), vfGHIJ);
 
-    // Store 20 (5x4) outputs at a time.
+    // Store 16 (4x4) outputs at a time.
     _mm_storeu_ps(output, vf0123);
     _mm_storeu_ps(output + 4, vf4567);
     _mm_storeu_ps(output + 8, vf89AB);
     _mm_storeu_ps(output + 12, vfCDEF);
-    _mm_storeu_ps(output + 16, vfGHIJ);
-    output += 20;
+    output += 16;
 
     // Accumulate computed exponents.
     vacc0 = _mm_add_ps(vacc0, vf0123);
     vacc0 = _mm_add_ps(vacc0, vf4567);
     vacc0 = _mm_add_ps(vacc0, vf89AB);
     vacc0 = _mm_add_ps(vacc0, vfCDEF);
-    vacc0 = _mm_add_ps(vacc0, vfGHIJ);
   }
-  // Add up all accumulators to vacc0
-  vacc0 = _mm_add_ps(vacc0, vacc1);
 
   __m128 vacc = vacc0;
   for (; batch >= 4 * sizeof(float); batch -= 4 * sizeof(float)) {
