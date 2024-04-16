7b6341acc22e4b845c238895a90b5b7ec6ca7bca
Pedro Gonnet <gonnet@google.com>
2024-04-16 04:22:50 -0700

    Add a `ReplicableRandomDevice` to create reproducible randomized tests.
    This is a wrapper for `std::mt19937` that does the following:
    
     * If/when a test fails, prints the random seed used in the failed test,
     * Allows users to provide a fixed random seed via the `--gunit_random_seed` flag.
    
    The wrapper is used e.g. by replacing the common pattern
    ```
    std::random_device random_device;
    auto rng = std::mt19937(random_device());
    ```
    with
    ```
    xnnpack::ReplicableRandomDevice rng;
    ```
    PiperOrigin-RevId: 625280619

---
+++ test/replicable_random_device.h
@@ -0,0 +1,53 @@
+// Copyright 2024 Google LLC
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+#ifndef __XNNPACK_TEST_REPLICABLE_RANDOM_NUMBER_GENERATOR_H_
+#define __XNNPACK_TEST_REPLICABLE_RANDOM_NUMBER_GENERATOR_H_
+
+#include <xnnpack.h>
+
+#include <cstddef>
+#include <random>
+#include <string>
+
+#include <gtest/gtest.h>
+
+namespace xnnpack {
+
+using BaseRandomDevice = std::mt19937;
+
+// Wraps `std::mt19937` such that on construction, the random generator is
+// seeded with the underlying `UnitTest`'s random seed, which can be coerced on
+// the command line.
+//
+// This class additionally registers a `ScopedTrace` that prints the random seed
+// used on test failure, so that the (randomized) test failure can be easily
+// reproduced.
+class ReplicableRandomDevice {
+ public:
+  typedef BaseRandomDevice::result_type result_type;
+  ReplicableRandomDevice()
+      : random_seed_(testing::UnitTest::GetInstance()->random_seed()),
+        random_generator_(random_seed_),
+        scoped_trace_(__FILE__, __LINE__,
+                      "To replicate this failure, re-run the test with "
+                      "`--gunit_random_seed=" +
+                          std::to_string(random_seed_) + "`.") {}
+
+  // Wrapped methods from `std::mt19937`.
+  result_type operator()() { return random_generator_(); }
+  void discard(size_t count) { random_generator_.discard(count); }
+  static constexpr result_type min() { return BaseRandomDevice::min(); }
+  static constexpr result_type max() { return BaseRandomDevice::max(); }
+
+ private:
+  const int random_seed_;
+  BaseRandomDevice random_generator_;
+  testing::ScopedTrace scoped_trace_;
+};
+
+}  // namespace xnnpack
+
+#endif  // __XNNPACK_TEST_REPLICABLE_RANDOM_NUMBER_GENERATOR_H_
--- test/vunary-microkernel-tester.h
+++ test/vunary-microkernel-tester.h
@@ -21,6 +21,7 @@
 #include <random>
 #include <vector>
 
+#include "replicable_random_device.h"
 #include <gtest/gtest.h>
 #include <fp16/fp16.h>
 
@@ -245,8 +246,7 @@ class VUnaryMicrokernelTester {
                                 const UKernelParamsType*),
                 InitParamsFunc init_params, ReferenceFunc ref,
                 ToleranceFunc tol, float range_min, float range_max) const {
-    std::random_device random_device;
-    auto rng = std::mt19937(random_device());
+    xnnpack::ReplicableRandomDevice rng;
     std::uniform_real_distribution<float> f32dist(range_min, range_max);
 
     std::vector<float> x(batch_size() + XNN_EXTRA_BYTES / sizeof(float));
@@ -276,7 +276,7 @@ class VUnaryMicrokernelTester {
 
       // Verify results.
       for (size_t i = 0; i < batch_size(); i++) {
-        EXPECT_NEAR(y[i], y_ref[i], tol(y_ref[i]))
+        ASSERT_NEAR(y[i], y_ref[i], tol(y_ref[i]))
             << "at " << i << " / " << batch_size() << ", x[" << i
             << "] = " << x[i];
       }
@@ -319,8 +319,7 @@ class VUnaryMicrokernelTester {
                                 const UKernelParamsType*),
                 InitParamsFunc init_params, ReferenceFunc ref,
                 ToleranceFunc tol, float range_min, float range_max) const {
-    std::random_device random_device;
-    auto rng = std::mt19937(random_device());
+    xnnpack::ReplicableRandomDevice rng;
     auto distribution =
         std::uniform_real_distribution<float>(range_min, range_max);
     auto bf16rng = [&]() {
@@ -354,7 +353,7 @@ class VUnaryMicrokernelTester {
 
       // Verify results.
       for (size_t i = 0; i < batch_size(); i++) {
-        EXPECT_NEAR(cvt_bf16_f32(y[i]), y_ref[i], tol(y_ref[i]))
+        ASSERT_NEAR(cvt_bf16_f32(y[i]), y_ref[i], tol(y_ref[i]))
             << "at " << i << " / " << batch_size() << ", x[" << i
             << "] = " << cvt_bf16_f32(x[i]);
       }
@@ -380,8 +379,7 @@ class VUnaryMicrokernelTester {
                                 const UKernelParamsType*),
                 InitParamsFunc init_params, ReferenceFunc ref,
                 ToleranceFunc tol, float range_min, float range_max) const {
-    std::random_device random_device;
-    auto rng = std::mt19937(random_device());
+    xnnpack::ReplicableRandomDevice rng;
     auto distribution =
         std::uniform_real_distribution<float>(range_min, range_max);
     auto f16rng = [&]() {
@@ -415,7 +413,7 @@ class VUnaryMicrokernelTester {
 
       // Verify results.
       for (size_t i = 0; i < batch_size(); i++) {
-        EXPECT_NEAR(fp16_ieee_to_fp32_value(y[i]), y_ref[i], tol(y_ref[i]))
+        ASSERT_NEAR(fp16_ieee_to_fp32_value(y[i]), y_ref[i], tol(y_ref[i]))
             << "at " << i << " / " << batch_size() << ", x[" << i
             << "] = " << fp16_ieee_to_fp32_value(x[i]);
       }
