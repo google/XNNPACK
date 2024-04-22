a15f7d9ab9ad7ff15a2e2a2a6c73d6f7c1701c3c
Frank Barchard <fbarchard@google.com>
2024-04-22 11:29:55 -0700

    F16-RMAX enable scalar microkernel for all cpus
    PiperOrigin-RevId: 627102495

--- src/amalgam/gen/scalar.c
+++ src/amalgam/gen/scalar.c
@@ -243,15 +243,7 @@ static int16_t math_signcompliment_f16(const uint16_t a) {
   return (a & 0x7FFF) ^ -((int16_t) a < 0);
 }
 
-static uint16_t math_max_f16(const uint16_t a, const uint16_t b) {
-  return math_signcompliment_f16(a) > math_signcompliment_f16(b) ? a : b;
-}
-
-static uint16_t math_min_f16(const uint16_t a, const uint16_t b) {
-  return math_signcompliment_f16(a) < math_signcompliment_f16(b) ? a : b;
-}
-
-void xnn_f16_rminmax_ukernel__scalar_u4_acc4(
+void xnn_f16_rmax_ukernel__scalar_u2_acc2(
     size_t batch,
     const void* input,
     void* output,
@@ -265,47 +257,69 @@ void xnn_f16_rminmax_ukernel__scalar_u4_acc4(
   const uint16_t* i = (const uint16_t*) input;
   uint16_t* o = (uint16_t*) output;
 
-  uint16_t vmin0 = *i;
-  uint16_t vmax0 = *i;
-  uint16_t vmin1 = vmin0;
-  uint16_t vmax1 = vmax0;
-  uint16_t vmin2 = vmin0;
-  uint16_t vmax2 = vmax0;
-  uint16_t vmin3 = vmin0;
-  uint16_t vmax3 = vmax0;
-  for (; batch >= 4 * sizeof(uint16_t); batch -= 4 * sizeof(uint16_t)) {
-    const uint16_t vt0 = i[0];
-    const uint16_t vt1 = i[1];
-    const uint16_t vt2 = i[2];
-    const uint16_t vt3 = i[3];
-    i += 4;
+  int16_t vt = math_signcompliment_f16(*i);
+  int16_t vmax0 = vt;
+  int16_t vmax1 = vt;
+  for (; batch >= 2 * sizeof(uint16_t); batch -= 2 * sizeof(uint16_t)) {
+    const int16_t vt0 = math_signcompliment_f16(i[0]);
+    const int16_t vt1 = math_signcompliment_f16(i[1]);
+    i += 2;
 
-    vmin0 = math_min_f16(vmin0, vt0);
-    vmax0 = math_max_f16(vmax0, vt0);
-    vmin1 = math_min_f16(vmin1, vt1);
-    vmax1 = math_max_f16(vmax1, vt1);
-    vmin2 = math_min_f16(vmin2, vt2);
-    vmax2 = math_max_f16(vmax2, vt2);
-    vmin3 = math_min_f16(vmin3, vt3);
-    vmax3 = math_max_f16(vmax3, vt3);
+    vmax0 = math_max_s16(vmax0, vt0);
+    vmax1 = math_max_s16(vmax1, vt1);
   }
-  vmin0 = math_min_f16(vmin0, vmin1);
-  vmax0 = math_max_f16(vmax0, vmax1);
-  vmin2 = math_min_f16(vmin2, vmin3);
-  vmax2 = math_max_f16(vmax2, vmax3);
-  vmin0 = math_min_f16(vmin0, vmin2);
-  vmax0 = math_max_f16(vmax0, vmax2);
+  vmax0 = math_max_s16(vmax0, vmax1);
 
   if XNN_UNLIKELY(batch != 0) {
-    do {
-      const uint16_t vt = *i++;
-      vmin0 = math_min_f16(vmin0, vt);
-      vmax0 = math_max_f16(vmax0, vt);
-      batch -= sizeof(uint16_t);
-    } while (batch != 0);
+    vt = math_signcompliment_f16(*i);
+    vmax0 = math_max_s16(vmax0, vt);
   }
-  o[0] = vmin0;
-  o[1] = vmax0;
+  o[0] = (uint16_t) math_signcompliment_f16((uint16_t) vmax0);
+}
+
+static int16_t math_signcompliment_f16(const uint16_t a) {
+  return (a & 0x7FFF) ^ -((int16_t) a < 0);
+}
+
+void xnn_f16_rminmax_ukernel__scalar_u2_acc2(
+    size_t batch,
+    const void* input,
+    void* output,
+    const union xnn_f16_default_params params[restrict XNN_MIN_ELEMENTS(1)])
+{
+  assert(batch != 0);
+  assert(batch % sizeof(uint16_t) == 0);
+  assert(input != NULL);
+  assert(output != NULL);
+
+  const uint16_t* i = (const uint16_t*) input;
+  uint16_t* o = (uint16_t*) output;
+
+  int16_t vt = math_signcompliment_f16(*i);
+  int16_t vmin0 = vt;
+  int16_t vmax0 = vt;
+  int16_t vmin1 = vt;
+  int16_t vmax1 = vt;
+  for (; batch >= 2 * sizeof(uint16_t); batch -= 2 * sizeof(uint16_t)) {
+    const int16_t vt0 = math_signcompliment_f16(i[0]);
+    const int16_t vt1 = math_signcompliment_f16(i[1]);
+    i += 2;
+
+    vmin0 = math_min_s16(vmin0, vt0);
+    vmax0 = math_max_s16(vmax0, vt0);
+    vmin1 = math_min_s16(vmin1, vt1);
+    vmax1 = math_max_s16(vmax1, vt1);
+  }
+  vmin0 = math_min_s16(vmin0, vmin1);
+  vmax0 = math_max_s16(vmax0, vmax1);
+
+  if XNN_UNLIKELY(batch != 0) {
+    vt = math_signcompliment_f16(*i);
+    vmin0 = math_min_s16(vmin0, vt);
+    vmax0 = math_max_s16(vmax0, vt);
+  }
+  o[0] = (uint16_t) math_signcompliment_f16((uint16_t) vmin0);
+  o[1] = (uint16_t) math_signcompliment_f16((uint16_t) vmax0);
 }
 
 void xnn_f32_argmaxpool_ukernel_4x__scalar_c1(
--- src/configs/rmax-config.c
+++ src/configs/rmax-config.c
@@ -48,9 +48,11 @@ static void init_f16_rmax_config(void) {
   #elif (XNN_ARCH_X86 || XNN_ARCH_X86_64) && !XNN_PLATFORM_MOBILE
     const struct xnn_hardware_config* hardware_config = xnn_init_hardware_config();
     assert(hardware_config != NULL);
-    if (hardware_config->use_x86_avx2) {
+    if (hardware_config->use_x86_f16c) {
       f16_rmax_config.ukernel = (xnn_rmax_ukernel_fn) xnn_f16_rmax_ukernel__f16c_u32;
     }
+  #else
+    f16_rmax_config.ukernel = (xnn_rmax_ukernel_fn) xnn_f16_rmax_ukernel__scalar_u2_acc2;
   #endif
 }
 
