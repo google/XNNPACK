71871aaba5cf4c4f57eaac33ad8446b2d984f530
EJ Park <eunjpark@quicinc.com>
2024-04-23 10:59:18 -0500

    Change Hexagon minimum supported version to v68 (from v66)

---
+++ scripts/build-qurt-v68.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -e
+
+mkdir -p build/qurt/v68
+
+CMAKE_ARGS=()
+
+# CMake-level configuration
+CMAKE_ARGS+=("-DCMAKE_TOOLCHAIN_FILE=$PWD/cmake/hexagon.toolchain")
+CMAKE_ARGS+=("-DCMAKE_BUILD_TYPE=RelWithDebInfo")
+
+# If Ninja is installed, prefer it to Make
+if [ -x "$(command -v ninja)" ]
+then
+  CMAKE_ARGS+=("-GNinja")
+fi
+
+CMAKE_ARGS+=("-DXNNPACK_BUILD_LIBRARY=ON")
+CMAKE_ARGS+=("-DXNNPACK_BUILD_BENCHMARKS=ON")
+CMAKE_ARGS+=("-DXNNPACK_BUILD_TESTS=ON")
+
+# Cross-compilation options for Google Benchmark
+CMAKE_ARGS+=("-DHAVE_STEADY_CLOCK=0")
+
+# Use-specified CMake arguments go last to allow overridding defaults
+CMAKE_ARGS+=($@)
+
+cd build/qurt/v68 && cmake ../../.. \
+    "${CMAKE_ARGS[@]}"
+
+cmake --build . -- "-j$((2*$(nproc)))"
