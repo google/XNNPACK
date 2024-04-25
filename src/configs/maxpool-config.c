7d5eeece9ef6f6d21c5ca72415fb0909ac7102ef
kaustubh-raste <Kaustubh.Raste@imgtec.com>
2024-04-25 05:18:51 +0100

    Add RVV f32-maxpool config.
    In this commit update the f32_maxpool_config to optimized rvv implemetation. Update the rvv.c file in amalgam as per config.

--- src/configs/maxpool-config.c
+++ src/configs/maxpool-config.c
@@ -110,6 +110,11 @@ static void init_f32_maxpool_config(void) {
     f32_maxpool_config.init.f32 = xnn_init_f32_minmax_scalar_params;
     f32_maxpool_config.first_pass_tile_size = 9;
     f32_maxpool_config.remainder_pass_tile_size = 8;
+  #elif XNN_ARCH_RISCV && XNN_ENABLE_RISCV_VECTOR
+    f32_maxpool_config.ukernel = (xnn_maxpool_ukernel_fn) xnn_f32_maxpool_minmax_ukernel_9p8x__rvv_c2v;
+    f32_maxpool_config.init.f32 = xnn_init_f32_minmax_scalar_params;
+    f32_maxpool_config.first_pass_tile_size = 9;
+    f32_maxpool_config.remainder_pass_tile_size = 8;
   #else
     f32_maxpool_config.ukernel = (xnn_maxpool_ukernel_fn) xnn_f32_maxpool_minmax_ukernel_9p8x__scalar_c1;
     f32_maxpool_config.init.f32 = xnn_init_f32_minmax_scalar_params;
