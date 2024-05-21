96e2176c904ded427cfb1d80b49535a2347e1324
Frank Barchard <fbarchard@google.com>
2024-05-20 23:26:46 -0700

    RMAX assert param is not NULL
    PiperOrigin-RevId: 635692382

--- src/f32-rminmax/gen/f32-rmax-avx-u24-acc3.c
+++ src/f32-rminmax/gen/f32-rmax-avx-u24-acc3.c
@@ -25,6 +25,7 @@ void xnn_f32_rmax_ukernel__avx_u24_acc3(
   assert(batch % sizeof(float) == 0);
   assert(input != NULL);
   assert(output != NULL);
+  assert(params != NULL);
 
   __m256 vmax0 = _mm256_broadcast_ss(input);
   __m256 vmax1 = vmax0;
--- src/f32-rminmax/gen/f32-rmin-avx-u32-acc2.c
+++ src/f32-rminmax/gen/f32-rmin-avx-u32-acc2.c
@@ -25,6 +25,7 @@ void xnn_f32_rmin_ukernel__avx_u32_acc2(
   assert(batch % sizeof(float) == 0);
   assert(input != NULL);
   assert(output != NULL);
+  assert(params != NULL);
 
   __m256 vmin0 = _mm256_broadcast_ss(input);
   __m256 vmin1 = vmin0;
