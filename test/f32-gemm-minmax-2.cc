9bc54e23022d0b5b8613cf95413cd1656353d19b
Muntasir Mallick <quic_mallick@quicinc.com>
2024-05-31 14:10:54 -0700

    Regenerate gemm test to include ISA Guard

--- test/f32-gemm-minmax-2.cc
+++ test/f32-gemm-minmax-2.cc
@@ -3193,52 +3193,58 @@ INSTANTIATE_TEST_SUITE_P(
     });
 
 
-INSTANTIATE_TEST_SUITE_P(
-    F32_GEMM_MINMAX_1X32__HVX_BROADCAST, GemmTest,
-    testing::ValuesIn(CreateTests1(
-        /*k_block=*/1,
-        /*adj_k_block=*/1,
-        /*mr=*/1, /*nr=*/32, /*kr=*/1, /*sr=*/1,
-        /*is_igemm=*/false,
-        [](GemmMicrokernelTester& tester) {
-          tester.Test(xnn_f32_gemm_minmax_ukernel_1x32__hvx_broadcast,
-                      xnn_init_f32_minmax_hvx_params,
-                      xnn_pack_f32_gemm_goi_w);
-        })),
-    [](const testing::TestParamInfo<GemmTest::ParamType>& info) {
-      return info.param.test_name;
-    });
+#if XNN_ARCH_HEXAGON
+  INSTANTIATE_TEST_SUITE_P(
+      F32_GEMM_MINMAX_1X32__HVX_BROADCAST, GemmTest,
+      testing::ValuesIn(CreateTests1(
+          /*k_block=*/1,
+          /*adj_k_block=*/1,
+          /*mr=*/1, /*nr=*/32, /*kr=*/1, /*sr=*/1,
+          /*is_igemm=*/false,
+          [](GemmMicrokernelTester& tester) {
+            tester.Test(xnn_f32_gemm_minmax_ukernel_1x32__hvx_broadcast,
+                        xnn_init_f32_minmax_hvx_params,
+                        xnn_pack_f32_gemm_goi_w);
+          })),
+      [](const testing::TestParamInfo<GemmTest::ParamType>& info) {
+        return info.param.test_name;
+      });
+#endif  // XNN_ARCH_HEXAGON
 
 
-INSTANTIATE_TEST_SUITE_P(
-    F32_GEMM_MINMAX_1X64__HVX_BROADCAST, GemmTest,
-    testing::ValuesIn(CreateTests1(
-        /*k_block=*/1,
-        /*adj_k_block=*/1,
-        /*mr=*/1, /*nr=*/64, /*kr=*/1, /*sr=*/1,
-        /*is_igemm=*/false,
-        [](GemmMicrokernelTester& tester) {
-          tester.Test(xnn_f32_gemm_minmax_ukernel_1x64__hvx_broadcast,
-                      xnn_init_f32_minmax_hvx_params,
-                      xnn_pack_f32_gemm_goi_w);
-        })),
-    [](const testing::TestParamInfo<GemmTest::ParamType>& info) {
-      return info.param.test_name;
-    });
+#if XNN_ARCH_HEXAGON
+  INSTANTIATE_TEST_SUITE_P(
+      F32_GEMM_MINMAX_1X64__HVX_BROADCAST, GemmTest,
+      testing::ValuesIn(CreateTests1(
+          /*k_block=*/1,
+          /*adj_k_block=*/1,
+          /*mr=*/1, /*nr=*/64, /*kr=*/1, /*sr=*/1,
+          /*is_igemm=*/false,
+          [](GemmMicrokernelTester& tester) {
+            tester.Test(xnn_f32_gemm_minmax_ukernel_1x64__hvx_broadcast,
+                        xnn_init_f32_minmax_hvx_params,
+                        xnn_pack_f32_gemm_goi_w);
+          })),
+      [](const testing::TestParamInfo<GemmTest::ParamType>& info) {
+        return info.param.test_name;
+      });
+#endif  // XNN_ARCH_HEXAGON
 
 
-INSTANTIATE_TEST_SUITE_P(
-    F32_GEMM_MINMAX_4X64__HVX_BROADCAST, GemmTest,
-    testing::ValuesIn(CreateTests1(
-        /*k_block=*/1,
-        /*adj_k_block=*/1,
-        /*mr=*/4, /*nr=*/64, /*kr=*/1, /*sr=*/1,
-        /*is_igemm=*/false,
-        [](GemmMicrokernelTester& tester) {
-          tester.Test(xnn_f32_gemm_minmax_ukernel_4x64__hvx_broadcast,
-                      xnn_init_f32_minmax_hvx_params,
-                      xnn_pack_f32_gemm_goi_w);
-        })),
-    [](const testing::TestParamInfo<GemmTest::ParamType>& info) {
-      return info.param.test_name;
-    });
+#if XNN_ARCH_HEXAGON
+  INSTANTIATE_TEST_SUITE_P(
+      F32_GEMM_MINMAX_4X64__HVX_BROADCAST, GemmTest,
+      testing::ValuesIn(CreateTests1(
+          /*k_block=*/1,
+          /*adj_k_block=*/1,
+          /*mr=*/4, /*nr=*/64, /*kr=*/1, /*sr=*/1,
+          /*is_igemm=*/false,
+          [](GemmMicrokernelTester& tester) {
+            tester.Test(xnn_f32_gemm_minmax_ukernel_4x64__hvx_broadcast,
+                        xnn_init_f32_minmax_hvx_params,
+                        xnn_pack_f32_gemm_goi_w);
+          })),
+      [](const testing::TestParamInfo<GemmTest::ParamType>& info) {
+        return info.param.test_name;
+      });
+#endif  // XNN_ARCH_HEXAGON
