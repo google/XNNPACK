eb49da3434af6ccc7ea0d1a28a75590e38efbaf0
Frank Barchard <fbarchard@google.com>
2024-05-31 17:41:31 -0700

    Use VNNI for m=1 when using AMX microkernel. Create matching VNNI microkernels with m=1.
    PiperOrigin-RevId: 639204472

--- src/configs/gemm-config.c
+++ src/configs/gemm-config.c
@@ -1652,7 +1652,7 @@ static void init_qd8_f32_qc4w_gemm_config(void) {
     assert(hardware_config != NULL);
     #if XNN_ENABLE_AVX512AMX
     if (!XNN_PLATFORM_MOBILE && hardware_config->use_x86_avx512amx) {
-      qd8_f32_qc4w_gemm_config.minmax.dqgemm[XNN_MR_TO_INDEX(1)] = xnn_init_hmp_dqgemm_ukernel((xnn_dqgemm_ukernel_fn) xnn_qd8_f32_qc4w_gemm_minmax_ukernel_1x16c4__avx512amx);
+      qd8_f32_qc4w_gemm_config.minmax.dqgemm[XNN_MR_TO_INDEX(1)] = xnn_init_hmp_dqgemm_ukernel((xnn_dqgemm_ukernel_fn) xnn_qd8_f32_qc4w_gemm_minmax_ukernel_1x16c4__avx512vnnigfni_prfm);
       qd8_f32_qc4w_gemm_config.minmax.dqgemm[XNN_MR_TO_INDEX(16)] = xnn_init_hmp_dqgemm_ukernel((xnn_dqgemm_ukernel_fn) xnn_qd8_f32_qc4w_gemm_minmax_ukernel_16x16c4__avx512amx);
       qd8_f32_qc4w_gemm_config.init.f32_qc4w = xnn_init_f32_qc4w_minmax_avx512vnni_params;
       qd8_f32_qc4w_gemm_config.pack_gemm_gio = (xnn_packw_gemm_gio_ukernel_fn) xnn_pack_qs8_qc4w_gemm_gio_w;
