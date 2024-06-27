566844699e65186bc727d5d2ffecc553d53baafb
Pedro Gonnet <gonnet@google.com>
2024-06-27 01:06:50 -0700

    Add a "Do not edit!" notice to the generated amalgam files.
    PiperOrigin-RevId: 647222520

--- tools/update-microkernels.py
+++ tools/update-microkernels.py
@@ -124,6 +124,9 @@ AMALGAMATION_HEADER = """\
 //
 // This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree.
+//
+// Auto-generated file. Do not edit!
+//   Generator: tools/update-microkernels.py -a
 
 """
 
