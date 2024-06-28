2192847f47f697f25abcd25b83293dd0ac4faf05
Frank Barchard <fbarchard@google.com>
2024-06-27 18:53:35 -0700

    Run generator scripts to update enums
    PiperOrigin-RevId: 647514597

--- src/enums/operator-type.c
+++ src/enums/operator-type.c
@@ -10,7 +10,7 @@
 #include <assert.h>
 #include <stdint.h>
 
-#include "xnnpack/operator-type.h"
+#include <xnnpack/operator-type.h>
 
 static const uint16_t offset[162] = {
   0, 8, 22, 36, 50, 64, 78, 92, 119, 147, 175, 203, 230, 257, 289, 321, 364, 382, 400, 425, 451, 467, 483, 498, 513,
