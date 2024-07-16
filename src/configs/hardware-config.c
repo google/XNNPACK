3267691fdb37df7990b8c7c449af4c92d2b34b7b
Alan Kelly <alankelly@google.com>
2024-07-16 07:16:59 -0700

    Fix OS builds
    PiperOrigin-RevId: 652829686

--- src/configs/hardware-config.c
+++ src/configs/hardware-config.c
@@ -146,19 +146,19 @@ static void init_hardware_config(void) {
 #else
     hardware_config.use_x86_avxvnni = 0;
 #endif
-#if XNN_ENABLE_AVX256SKX
+#if XNN_ENABLE_AVX256SKX && XNN_ENABLE_AVX512AMX
     // Using cpuinfo_has_x86_amx_int8 as placeholder for cpuinfo_has_x86_avx10
     hardware_config.use_x86_avx256skx = hardware_config.use_x86_avx512skx || cpuinfo_has_x86_amx_int8();
 #else
     hardware_config.use_x86_avx256skx = 0;
 #endif
-#if XNN_ENABLE_AVX256VNNI
+#if XNN_ENABLE_AVX256VNNI && XNN_ENABLE_AVX512AMX
     // Using cpuinfo_has_x86_amx_int8 as placeholder for cpuinfo_has_x86_avx10
     hardware_config.use_x86_avx256vnni = (hardware_config.use_x86_avx512skx && cpuinfo_has_x86_avxvnni()) || cpuinfo_has_x86_amx_int8();
 #else
     hardware_config.use_x86_avx256vnni = 0;
 #endif
-#if XNN_ENABLE_AVX256VNNIGFNI
+#if XNN_ENABLE_AVX256VNNIGFNI && XNN_ENABLE_AVX512AMX
     // Using cpuinfo_has_x86_amx_int8 as placeholder for cpuinfo_has_x86_avx10
     hardware_config.use_x86_avx256vnnigfni = hardware_config.use_x86_avx256vnni && cpuinfo_has_x86_gfni();
 #else
