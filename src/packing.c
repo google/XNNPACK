af306c1d6c202ddb418ca2c61fb88534840d4862
Pedro Gonnet <gonnet@google.com>
2024-07-17 07:17:32 -0700

    Use `kai_get_rhs_packed_stride_rhs_pack_kxn_qsi4cxp_qsu4cxs1s0` instead of dividing the result of `kai_get_rhs_packed_offset_rhs_pack_kxn_qsi4cxp_qsu4cxs1s0`.
    PiperOrigin-RevId: 653224192

--- src/packing.c
+++ src/packing.c
@@ -1488,9 +1488,8 @@ size_t xnn_packed_stride_kai_qs4_weights_and_biases(
   const uint32_t nr = gemm_config->nr;
   const uint32_t kr = UINT32_C(1) << gemm_config->log2_kr;
   const uint32_t sr = UINT32_C(1) << gemm_config->log2_sr;
-  return kai_get_rhs_packed_offset_rhs_pack_kxn_qsi4cxp_qsu4cxs1s0(nr, k, nr,
-                                                                   kr, sr) /
-         nr;
+  return kai_get_rhs_packed_stride_rhs_pack_kxn_qsi4cxp_qsu4cxs1s0(k, nr, kr,
+                                                                   sr);
 }
 
 void xnn_pack_kai_qs4_weights_and_biases(
