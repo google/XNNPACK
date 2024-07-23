9f6e1c4133c34df3e21f11987b28d4c93f7d8c60
Uma Devi.S <umadevi@multicorewareinc.com>
2024-07-23 12:21:49 +0530

    Fix arm compilation issue

--- src/xnnpack/simd/s32-neon.h
+++ src/xnnpack/simd/s32-neon.h
@@ -15,7 +15,7 @@
 #include "xnnpack/common.h"
 
 // SIMD vector type for s32 using NEON.
-typedef int32x4_t xnn_simd_s32_t;
+typedef int32x4_t  xnn_simd_s32_t;
 #define xnn_simd_size_s32 4
 #define xnn_simd_log2_size_s32 2
 #define xnn_simd_bytes_s32 (xnn_simd_size_s32 * sizeof(int32_t))
@@ -47,11 +47,11 @@ static XNN_INLINE float32x4_t xnn_cvt_s32_f32(xnn_simd_s32_t a) {
 }
 
 static XNN_INLINE xnn_simd_s32_t xnn_add_s32(xnn_simd_s32_t a,xnn_simd_s32_t b) {
-   return vadd_s32(a, b);
+   return vaddq_s32(a, b);
 }
 
 static XNN_INLINE xnn_simd_s32_t xnn_sub_s32(xnn_simd_s32_t a,xnn_simd_s32_t b) {
-   return vsub_s32(a, b);
+   return vsubq_s32(a, b);
 }
 
 // Load/store operations.
