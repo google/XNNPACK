36b13cdfe0bfe82709c167341e98473c8e34ec72
Quentin Khan <qkhan@google.com>
2024-07-25 05:23:42 -0700

    Add polyfills for missing AVX512F intrinsics in GCC 9.
    PiperOrigin-RevId: 655918862

--- src/xnnpack/intrinsics-polyfill.h
+++ src/xnnpack/intrinsics-polyfill.h
@@ -85,9 +85,9 @@ static XNN_INTRINSIC
 float _mm512_reduce_add_ps(__m512 v) {
 #if __AVX512DQ__
   const __m256 sum2 = _mm256_add_ps(_mm512_castps512_ps256(v), _mm512_extractf32x8_ps(v, 1));
-#else
+#else  // __AVX512DQ__
   const __m256 sum2 = _mm256_add_ps(_mm512_castps512_ps256(v), _mm256_castpd_ps(_mm512_extractf64x4_pd(_mm512_castps_pd(v), 1)));
-#endif
+#endif  // __AVX512DQ__
   const __m128 sum4 = _mm_add_ps(_mm256_castps256_ps128(sum2), _mm256_extractf128_ps(sum2, 1));
   const __m128 sum8 = _mm_add_ps(sum4, _mm_movehl_ps(sum4, sum4));
   const __m128 sum16 = _mm_add_ss(sum8, _mm_movehdup_ps(sum8));
@@ -98,9 +98,9 @@ static XNN_INTRINSIC
 float _mm512_reduce_max_ps(__m512 v) {
 #if __AVX512DQ__
   const __m256 sum2 = _mm256_max_ps(_mm512_castps512_ps256(v), _mm512_extractf32x8_ps(v, 1));
-#else
+#else  // __AVX512DQ__
   const __m256 sum2 = _mm256_max_ps(_mm512_castps512_ps256(v), _mm256_castpd_ps(_mm512_extractf64x4_pd(_mm512_castps_pd(v), 1)));
-#endif
+#endif  // __AVX512DQ__
   const __m128 sum4 = _mm_max_ps(_mm256_castps256_ps128(sum2), _mm256_extractf128_ps(sum2, 1));
   const __m128 sum8 = _mm_max_ps(sum4, _mm_movehl_ps(sum4, sum4));
   const __m128 sum16 = _mm_max_ss(sum8, _mm_movehdup_ps(sum8));
@@ -147,6 +147,26 @@ __m512i _mm512_set_epi8(
 #if defined(__GNUC__) && !defined(__clang__) && !defined(__INTEL_COMPILER) && (__GNUC__ < 10)
 static XNN_INTRINSIC __m512 _mm512_zextps128_ps512(__m128 v) {
   return _mm512_insertf32x4(_mm512_setzero_ps(), v, 0);
+}
+
+static XNN_INTRINSIC
+__m512i _mm512_loadu_epi16 (void const* mem_addr) {
+  return _mm512_set1_epi16((int) unaligned_load_s16(mem_addr));
+}
+
+static XNN_INTRINSIC
+__m512i _mm512_loadu_epi32 (void const* mem_addr) {
+  return _mm512_set1_epi32((int) unaligned_load_s32(mem_addr));
+}
+
+static XNN_INTRINSIC
+void _mm512_storeu_epi16 (void* mem_addr, __m512i a) {
+  _mm512_storeu_si512(mem_addr, a);
+}
+
+static XNN_INTRINSIC
+void _mm512_storeu_epi32 (void* mem_addr, __m512i a) {
+  _mm512_storeu_si512(mem_addr, a);
 }
 #endif  // GCC pre-10
 
