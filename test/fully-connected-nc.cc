GregoryComer/XNNPACK1

Operator support for qd8-f32-qb4w

test/fully-connected-nc.cc
@@ -1397,6 +1397,51 @@ TEST(FULLY_CONNECTED_NC_QD8_F32_QC4W, weights_cache_unit_batch_transpose_weights
     .TestQD8F32QC4W();
 }
 
+TEST(FULLY_CONNECTED_NC_QD8_F32_QB4W, bl_lt_ic) {
+  for (size_t ic=16; ic<=32; ic+=8){
+    for (size_t bs=ic/2; bs<=ic; bs+=ic/2) {
+      if (bs % 8 /*MAX_KR*/ != 0) {
+        continue;
+      }
+      FullyConnectedOperatorTester()
+        .batch_size(1)
+        .output_channels(18)
+        .input_channels(ic)
+        .block_size(bs)
+        .kernel_zero_point(8)
+        .iterations(3)
+        .TestQD8F32QB4W();
+    }
+  }
+}
+
+TEST(FULLY_CONNECTED_NC_QD8_F32_QB4W, bl_eq_kr) {
+  for (size_t ic=16; ic<=32; ic+=8){
+    FullyConnectedOperatorTester()
+      .batch_size(1)
+      .output_channels(21)
+      .input_channels(ic)
+      .block_size(8)
+      .kernel_zero_point(8)
+      .iterations(3)
+      .TestQD8F32QB4W();
+  }
+}
+
+TEST(FULLY_CONNECTED_NC_QD8_F32_QB4W, bl_eq_kr_no_bias) {
+  for (size_t ic=16; ic<=32; ic+=8){
+    FullyConnectedOperatorTester()
+      .has_bias(false)
+      .batch_size(1)
+      .output_channels(23)
+      .input_channels(ic)
+      .block_size(8)
+      .kernel_zero_point(8)
+      .iterations(3)
+      .TestQD8F32QB4W();
+    }
+}
+
 TEST(FULLY_CONNECTED_NC_QD8_F32_QC8W, unit_batch) {
   FullyConnectedOperatorTester()
     .batch_size(1)
@@ -1858,3 +1903,48 @@ TEST(FULLY_CONNECTED_NC_QD8_F16_QC4W, weights_cache_unit_batch_transpose_weights
     .iterations(3)
     .TestQD8F16QC4W();
 }
+
+// TEST(FULLY_CONNECTED_NC_QD8_F16_QB4W, bl_lt_ic) {
+//   for (size_t ic=16; ic<=32; ic+=8){
+//     for (size_t bs=ic/2; bs<=ic; bs+=ic/2) {
+//       if (bs % 8 /*MAX_KR*/ != 0) {
+//         continue;
+//       }
+//       FullyConnectedOperatorTester()
+//         .batch_size(1)
+//         .output_channels(18)
+//         .input_channels(ic)
+//         .block_size(bs)
+//         .kernel_zero_point(8)
+//         .iterations(3)
+//         .TestQD8F16QB4W();
+//     }
+//   }
+// }
+// 
+// TEST(FULLY_CONNECTED_NC_QD8_F16_QB4W, bl_eq_kr) {
+//   for (size_t ic=16; ic<=32; ic+=8){
+//     FullyConnectedOperatorTester()
+//       .batch_size(1)
+//       .output_channels(21)
+//       .input_channels(ic)
+//       .block_size(8)
+//       .kernel_zero_point(8)
+//       .iterations(3)
+//       .TestQD8F16QB4W();
+//   }
+// }
+// 
+// TEST(FULLY_CONNECTED_NC_QD8_F16_QB4W, bl_eq_kr_no_bias) {
+//   for (size_t ic=16; ic<=32; ic+=8){
+//     FullyConnectedOperatorTester()
+//       .has_bias(false)
+//       .batch_size(1)
+//       .output_channels(23)
+//       .input_channels(ic)
+//       .block_size(8)
+//       .kernel_zero_point(8)
+//       .iterations(3)
+//       .TestQD8F16QB4W();
+//     }
+// }

google/XNNPACK

test/fully-connected-nc.cc
@@ -1396,6 +1396,51 @@ TEST(FULLY_CONNECTED_NC_QD8_F32_QC4W, weights_cache_unit_batch_transpose_weights
     .TestQD8F32QC4W();
 }
 
+// TEST(FULLY_CONNECTED_NC_QD8_F32_QB4W, bl_lt_ic) {
+//   for (size_t ic=16; ic<=32; ic+=8){
+//     for (size_t bs=ic/2; bs<=ic; bs+=ic/2) {
+//       if (bs % 8 /*MAX_KR*/ != 0) {
+//         continue;
+//       }
+//       FullyConnectedOperatorTester()
+//         .batch_size(1)
+//         .output_channels(18)
+//         .input_channels(ic)
+//         .block_size(bs)
+//         .kernel_zero_point(8)
+//         .iterations(3)
+//         .TestQD8F32QB4W();
+//     }
+//   }
+// }
+// 
+// TEST(FULLY_CONNECTED_NC_QD8_F32_QB4W, bl_eq_kr) {
+//   for (size_t ic=16; ic<=32; ic+=8){
+//     FullyConnectedOperatorTester()
+//       .batch_size(1)
+//       .output_channels(21)
+//       .input_channels(ic)
+//       .block_size(8)
+//       .kernel_zero_point(8)
+//       .iterations(3)
+//       .TestQD8F32QB4W();
+//   }
+// }
+// 
+// TEST(FULLY_CONNECTED_NC_QD8_F32_QB4W, bl_eq_kr_no_bias) {
+//   for (size_t ic=16; ic<=32; ic+=8){
+//     FullyConnectedOperatorTester()
+//       .has_bias(false)
+//       .batch_size(1)
+//       .output_channels(23)
+//       .input_channels(ic)
+//       .block_size(8)
+//       .kernel_zero_point(8)
+//       .iterations(3)
+//       .TestQD8F32QB4W();
+//     }
+// }
+
 TEST(FULLY_CONNECTED_NC_QD8_F32_QC8W, unit_batch) {
   FullyConnectedOperatorTester()
     .batch_size(1)
@@ -2033,3 +2078,47 @@ TEST(FULLY_CONNECTED_NC_QP8_F32_QC4W, weights_cache_unit_batch_transpose_weights
     .TestQP8F32QC4W();
 }
 
+TEST(FULLY_CONNECTED_NC_QD8_F16_QB4W, bl_lt_ic) {
+  for (size_t ic=16; ic<=32; ic+=8){
+    for (size_t bs=ic/2; bs<=ic; bs+=ic/2) {
+      if (bs % 8 /*MAX_KR*/ != 0) {
+        continue;
+      }
+      FullyConnectedOperatorTester()
+        .batch_size(1)
+        .output_channels(18)
+        .input_channels(ic)
+        .block_size(bs)
+        .kernel_zero_point(8)
+        .iterations(3)
+        .TestQD8F16QB4W();
+    }
+  }
+}
+
+TEST(FULLY_CONNECTED_NC_QD8_F16_QB4W, bl_eq_kr) {
+  for (size_t ic=16; ic<=32; ic+=8){
+    FullyConnectedOperatorTester()
+      .batch_size(1)
+      .output_channels(21)
+      .input_channels(ic)
+      .block_size(8)
+      .kernel_zero_point(8)
+      .iterations(3)
+      .TestQD8F16QB4W();
+  }
+}
+
+TEST(FULLY_CONNECTED_NC_QD8_F16_QB4W, bl_eq_kr_no_bias) {
+  for (size_t ic=16; ic<=32; ic+=8){
+    FullyConnectedOperatorTester()
+      .has_bias(false)
+      .batch_size(1)
+      .output_channels(23)
+      .input_channels(ic)
+      .block_size(8)
+      .kernel_zero_point(8)
+      .iterations(3)
+      .TestQD8F16QB4W();
+    }
+}