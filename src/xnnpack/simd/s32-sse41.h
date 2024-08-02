650f1d7674dc14ded5fc38044bc5e51cc13edc4e
Uma Devi.S <umadevi@multicorewareinc.com>
2024-08-02 18:20:42 +0530

    Add portable and instructions wrapper for s32

--- src/xnnpack/simd/s32-sse41.h
+++ src/xnnpack/simd/s32-sse41.h
@@ -41,6 +41,13 @@ static XNN_INLINE xnn_simd_s32_t xnn_min_s32(xnn_simd_s32_t a,
   return _mm_min_epi32(a, b);
 }
 
+// Bitwise operations
+
+static XNN_INLINE xnn_simd_s32_t xnn_and_s32(xnn_simd_s32_t a,
+                                             xnn_simd_s32_t b) {
+  return _mm_and_si128(a, b);
+}
+
 // Load/store operations.
 
 static XNN_INLINE xnn_simd_s32_t xnn_loadu_s32(const int32_t* ptr) {
