8b05d0a7a4efc2f3b111bd8789b338f43aa87518
LokeshReddyOVS-MCW <lokesh.reddy@multicorewareinc.com>
2024-08-05 18:15:12 +0530

    Adds f32 vremainder subgraph

--- include/xnnpack.h
+++ include/xnnpack.h
@@ -1419,6 +1419,24 @@ enum xnn_status xnn_define_copy(
   uint32_t output_id,
   uint32_t flags);
 
+/// Define a Remainder Node and add it to a Subgraph.
+///
+/// The Remainder Node gives the element-wise remainder of 2 input.
+///
+/// @param subgraph - a Subgraph object that will own the created Node.
+/// @param input1_id - Value ID for the first input tensor. The input tensor must be defined in the @a subgraph.
+/// @param input2_id - Value ID for the second input tensor. The input tensor must be defined in the @a subgraph.
+/// @param output_id - Value ID for the output tensor.
+/// @param flags - binary features of the Remainder Node. No supported flags are currently defined.
+enum xnn_status xnn_define_remainder(
+  xnn_subgraph_t subgraph,
+  float output_min,
+  float output_max,
+  uint32_t input1_id,
+  uint32_t input2_id,
+  uint32_t output_id,
+  uint32_t flags);
+
 /// Define a 2-Output Split Node and add it to a Subgraph.
 ///
 /// The 2-Output Split Node splits an input tensor into two output tensors along a specified axis evenly.
