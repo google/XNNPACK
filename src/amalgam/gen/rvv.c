12bb8579578ba10f09f4a9175bab99d5b2bafe62
Mihai.Olinovici <mihai.olinovici@imgtec.com>
2024-08-16 10:19:03 +0000

    Add RVV f32-qs8/qu8-vcvt configs.

--- src/amalgam/gen/rvv.c
+++ src/amalgam/gen/rvv.c
@@ -21,6 +21,7 @@
 #include "xnnpack/raddstoreexpminusmax.h"
 #include "xnnpack/transpose.h"
 #include "xnnpack/vbinary.h"
+#include "xnnpack/vcvt.h"
 #include "xnnpack/vunary.h"
 
 
@@ -812,6 +813,76 @@ void xnn_f32_pavgpool_minmax_ukernel_9x__rvv_c1v(
     }
     output = (float*) ((uintptr_t) output + output_increment);
   } while (--output_pixels != 0);
+}
+
+void xnn_f32_qs8_vcvt_ukernel__rvv_u8v(
+    size_t batch,
+    const float* input,
+    int8_t* output,
+    const union xnn_f32_qs8_cvt_params params[restrict XNN_MIN_ELEMENTS(1)]) XNN_OOB_READS
+{
+  assert(batch != 0);
+  assert(batch % sizeof(float) == 0);
+  assert(input != NULL);
+  assert(output != NULL);
+
+  batch >>= XNN_LOG2_SIZEOF_FLOAT;
+
+  const float scale = params->rvv.scale;
+  const vfloat32m8_t magic_bias_f32v = __riscv_vfmv_v_f_f32m8(params->rvv.magic_bias, __riscv_vsetvl_e32m8(batch));
+  const int32_t magic_bias_less_zero_point = params->rvv.magic_bias_less_zero_point;
+  const int32_t magic_min = params->rvv.magic_min;
+  const int32_t magic_max = params->rvv.magic_max;
+
+  for (; batch > 0; ) {
+    const int32_t n = __riscv_vsetvl_e32m8(batch); batch -= n;
+
+    vfloat32m8_t x_f32v = __riscv_vle32_v_f32m8(input, n); input += n;
+
+    x_f32v = __riscv_vfmadd_vf_f32m8(x_f32v, scale, magic_bias_f32v, n);
+
+    vint32m8_t y_i32v = __riscv_vreinterpret_v_f32m8_i32m8(x_f32v);
+    y_i32v = __riscv_vmax_vx_i32m8(y_i32v, magic_min, n);
+    y_i32v = __riscv_vmin_vx_i32m8(y_i32v, magic_max, n);
+    y_i32v = __riscv_vssub_vx_i32m8(y_i32v, magic_bias_less_zero_point, n);
+
+    __riscv_vse8_v_i8m2(output, __riscv_vncvt_x_x_w_i8m2(__riscv_vncvt_x_x_w_i16m4(y_i32v, n), n), n); output += n;
+  }
+}
+
+void xnn_f32_qu8_vcvt_ukernel__rvv_u8v(
+    size_t batch,
+    const float* input,
+    uint8_t* output,
+    const union xnn_f32_qu8_cvt_params params[restrict XNN_MIN_ELEMENTS(1)]) XNN_OOB_READS
+{
+  assert(batch != 0);
+  assert(batch % sizeof(float) == 0);
+  assert(input != NULL);
+  assert(output != NULL);
+
+  batch >>= XNN_LOG2_SIZEOF_FLOAT;
+
+  const float scale = params->rvv.scale;
+  const vfloat32m8_t magic_bias_f32v = __riscv_vfmv_v_f_f32m8(params->rvv.magic_bias, __riscv_vsetvl_e32m8(batch));
+  const int32_t magic_bias_less_zero_point = params->rvv.magic_bias_less_zero_point;
+  const int32_t magic_min = params->rvv.magic_min;
+  const int32_t magic_max = params->rvv.magic_max;
+
+  for (; batch > 0; ) {
+    const int32_t n = __riscv_vsetvl_e32m8(batch); batch -= n;
+
+    vfloat32m8_t x_f32v = __riscv_vle32_v_f32m8(input, n); input += n;
+
+    x_f32v = __riscv_vfmadd_vf_f32m8(x_f32v, scale, magic_bias_f32v, n);
+
+    vint32m8_t y_i32v = __riscv_vreinterpret_v_f32m8_i32m8(x_f32v);
+    y_i32v = __riscv_vmax_vx_i32m8(y_i32v, magic_min, n);
+    y_i32v = __riscv_vmin_vx_i32m8(y_i32v, magic_max, n);
+    y_i32v = __riscv_vssub_vx_i32m8(y_i32v, magic_bias_less_zero_point, n);
+
+    __riscv_vse8_v_u8m2(output, __riscv_vncvt_x_x_w_u8m2(__riscv_vncvt_x_x_w_u16m4(__riscv_vreinterpret_v_i32m8_u32m8(y_i32v), n), n), n); output += n;
+  }
 }
 
 static inline vfloat32m4_t eval_poly_horner(vfloat32m4_t x,
