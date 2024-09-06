379bd1c3f3200b43d83380f13008a87566204674
Max Ren <maxren@meta.com>
2024-09-06 00:49:30 -0700

    Add Benchmarking for Kernels

--- bench/gemm-benchmark.h
+++ bench/gemm-benchmark.h
@@ -80,6 +80,15 @@ void GEMMBenchmark(benchmark::State& state,
                    size_t nr, size_t kr, size_t sr, size_t mr_packed,
                    benchmark::utils::IsaCheckFunction isa_check);
 
+void GEMMBenchmark(benchmark::State& state,
+                   xnn_qp8_f32_qb4w_gemm_minmax_ukernel_fn gemm,
+                   xnn_init_f32_qb4w_minmax_params_fn init_params,
+                   xnn_pack_weights_and_biases_fn pack_weights,
+                   xnn_packed_stride_weights_and_biases_fn packed_stride,
+                   size_t mr,
+                   size_t nr, size_t kr, size_t sr, size_t mr_packed,
+                   benchmark::utils::IsaCheckFunction isa_check);
+
 void GEMMBenchmark(benchmark::State& state, xnn_qu8_gemm_minmax_ukernel_fn gemm,
                    xnn_init_qu8_conv_minmax_params_fn init_params,
                    xnn_pack_qu8_gemm_fn pack, size_t mr, size_t nr, size_t kr,
