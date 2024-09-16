0d243a04a08c881e73b80af0f28c41ed6f4ccf5a
kaustubh-raste <Kaustubh.Raste@imgtec.com>
2024-09-16 07:24:59 +0100

    Fix f32-qs8/qu8 vcvt rvv kernel.
    Do zero_point calculation before min/max

--- src/f32-qs8-vcvt/gen/f32-qs8-vcvt-rvv-u8v.c
+++ src/f32-qs8-vcvt/gen/f32-qs8-vcvt-rvv-u8v.c
@@ -30,11 +30,9 @@ void xnn_f32_qs8_vcvt_ukernel__rvv_u8v(
 
   const float scale = params->scalar.scale;
   const vfloat32m8_t magic_bias_f32v = __riscv_vfmv_v_f_f32m8(12582912.0f, __riscv_vsetvl_e32m8(batch));
-  const float output_min_less_zero_point = (float) ((int32_t) params->scalar.output_min - (int32_t) params->scalar.output_zero_point);
-  const float output_max_less_zero_point = (float) ((int32_t) params->scalar.output_max - (int32_t) params->scalar.output_zero_point);
-  const int32_t magic_min = (int32_t) float_as_uint32(12582912.0f + output_min_less_zero_point);
-  const int32_t magic_max = (int32_t) float_as_uint32(12582912.0f + output_max_less_zero_point);
   const int32_t magic_bias_less_zero_point = INT32_C(0x4B400000) - (int32_t) params->scalar.output_zero_point;
+  const int32_t output_min = (int32_t) params->scalar.output_min;
+  const int32_t output_max = (int32_t) params->scalar.output_max;
 
   for (; batch > 0; ) {
     const int32_t n = __riscv_vsetvl_e32m8(batch); batch -= n;
@@ -44,9 +42,9 @@ void xnn_f32_qs8_vcvt_ukernel__rvv_u8v(
     x_f32v = __riscv_vfmadd_vf_f32m8(x_f32v, scale, magic_bias_f32v, n);
 
     vint32m8_t y_i32v = __riscv_vreinterpret_v_f32m8_i32m8(x_f32v);
-    y_i32v = __riscv_vmax_vx_i32m8(y_i32v, magic_min, n);
-    y_i32v = __riscv_vmin_vx_i32m8(y_i32v, magic_max, n);
     y_i32v = __riscv_vssub_vx_i32m8(y_i32v, magic_bias_less_zero_point, n);
+    y_i32v = __riscv_vmax_vx_i32m8(y_i32v, output_min, n);
+    y_i32v = __riscv_vmin_vx_i32m8(y_i32v, output_max, n);
 
     __riscv_vse8_v_i8m2(output, __riscv_vncvt_x_x_w_i8m2(__riscv_vncvt_x_x_w_i16m4(y_i32v, n), n), n); output += n;
   }
