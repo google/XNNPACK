f09f91adeedf88dfe03f97a2e8d848072d2ae187
Dillon Sharlet <dsharlet@google.com>
2024-09-21 00:51:24 -0700

    Replace (some) end2end benchmarks with subgraph benchmarks
    The existing "end2end" benchmarks are not actually end-to-end, because they skip the subgraph API, which contains nontrivial optimizations and work.
    
    This change removes these existing end2end benchmarks, and adds subgraph benchmarks instead. Currently, only qs8, fp16, and fp32 mobilenet benchmarks are included. More datatypes and kinds of models are still TODO.
    
    This also allows closing a const correctness loophole in the configs that existed specifically to enable these benchmarks to hack the configs.
    
    There is also coverage of the individual ops at the microkernel level.
    
    PiperOrigin-RevId: 677129403

---
+++ bench/models/models.h
@@ -0,0 +1,19 @@
+// Copyright 2019 Google LLC
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+#pragma once
+
+#include "xnnpack.h"
+
+namespace models {
+
+xnn_subgraph_t FP32MobileNetV1();
+xnn_subgraph_t FP32MobileNetV2();
+xnn_subgraph_t FP32MobileNetV3Large();
+xnn_subgraph_t FP32MobileNetV3Small();
+
+xnn_subgraph_t QS8MobileNetV2();
+
+}  // namespace models
