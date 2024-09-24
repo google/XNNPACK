d092e25cf808e44fa8f5b77040e51e2e21bff5c7
Dillon Sharlet <dsharlet@google.com>
2024-09-24 12:10:05 -0700

    Use header tables to generate enums instead of scripts
    This makes it impossible for the enum string data to get stale
    
    PiperOrigin-RevId: 678345795

--- src/xnnpack/microkernel-type.h
+++ src/xnnpack/microkernel-type.h
@@ -2,10 +2,6 @@
 //
 // This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree.
-//
-// Auto-generated file. Do not edit!
-//   Specification: src/enums/microkernel-type.yaml
-//   Generator: tools/generate-enum.py
 
 #pragma once
 
@@ -17,19 +13,11 @@ extern "C" {
 #endif
 
 enum xnn_microkernel_type {
-  xnn_microkernel_type_default = 0,
-  xnn_microkernel_type_average_pooling,
-  xnn_microkernel_type_conv2d_hwc2chw,
-  xnn_microkernel_type_dwconv,
-  xnn_microkernel_type_gemm,
-  xnn_microkernel_type_global_average_pooling,
-  xnn_microkernel_type_igemm,
-  xnn_microkernel_type_mean,
-  xnn_microkernel_type_pixelwise_average_pooling,
-  xnn_microkernel_type_spmm,
-  xnn_microkernel_type_subconv2d,
-  xnn_microkernel_type_transpose,
-  xnn_microkernel_type_vmulcaddc,
+#define XNN_ENUM_ITEM_0(enum_name, enum_string) enum_name = 0,
+#define XNN_ENUM_ITEM(enum_name, enum_string) enum_name,
+  #include "xnnpack/microkernel-type-defs.h"
+#undef XNN_ENUM_ITEM_0
+#undef XNN_ENUM_ITEM
 };
 
 XNN_INTERNAL const char* xnn_microkernel_type_to_string(enum xnn_microkernel_type microkernel_type);
