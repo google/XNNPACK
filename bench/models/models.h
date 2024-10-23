e0698d117c62c064ac0a7c9eb9830d3138d3c4c4
Alan Kelly <alankelly@google.com>
2024-10-23 10:39:40 -0700

    Add Dynamically quantized version of the Attention benchmark to bench/models.
    PiperOrigin-RevId: 689021020

--- bench/models/models.h
+++ bench/models/models.h
@@ -9,11 +9,25 @@
 
 namespace models {
 
+struct QD8AttentionWeights {
+  std::vector<int8_t> query_data;
+  std::vector<float> query_scale;
+  std::vector<int8_t> key_data;
+  std::vector<float> key_scale;
+  std::vector<int8_t> value_data;
+  std::vector<float> value_scale;
+  std::vector<int8_t> post_proj_data;
+  std::vector<float> post_proj_scale;
+};
+
 xnn_subgraph_t FP32Attention(size_t b, size_t t, size_t h, size_t n, size_t s);
 xnn_subgraph_t FP32MobileNetV1();
 xnn_subgraph_t FP32MobileNetV2();
 xnn_subgraph_t FP32MobileNetV3Large();
 xnn_subgraph_t FP32MobileNetV3Small();
+xnn_subgraph_t QD8Attention(size_t batch_size, size_t seq_len,
+                            size_t embedding_dim, size_t num_heads,
+                            size_t head_dim, QD8AttentionWeights &weights);
 xnn_subgraph_t QS8MobileNetV2();
 
 }  // namespace models
