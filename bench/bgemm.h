fb229be046aff73702605c5642f66e229499f9aa
Frank Barchard <fbarchard@google.com>
2024-11-04 11:12:26 -0800

    x32_packw_bench add attention parameters for GIO benchmarks
    PiperOrigin-RevId: 693030225

--- bench/bgemm.h
+++ bench/bgemm.h
@@ -8,6 +8,7 @@
 #include <benchmark/benchmark.h>
 
 #define BENCHMARK_BGEMM(bgemm_fn) \
+  BENCHMARK_CAPTURE(bgemm_fn, attention, "Attention")->Apply(QD8AttentionBgemmArguments)->UseRealTime(); \
   BENCHMARK_CAPTURE(bgemm_fn, albert, "Albert")->Apply(AlbertBgemmArguments)->UseRealTime(); \
   BENCHMARK_CAPTURE(bgemm_fn, mobilebert, "MobileBert")->Apply(MobilebertBgemmArguments)->UseRealTime(); \
   BENCHMARK_CAPTURE(bgemm_fn, sd1x_diffusion, "SD1.X Diffusion")->Apply(SD1XDiffusionBgemmArguments)->UseRealTime(); \
@@ -15,6 +16,7 @@
   BENCHMARK_CAPTURE(bgemm_fn, sd1x_text_encoder, "SD1.X Text Encoder")->Apply(SD1XTextEncoderBgemmArguments)->UseRealTime();
 
 #define BENCHMARK_CAPTURE_BGEMM(bgemm_fn, name_prefix, ...) \
+  BENCHMARK_CAPTURE(bgemm_fn, name_prefix##attention, "Attention", __VA_ARGS__)->Apply(QD8AttentionBgemmArguments)->UseRealTime(); \
   BENCHMARK_CAPTURE(bgemm_fn, name_prefix##albert, "Albert", __VA_ARGS__)->Apply(AlbertBgemmArguments)->UseRealTime(); \
   BENCHMARK_CAPTURE(bgemm_fn, name_prefix##mobilebert, "MobileBert", __VA_ARGS__)->Apply(MobilebertBgemmArguments)->UseRealTime(); \
   BENCHMARK_CAPTURE(bgemm_fn, name_prefix##sd1x_diffusion, "SD1.X Diffusion", __VA_ARGS__)->Apply(SD1XDiffusionBgemmArguments)->UseRealTime(); \
@@ -74,4 +76,25 @@ static void SD1XTextEncoderBgemmArguments(benchmark::internal::Benchmark* b) {
   /*       B   M    N   K */
   b->Args({12, 77, 77, 64});
   b->Args({12, 77, 64, 77});
+}
+
+static void QD8AttentionBgemmArguments(benchmark::internal::Benchmark* b) {
+  b->ArgNames({"B", "M", "N", "K"});
+
+  /*       B      M    N   K */
+  b->Args({36186, 1,  4, 16});
+  b->Args({9,     1,  4, 1792});
+  b->Args({110,   1, 16, 1024});
+  b->Args({50,    1, 16, 1536});
+  b->Args({7,     1, 16, 2304});
+  b->Args({18,    1, 16, 1792});
+  b->Args({14,    1, 16, 2048});
+  b->Args({3,     1, 16, 3072});
+  b->Args({55,    1, 14, 1024});
+  b->Args({36,    1,  6, 1536});
+  b->Args({7,     1, 10, 2304});
+  b->Args({3,     1, 12, 3072});
+  b->Args({4,     1,  8, 2048});
+  b->Args({14,    1,  2, 1536});
+  b->Args({10,    1,  2, 2048});
 }
