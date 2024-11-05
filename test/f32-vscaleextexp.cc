257123c518d6c5291d8fd009d5e099edb63f622a
RahulSudarMCW <rahul.sundar@multicorewareinc.com>
2024-11-05 11:59:03 +0530

    Addressed review commemts

--- test/f32-vscaleextexp.cc
+++ test/f32-vscaleextexp.cc
@@ -3,9 +3,7 @@
 // This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree.
 //
-// Auto-generated file. Do not edit!
 //   Specification: f32-vscaleextexp
-//   Generator: tools/generate-vscaleextexp-test.py
 
 
 #include <gtest/gtest.h>
@@ -13,6 +11,34 @@
 #include "xnnpack/isa-checks.h"
 #include "xnnpack/vscaleextexp.h"
 #include "vscaleextexp-microkernel-tester.h"
+
+#define XNN_TEST_VSCALEEXTEXP_ELEMENT_EQ(ukernel, arch_flags, element_tile, ...)                                       \
+  TEST(ukernel, element_eq)                                                                                            \
+  {                                                                                                                    \
+    VScaleExtExpMicrokernelTester().elements(element_tile).Test(ukernel);                                              \
+  }
+#define XNN_TEST_VSCALEEXTEXP_ELEMENT_GT(ukernel, arch_flags, element_tile, ...)                                       \
+  TEST(ukernel, element_gt)                                                                                            \
+  {                                                                                                                    \
+    for (size_t element_size = element_tile + 1; element_size < ((element_tile == 1) ? 10 : element_tile * 2);         \
+         element_size++) {                                                                                             \
+      VScaleExtExpMicrokernelTester().elements(element_size).Test(ukernel);                                            \
+    }                                                                                                                  \
+  }
+#define XNN_TEST_VSCALEEXTEXP_ELEMENT_LT(ukernel, arch_flags, element_tile, ...)                                       \
+  TEST(ukernel, element_lt)                                                                                            \
+  {                                                                                                                    \
+    for (size_t element_size = 1; element_size < element_tile; element_size++) {                                       \
+      VScaleExtExpMicrokernelTester().elements(element_size).Test(ukernel);                                            \
+    }                                                                                                                  \
+  }
+#define XNN_TEST_VSCALEEXTEXP_ELEMENT_DIV(ukernel, arch_flags, element_tile, ...)                                      \
+  TEST(ukernel, element_div)                                                                                           \
+  {                                                                                                                    \
+    for (size_t element_size = 2 * element_tile; element_size < 10 * element_tile; element_size += element_tile) {     \
+      VScaleExtExpMicrokernelTester().elements(element_size).Test(ukernel);                                            \
+    }                                                                                                                  \
+  }
 
 #define XNN_UKERNEL_WITH_PARAMS(arch_flags, ukernel, element_tile, datatype, params_type, init_params)                 \
   XNN_TEST_VSCALEEXTEXP_ELEMENT_EQ(ukernel, arch_flags, element_tile, init_params);                                    \
