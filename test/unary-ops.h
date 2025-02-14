4186e59925f79d05753d881ceb11b8af7923fdb8
Frank Barchard <fbarchard@google.com>
2025-02-13 23:06:40 -0800

    F32-VSQRT test adjust tolerance for approximated reciprocal SSE and AVX
    PiperOrigin-RevId: 726785288

--- test/unary-ops.h
+++ test/unary-ops.h
@@ -381,7 +381,7 @@ struct SquareRoot : public UnaryOpInfo {
   float Tolerance(float y_ref, xnn_datatype datatype) const override {
     switch (datatype) {
       case xnn_datatype_fp32:
-        return TolRelative(y_ref, 2.5f * std::numeric_limits<float>::epsilon());
+        return TolRelative(y_ref, 3.0f * std::numeric_limits<float>::epsilon());
       case xnn_datatype_fp16:
       case xnn_datatype_bf16:
         return TolMixed(y_ref, 1.0e-4f, 5.0e-3f);
