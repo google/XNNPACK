bb77374e5d103d398f2909b86c97f938d7a77fff
Ken Unger <ken.j.unger@gmail.com>
2025-03-09 22:59:11 -0700

    generate auto-generated tests; regenerate kernel with include path change

--- src/qd8-f32-qc8w-igemm/gen/qd8-f32-qc8w-igemm-6x4v-minmax-rvv.c
+++ src/qd8-f32-qc8w-igemm/gen/qd8-f32-qc8w-igemm-6x4v-minmax-rvv.c
@@ -1,3 +1,4 @@
+// clang-format off
 // Auto-generated file. Do not edit!
 //   Template: src/qs8-igemm/rvv.c.in
 //   Generator: tools/xngen
@@ -11,8 +12,8 @@
 
 #include <riscv_vector.h>
 
-#include "xnnpack/math.h"
-#include "xnnpack/igemm.h"
+#include "src/xnnpack/math.h"
+#include "src/xnnpack/igemm.h"
 
 void xnn_qd8_f32_qc8w_igemm_minmax_ukernel_6x4v__rvv(
     size_t mr,
