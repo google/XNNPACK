4e06abf400849c7cb5d91d4b93d9180b4933cdb4
Misha Gutman <aelphy@google.com>
2025-03-12 12:15:14 -0700

    Regenerated f32-rminmax and tests to get rid of // clang-format off changes in other CLs.
    PiperOrigin-RevId: 736220964

--- src/f32-rminmax/gen/f32-rmax-neon-u8-acc2.c
+++ src/f32-rminmax/gen/f32-rmax-neon-u8-acc2.c
@@ -1,3 +1,4 @@
+// clang-format off
 // Auto-generated file. Do not edit!
 //   Template: src/f32-rminmax/neon.c.in
 //   Generator: tools/xngen
--- src/f32-rminmax/gen/f32-rminmax-sse-u12-acc3.c
+++ src/f32-rminmax/gen/f32-rminmax-sse-u12-acc3.c
@@ -1,3 +1,4 @@
+// clang-format off
 // Auto-generated file. Do not edit!
 //   Template: src/f32-rminmax/sse.c.in
 //   Generator: tools/xngen
