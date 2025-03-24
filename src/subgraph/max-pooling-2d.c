google/XNNPACK

commit fb9462250717611d4c47f71b95c220ebcc0b599c

Rewrite pooling tests to be independent of operator API
This is the first real non-trivial test to be rewritten this way. It has a lot to talk about:
- Many bugs found by these new tests:
  - Max pooling cached the indirection buffer, even when the number of channels changed. This led to invalid/stale indirection buffers being used. Found because the new test tests reshaping in a way the old tests did not.
  - The indirection buffer logic for some of these pooling ops had many bugs:
    - Dilation != 1 codepath did not agree with the dilation = 1 codepath. But if one dimension is dilation = 1, and another dimension is not, the dilation != 1 codepath is used. So the dilation = 1 dimension gets buggy results.
    - When dilation != 2, the logic for handling dilation clamping just doesn't work.
    - It relied on unsigned integer overflow behavior to handle clamping at the min (< 0 -> huge -> bigger than max). This isn't technically a bug by itself, but attempting to fix the other bugs uncovered this bug, which made it overall a nightmare to figure out what was going on.
    - There's a conceptual bug: the header claims max pooling is "zero padded", but it actually tries to use data from the valid area as an "identity" for max. This just isn't possible if the kernel is fully outside the valid area. This occurs either because the padding is bigger than the kernel, or because the stride "jumps over" the valid data.
- This relaxes some constraints on pooling ops, e.g. they would not allow 1x1 pooling. It is of course pointless, but I think it is best to just allow such things. Imagine if it were a runtime error to compute `x + 0`... Also, these constraints were not consistent between the various kinds of pooling ops we have.

src/subgraph/max-pooling-2d.c
@@ -146,7 +146,6 @@ static enum xnn_status reshape_max_pooling_operator(
   const size_t channels = values[input_id].shape.dim[3];
 
   enum xnn_status status = xnn_status_invalid_state;
-  const size_t old_workspace_size = opdata->workspace_size;
   size_t output_height, output_width;
   switch (opdata->operator_objects[0]->type) {
     case xnn_operator_type_max_pooling_nhwc_f16:
@@ -216,8 +215,7 @@ static enum xnn_status reshape_max_pooling_operator(
 
   output_value->shape.num_dims = 4;
   const size_t new_size = xnn_tensor_get_size(output_value);
-  if (new_size > output_value->size || opdata->workspace_size > old_workspace_size) {
-    output_value->size = new_size;
+  if (new_size > output_value->size) {
     return xnn_status_reallocation_required;
   }
   return xnn_status_success;
@@ -303,13 +301,6 @@ enum xnn_status xnn_define_max_pooling_2d(
     return xnn_status_invalid_parameter;
   }
 
-  if (pooling_size == 1) {
-    xnn_log_error(
-      "failed to define %s operator with 1 pooling element: 1x1 pooling is meaningless",
-      xnn_node_type_to_string(xnn_node_type_max_pooling_2d));
-    return xnn_status_invalid_parameter;
-  }
-
   if (stride_height == 0 || stride_width == 0) {
     xnn_log_error(
       "failed to define %s operator with %" PRIu32 "x%" PRIu32 " stride: stride dimensions must be non-zero",
@@ -321,20 +312,6 @@ enum xnn_status xnn_define_max_pooling_2d(
     xnn_log_error(
       "failed to define %s operator with %" PRIu32 "x%" PRIu32 " dilation: dilation dimensions must be non-zero",
       xnn_node_type_to_string(xnn_node_type_max_pooling_2d), dilation_width, dilation_height);
-    return xnn_status_invalid_parameter;
-  }
-
-  if (stride_height > pooling_height) {
-    xnn_log_error(
-      "failed to define %s operator with %" PRIu32 " stride height: must be less than pooling height %" PRIu32,
-      xnn_node_type_to_string(xnn_node_type_max_pooling_2d), stride_height, pooling_height);
-    return xnn_status_invalid_parameter;
-  }
-
-  if (stride_width > pooling_width) {
-    xnn_log_error(
-      "failed to define %s operator with %" PRIu32 " stride width: must be less than pooling width %" PRIu32,
-      xnn_node_type_to_string(xnn_node_type_max_pooling_2d), stride_width, pooling_width);
     return xnn_status_invalid_parameter;
   }
 

CMakeLists.txt
@@ -1391,7 +1391,6 @@ IF(XNNPACK_BUILD_TESTS)
     SET(LIBRARY_SUBGRAPH_UNIT_TESTS
         argmax-pooling-2d
         average-pooling-2d
-        average-pooling-2d-reshape
         batch-matrix-multiply
         binary
         concatenate

src/indirection.c
@@ -361,37 +361,47 @@ void xnn_indirection_init_maxpool2d(
   const size_t step_height,
   const size_t step_width)
 {
+  assert(input_width > 0);
+  assert(input_height > 0);
   const bool any_dilation = (dilation_height | dilation_width) > 1;
 
   if (any_dilation) {
     // Clamp to the border doesn't work for pooling with dilation.
-    const size_t adjusted_padding_top = input_padding_top % dilation_height;
-    const size_t adjusted_padding_left = input_padding_left % dilation_width;
+    // To implement this correctly, we need to move toward the valid region in
+    // steps of the dilation factor, until we are in bounds.
     for (size_t output_y = 0; output_y < output_height; output_y++) {
-      for (size_t pooling_y = 0; pooling_y < kernel_height; pooling_y++) {
-        size_t safe_input_y = output_y * stride_height;
-        if XNN_UNPREDICTABLE(safe_input_y < adjusted_padding_top) {
-          safe_input_y += dilation_height;
-        }
-        safe_input_y -= adjusted_padding_top;
+      size_t safe_input_y = output_y * stride_height;
+      while (safe_input_y < input_padding_top) {
+        safe_input_y += dilation_height;
+      }
+      safe_input_y -= input_padding_top;
 
-        size_t input_y = output_y * stride_height + pooling_y * dilation_height - input_padding_top;
-        if XNN_UNPREDICTABLE(input_y >= input_height) {
-          input_y = safe_input_y;
+      for (size_t pooling_y = 0; pooling_y < kernel_height; pooling_y++) {
+        size_t input_y = output_y * stride_height + pooling_y * dilation_height;
+        while (input_y < input_padding_top) {
+          input_y += dilation_height;
         }
+        while (input_y >= input_height + input_padding_top) {
+          input_y = doz(input_y, dilation_height);
+        }
+        input_y -= input_padding_top;
 
         for (size_t output_x = 0; output_x < output_width; output_x++) {
-          for (size_t pooling_x = 0; pooling_x < kernel_width; pooling_x++) {
-            size_t safe_input_x = output_x * stride_width;
-            if XNN_UNPREDICTABLE(safe_input_x < adjusted_padding_left) {
-              safe_input_x += dilation_width;
-            }
-            safe_input_x -= adjusted_padding_left;
+          size_t safe_input_x = output_x * stride_width;
+          while (safe_input_x < input_padding_left) {
+            safe_input_x += dilation_width;
+          }
+          safe_input_x -= input_padding_left;
 
-            size_t input_x = output_x * stride_width + pooling_x * dilation_width - input_padding_left;
-            if XNN_UNPREDICTABLE(input_x >= input_width) {
-              input_x = safe_input_x;
+          for (size_t pooling_x = 0; pooling_x < kernel_width; pooling_x++) {
+            size_t input_x = output_x * stride_width + pooling_x * dilation_width;
+            while (input_x < input_padding_left) {
+              input_x += dilation_width;
             }
+            while (input_x >= input_width + input_padding_left) {
+              input_x = doz(input_x, dilation_width);
+            }
+            input_x -= input_padding_left;
 
             const size_t index = output_y * step_height + output_x * step_width * kernel_height + pooling_x * kernel_height + pooling_y;
             indirection_buffer[index] = (const void*) ((uintptr_t) input + (input_y * input_width + input_x) * input_pixel_stride);

src/operators/argmax-pooling-nhwc.c
@@ -72,13 +72,6 @@ enum xnn_status xnn_create_argmax_pooling2d_nhwc_f32(
     goto error;
   }
 
-  if (pooling_size == 1) {
-    xnn_log_error(
-      "failed to create %s operator with 1 pooling element: 1x1 pooling is meaningless",
-      xnn_operator_type_to_string(xnn_operator_type_argmax_pooling_nhwc_f32));
-    goto error;
-  }
-
   const bool any_padding = (input_padding_left | input_padding_top | input_padding_right | input_padding_bottom) != 0;
   if ((flags & XNN_FLAG_TENSORFLOW_SAME_PADDING) != 0) {
     if (any_padding) {

src/operators/max-pooling-nhwc.c
@@ -76,13 +76,6 @@ static enum xnn_status create_max_pooling2d_nhwc(
     goto error;
   }
 
-  if (pooling_size == 1) {
-    xnn_log_error(
-      "failed to create %s operator with 1 pooling element: 1x1 pooling is meaningless",
-      xnn_operator_type_to_string(operator_type));
-    goto error;
-  }
-
   if (stride_height == 0 || stride_width == 0) {
     xnn_log_error(
       "failed to create %s operator with %" PRIu32 "x%" PRIu32 " stride: stride dimensions must be non-zero",
@@ -95,20 +88,6 @@ static enum xnn_status create_max_pooling2d_nhwc(
       "failed to create %s operator with %" PRIu32 "x%" PRIu32 " dilation: dilation dimensions must be non-zero",
       xnn_operator_type_to_string(operator_type), dilation_width, dilation_height);
     goto error;
-  }
-
-  if (stride_height > pooling_height) {
-    xnn_log_error(
-      "failed to create %s operator with %" PRIu32 " stride height: must be less than pooling height %" PRIu32,
-      xnn_operator_type_to_string(operator_type), stride_height, pooling_height);
-    return xnn_status_invalid_parameter;
-  }
-
-  if (stride_width > pooling_width) {
-    xnn_log_error(
-      "failed to create %s operator with %" PRIu32 " stride width: must be less than pooling width %" PRIu32,
-      xnn_operator_type_to_string(operator_type), stride_width, pooling_width);
-    return xnn_status_invalid_parameter;
   }
 
   const bool any_padding = (input_padding_left | input_padding_top | input_padding_right | input_padding_bottom) != 0;
@@ -485,7 +464,8 @@ static enum xnn_status reshape_max_pooling2d_nhwc(
   const size_t step_height = pooling_size + (output_width - 1) * step_width * pooling_height;
 
   if (input_height != max_pooling_op->last_input_height ||
-      input_width != max_pooling_op->last_input_width)
+      input_width != max_pooling_op->last_input_width ||
+      channels != max_pooling_op->last_input_channels)
   {
     const size_t indirection_buffer_size = sizeof(void*) * ((pooling_size - 1) + output_height * step_height);
     const void** indirection_buffer =
@@ -517,6 +497,7 @@ static enum xnn_status reshape_max_pooling2d_nhwc(
       step_height, step_width);
 
     max_pooling_op->last_input = max_pooling_op->input;
+    max_pooling_op->last_input_channels = channels;
     max_pooling_op->last_input_height = input_height;
     max_pooling_op->last_input_width = input_width;
   }

src/subgraph/argmax-pooling-2d.c
@@ -73,6 +73,8 @@ static enum xnn_status reshape_argmax_pooling_operator(
   }
   const uint32_t output_id = opdata->outputs[0];
   assert(output_id < num_values);
+  const uint32_t index_id = opdata->outputs[1];
+  assert(index_id < num_values);
 
   struct xnn_value* output_value = values + output_id;
   output_value->shape.dim[0] = batch_size;
@@ -80,10 +82,22 @@ static enum xnn_status reshape_argmax_pooling_operator(
   output_value->shape.dim[2] = output_width;
   output_value->shape.dim[3] = channel_dim;
 
+  struct xnn_value* output_index = values + index_id;
+  output_index->shape.dim[0] = batch_size;
+  output_index->shape.dim[1] = output_height;
+  output_index->shape.dim[2] = output_width;
+  output_index->shape.dim[3] = channel_dim;
+
   output_value->shape.num_dims = 4;
-  const size_t new_size = xnn_tensor_get_size(output_value);
-  if (new_size > output_value->size) {
-    output_value->size = new_size;
+  output_index->shape.num_dims = 4;
+  const size_t new_output_size = xnn_tensor_get_size(output_value);
+  if (new_output_size > output_value->size) {
+    output_value->size = new_output_size;
+    return xnn_status_reallocation_required;
+  }
+  const size_t new_index_size = xnn_tensor_get_size(output_index);
+  if (new_index_size > output_index->size) {
+    output_index->size = new_index_size;
     return xnn_status_reallocation_required;
   }
   return xnn_status_success;
@@ -150,13 +164,6 @@ enum xnn_status xnn_define_argmax_pooling_2d(
       "failed to define %s operator with %" PRIu32 "x%" PRIu32 " pooling size: "
       "pooling size dimensions must be non-zero",
       xnn_node_type_to_string(xnn_node_type_argmax_pooling_2d), pooling_width, pooling_height);
-    return xnn_status_invalid_parameter;
-  }
-
-  if (pooling_size == 1) {
-    xnn_log_error(
-      "failed to define %s operator with 1 pooling element: 1x1 pooling is meaningless",
-      xnn_node_type_to_string(xnn_node_type_argmax_pooling_2d));
     return xnn_status_invalid_parameter;
   }
 

test/subgraph/BUILD
@@ -38,6 +38,15 @@ xnnpack_cxx_library(
     deps = xnnpack_test_deps_for_library(),
 )
 
+xnnpack_cxx_library(
+    name = "stencil",
+    hdrs = ["stencil.h"],
+    deps = [
+        "//:buffer",
+        "//:math",
+    ],
+)
+
 ########################## Size tests for the library #########################
 
 xnnpack_binary(
@@ -106,24 +115,13 @@ xnnpack_unit_test(
 xnnpack_unit_test(
     name = "argmax_pooling_2d_test",
     srcs = ["argmax-pooling-2d.cc"],
-    deps = SUBGRAPH_TEST_DEPS,
+    deps = SUBGRAPH_TEST_DEPS + [":stencil"],
 )
 
 xnnpack_unit_test(
     name = "average_pooling_2d_test",
     srcs = ["average-pooling-2d.cc"],
-    deps = SUBGRAPH_TEST_DEPS,
-)
-
-xnnpack_unit_test(
-    name = "average_pooling_2d_reshape_test",
-    srcs = ["average-pooling-2d-reshape.cc"],
-    deps = [
-        ":runtime_flags",
-        "//:XNNPACK",
-        "//:node_type",
-        "//:subgraph",
-    ],
+    deps = SUBGRAPH_TEST_DEPS + [":stencil"],
 )
 
 xnnpack_unit_test(
@@ -184,7 +182,7 @@ xnnpack_unit_test(
 xnnpack_unit_test(
     name = "max_pooling_2d_test",
     srcs = ["max-pooling-2d.cc"],
-    deps = SUBGRAPH_TEST_DEPS,
+    deps = SUBGRAPH_TEST_DEPS + [":stencil"],
 )
 
 xnnpack_unit_test(
@@ -217,7 +215,7 @@ xnnpack_unit_test(
 xnnpack_unit_test(
     name = "unpooling_2d_test",
     srcs = ["unpooling-2d.cc"],
-    deps = SUBGRAPH_TEST_DEPS,
+    deps = SUBGRAPH_TEST_DEPS + [":stencil"],
 )
 
 xnnpack_unit_test(
@@ -316,5 +314,16 @@ xnnpack_unit_test(
         "//:allocation_type",
         "//:allocator",
         "//:params",
+    ],
+)
+
+xnnpack_unit_test(
+    name = "stencil_test",
+    srcs = ["stencil.cc"],
+    deps = [
+        ":stencil",
+        "//:buffer",
+        "//:operator_utils",
+        "//test:replicable_random_device",
     ],
 )

commit 75581748a8b56a610b4553e3142da65a1a93cf7b

Fix silly mistake introduced in #8080
PiperOrigin-RevId: 739860392

src/subgraph/max-pooling-2d.c
@@ -216,6 +216,7 @@ static enum xnn_status reshape_max_pooling_operator(
   output_value->shape.num_dims = 4;
   const size_t new_size = xnn_tensor_get_size(output_value);
   if (new_size > output_value->size) {
+    output_value->size = new_size;
     return xnn_status_reallocation_required;
   }
   return xnn_status_success;