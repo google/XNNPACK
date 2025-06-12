8a396b78d157e459f6206851dce2771875d7bfa8
Dillon Sharlet <dsharlet@google.com>
2025-06-11 23:36:15 -0700

    Disable ARM SME2 support by default
    PiperOrigin-RevId: 770497853

--- src/configs/hardware-config.c
+++ src/configs/hardware-config.c
@@ -72,7 +72,7 @@ XNN_INIT_ONCE_GUARD(hardware);
 
 // TODO(b/409244409): Remove before end of 2025/Q2.
 #if XNN_ARCH_ARM64
-int32_t xnn_enable_arm_sme2_default = 1;
+int32_t xnn_enable_arm_sme2_default = 0;
 #endif  // XNN_ARCH_ARM64
 
 static void init_hardware_config(void) {
