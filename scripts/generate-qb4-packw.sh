bfb1285427a1297e22d3e9b6b9a9ffbc5a05a5e2
Frank Barchard <fbarchard@google.com>
2025-06-25 09:55:08 -0700

    Mark generate-qb4-packw.sh as executable script
    PiperOrigin-RevId: 775731684

--- scripts/generate-qb4-packw.sh
+++ scripts/generate-qb4-packw.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
+# Copyright 2025 Google LLC
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 
 #################################### Scalar ###################################
+### Generic C micro-kernels
 # C8 Packing
 tools/xngen src/qb4-packw/kr-scalar.c.in -D NR=16 -D KR=8 -D -o src/qb4-packw/gen/qb4-packw-x16c8-gemm-goi-scalar.c
 
