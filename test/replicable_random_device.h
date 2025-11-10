bb7cca124ba187056eafe6a3dd2266ab6774394f
XNNPACK Team <xnnpack-github-robot@google.com>
2025-11-10 14:09:10 -0800

    Print the seed used to create ReplicableRandomDevice.
    It does currently print it when the test fails, but not when the test crashes.
    
    PiperOrigin-RevId: 830592617

--- test/replicable_random_device.h
+++ test/replicable_random_device.h
@@ -11,12 +11,10 @@
 #include <cstdint>
 #include <cstring>
 #include <limits>
-#include <optional>
 #include <string>
 
 #include <gtest/gtest.h>
 #include "src/xnnpack/common.h"
-#include "src/xnnpack/log.h"
 
 namespace xnnpack {
 
@@ -91,18 +89,7 @@ class ReplicableRandomDevice {
         scoped_trace_(__FILE__, __LINE__,
                       "To replicate this failure, re-run the test with "
                       "`--gtest_random_seed=" +
-                          std::to_string(random_seed_) + "`.") {
-    static bool is_set = false;
-    static int last_seed = 0;
-    if (!is_set || last_seed != random_seed_) {
-      xnn_log_info(
-          "Creating a random device for testing, to replicate it re-run the "
-          "test with  `--gtest_random_seed=%d`.\n",
-          random_seed_);
-      is_set = true;
-      last_seed = random_seed_;
-    }
-  }
+                          std::to_string(random_seed_) + "`.") {}
 
   // Wrapped methods from `BaseRandomDevice`.
   result_type operator()() { return random_generator_(); }
