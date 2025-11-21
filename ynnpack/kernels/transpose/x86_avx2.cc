0a746666d5d4c851e48a2eda5311f7afc3520695
Dillon Sharlet <dsharlet@google.com>
2025-11-21 00:07:03 -0800

    Add 128-bit element transpose for AVX2
    This kernel is worth adding over the SSE kernel:
    
    ```
    ------------------------------------------------------------------------------------------
    Benchmark                                                Time             CPU   Iterations
    ------------------------------------------------------------------------------------------
    bench/transpose_x128_avx2/m:30/n:30/real_time          161 ns          161 ns       870313
    bench/transpose_x128_avx2/m:32/n:32/real_time          171 ns          171 ns       792787
    bench/transpose_x128_avx2/m:64/n:64/real_time         3046 ns         3046 ns        46140
    bench/transpose_x128_avx2/m:128/n:128/real_time      12354 ns        12354 ns        11605
    bench/transpose_x128_sse2/m:30/n:30/real_time          376 ns          376 ns       371141
    bench/transpose_x128_sse2/m:32/n:32/real_time          392 ns          392 ns       362619
    bench/transpose_x128_sse2/m:64/n:64/real_time         3085 ns         3083 ns        45320
    bench/transpose_x128_sse2/m:128/n:128/real_time      18142 ns        18135 ns         7572
    bench/transpose_x128/m:30/n:30/real_time               380 ns          380 ns       370416
    bench/transpose_x128/m:32/n:32/real_time               392 ns          392 ns       358465
    bench/transpose_x128/m:64/n:64/real_time              3116 ns         3114 ns        45346
    bench/transpose_x128/m:128/n:128/real_time           18442 ns        18433 ns         7746
    ```
    
    PiperOrigin-RevId: 835096665

--- ynnpack/kernels/transpose/x86_avx2.cc
+++ ynnpack/kernels/transpose/x86_avx2.cc
@@ -28,6 +28,11 @@ void transpose_x64_avx2(size_t m, size_t n, size_t n_bytes_a, size_t stride_a,
   transpose<std::array<__m256i, 4>>(m, n, n_bytes_a, stride_a, a, stride_x, x,
                                     std::integral_constant<size_t, 64>{});
 }
+void transpose_x128_avx2(size_t m, size_t n, size_t n_bytes_a, size_t stride_a,
+                        const void* a, size_t stride_x, void* x) {
+  transpose<std::array<__m256i, 2>>(m, n, n_bytes_a, stride_a, a, stride_x, x,
+                                    std::integral_constant<size_t, 128>{});
+}
 
 void interleave2_x4_avx2(size_t factor, size_t m, size_t n, size_t stride_a,
                          const void* a, void* x) {
