ea72bb362e850c0d04aa8c037b0c1e0b41a36491
Vaisakh K V <vaiskv@qti.qualcomm.com>
2025-11-27 18:14:44 +0530

    Added license header

--- src/qp8-f32-qc8w-gemm/qp8-f32-qc8w-gemm-minmax-16x64c4-neonsme2.c
+++ src/qp8-f32-qc8w-gemm/qp8-f32-qc8w-gemm-minmax-16x64c4-neonsme2.c
@@ -1,5 +1,9 @@
-#include <stddef.h>
+// Copyright 2025 Google LLC
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
 
+#include <stddef.h>
 #include "src/xnnpack/microparams.h"
 
 #if XNN_ENABLE_KLEIDIAI
