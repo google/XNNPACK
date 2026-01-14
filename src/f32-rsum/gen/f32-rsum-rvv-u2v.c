01ec44ffc0ed935dca10d20f56335988a0808b53
Ken Unger <ken.j.unger@gmail.com>
2026-01-14 14:07:13 -0800

    [RVV] add f32-rsum and f32-prelu rvv kernels

---
+++ src/f32-rsum/gen/f32-rsum-rvv-u2v.c
@@ -0,0 +1,46 @@
+// clang-format off
+// Auto-generated file. Do not edit!
+//   Template: src/f32-rsum/rvv.c.in
+//   Generator: tools/xngen
+//
+// Copyright 2026 Google
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+
+#include <assert.h>
+#include <riscv_vector.h>
+#include "src/xnnpack/common.h"
+#include "src/xnnpack/reduce.h"
+
+void xnn_f32_rsum_ukernel__rvv_u2v(
+    size_t batch,
+    const float* input,
+    float* output,
+    const struct xnn_f32_scale_params* restrict params)
+{
+  assert(batch != 0);
+  assert(batch % sizeof(float) == 0);
+  assert(input != NULL);
+  assert((size_t)input % sizeof(float) == 0);
+  assert(output != NULL);
+
+  batch >>= XNN_LOG2_SIZEOF_FLOAT;
+  vfloat32m1_t vzero = __riscv_vfmv_v_f_f32m1(0.f, __riscv_vsetvlmax_e32m1());
+
+  size_t vlmax = __riscv_vsetvl_e32m2(batch);
+  vfloat32m2_t vsum = __riscv_vle32_v_f32m2(input, vlmax); input += vlmax; batch -= vlmax;
+ 
+  for (; batch > 0;) {
+    size_t vl = __riscv_vsetvl_e32m2(batch); batch -= vl;
+
+    vfloat32m2_t vinput = __riscv_vle32_v_f32m2(input, vl); input += vl;
+    vsum = __riscv_vfadd_vv_f32m2(vsum, vinput, vl);
+
+  } while (batch != 0);
+
+  vfloat32m1_t vred = __riscv_vfredusum(vsum, vzero, vlmax);
+  const float vscale = params->scalar.scale;
+  *output += __riscv_vfmv_f_s_f32m1_f32(vred) * vscale;
+}
