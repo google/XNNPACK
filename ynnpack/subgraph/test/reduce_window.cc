d8992543936fe0c08aefcb6f165adecadeb6f3b0
Dillon Sharlet <dsharlet@google.com>
2026-02-05 15:59:46 -0800

    Add max pooling test case to reduce_window test
    PiperOrigin-RevId: 866152141

--- ynnpack/subgraph/test/reduce_window.cc
+++ ynnpack/subgraph/test/reduce_window.cc
@@ -16,6 +16,7 @@
 using ::testing::Each;
 using ::testing::FloatNear;
 using ::testing::Pointwise;
+using ::testing::ElementsAreArray;
 
 namespace ynn {
 
@@ -111,6 +112,56 @@ TEST(ReduceWindowTest, ReduceWindow2D) {
   runtime.SetupExternalTensor(output.data(), output_id).InvokeRuntime();
 
   ASSERT_THAT(output, Each(FloatNear(4.0f, 1e-4f)));
+}
+
+TEST(ReduceWindowTest, MaxPooling2D) {
+  SubgraphBuilder builder(/*external_value_count=*/2);
+  const uint32_t input_id = 0;
+  const uint32_t output_id = 1;
+  builder.AddInput(type_of<float>(), /*shape=*/2, input_id)
+      .AddOutput(type_of<float>(), /*shape=*/2, output_id);
+
+  uint32_t stencil_id = YNN_INVALID_VALUE_ID;
+  builder.AddTensor(type_of<float>(), /*shape=*/4, stencil_id);
+
+  builder.AddStencilCopy(/*stencil_axes=*/{0, 1}, /*new_axes=*/{2, 3},
+                         /*stencil_dims=*/{2, 3}, /*stencil_strides=*/{1, 1},
+                         /*stencil_dilations=*/{1, 1}, /*input_id=*/input_id,
+                         /*padding_id=*/YNN_INVALID_VALUE_ID,
+                         /*output_id=*/stencil_id);
+
+  builder.AddReduce(ynn_reduce_max, /*reduce_axes=*/{2, 3},
+                    /*input_a_id=*/stencil_id,
+                    /*input_b_id=*/YNN_INVALID_VALUE_ID,
+                    /*output_id=*/output_id);
+
+  Runtime runtime(builder.GetSubgraph());
+  ASSERT_EQ(runtime.Status(), ynn_status_success);
+
+  // clang-format off
+  float input[] = {
+      0, 1, 2, 3,
+      1, 2, 3, 4,
+      2, 3, 4, 5,
+      1, 2, 3, 4,
+      0, 1, 2, 3,
+  };
+  // clang-format on
+  runtime.ReshapeExternalTensor({5, 4}, input, input_id);
+  runtime.ReshapeRuntime();
+
+  float output[4 * 2];
+  runtime.SetupExternalTensor(output, output_id).InvokeRuntime();
+
+  // clang-format off
+  const float expected[] = {
+    3, 4,
+    4, 5,
+    4, 5,
+    3, 4,
+  };
+  // clang-format on
+  ASSERT_THAT(output, ElementsAreArray(expected));
 }
 
 }  // namespace ynn
