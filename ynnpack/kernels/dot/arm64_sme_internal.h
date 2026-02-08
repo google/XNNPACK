5cb7d4cb62686ba795c579c0b0f345fd8197c97c
Kasper Nielsen <kasper0406@gmail.com>
2026-02-08 19:07:59 +0100

    Add SME2 utility helpers and fix svcnt ambiguity for mixed-precision kernels.
    - Add svcnt overloads for bfloat16_t, float16_t, and int8_t to support generic template instantiation.
    - Implement svld1_hor_za and svst1_ver_za abstractions for type-generic ZA tile access.
    - Enable high-performance SME2 kernels to compile correctly for all supported data types.

--- ynnpack/kernels/dot/arm64_sme_internal.h
+++ ynnpack/kernels/dot/arm64_sme_internal.h
@@ -20,6 +20,9 @@ namespace ynn {
 // Helpers to call the right SME intrinsics from overloads based on the type.
 inline size_t svcnt(float) { return svcntsw(); }
 inline size_t svcnt(int32_t) { return svcntsw(); }
+inline size_t svcnt(bfloat16_t) { return svcntsh(); }
+inline size_t svcnt(float16_t) { return svcntsh(); }
+inline size_t svcnt(int8_t) { return svcntsb(); }
 
 template <uint64_t tile>
 YNN_ALWAYS_INLINE void svmopa(svbool_t pn, svbool_t pm, svfloat32_t zn,
@@ -93,6 +96,49 @@ YNN_ALWAYS_INLINE svcount_t svctrue(float16_t) YNN_SME_HELPER {
 }
 YNN_ALWAYS_INLINE svcount_t svctrue(int8_t) YNN_SME_HELPER {
   return svptrue_c8();
+}
+
+// Helpers for ZA loads/stores
+template <uint32_t tile>
+YNN_ALWAYS_INLINE void svld1_hor_za(uint32_t slice, svbool_t p, const float* ptr) YNN_SME_HELPER {
+  svld1_hor_za32(tile, slice, p, ptr);
+}
+template <uint32_t tile>
+YNN_ALWAYS_INLINE void svld1_hor_za(uint32_t slice, svbool_t p, const int32_t* ptr) YNN_SME_HELPER {
+  svld1_hor_za32(tile, slice, p, ptr);
+}
+template <uint32_t tile>
+YNN_ALWAYS_INLINE void svld1_hor_za(uint32_t slice, svbool_t p, const bfloat16_t* ptr) YNN_SME_HELPER {
+  svld1_hor_za16(tile, slice, p, ptr);
+}
+template <uint32_t tile>
+YNN_ALWAYS_INLINE void svld1_hor_za(uint32_t slice, svbool_t p, const float16_t* ptr) YNN_SME_HELPER {
+  svld1_hor_za16(tile, slice, p, ptr);
+}
+template <uint32_t tile>
+YNN_ALWAYS_INLINE void svld1_hor_za(uint32_t slice, svbool_t p, const int8_t* ptr) YNN_SME_HELPER {
+  svld1_hor_za8(tile, slice, p, ptr);
+}
+
+template <uint32_t tile>
+YNN_ALWAYS_INLINE void svst1_ver_za(uint32_t slice, svbool_t p, void* ptr) YNN_SME_HELPER {
+  svst1_ver_za32(tile, slice, p, ptr);
+}
+template <uint32_t tile>
+YNN_ALWAYS_INLINE void svst1_ver_za(uint32_t slice, svbool_t p, int32_t* ptr) YNN_SME_HELPER {
+  svst1_ver_za32(tile, slice, p, ptr);
+}
+template <uint32_t tile>
+YNN_ALWAYS_INLINE void svst1_ver_za(uint32_t slice, svbool_t p, bfloat16_t* ptr) YNN_SME_HELPER {
+  svst1_ver_za16(tile, slice, p, ptr);
+}
+template <uint32_t tile>
+YNN_ALWAYS_INLINE void svst1_ver_za(uint32_t slice, svbool_t p, float16_t* ptr) YNN_SME_HELPER {
+  svst1_ver_za16(tile, slice, p, ptr);
+}
+template <uint32_t tile>
+YNN_ALWAYS_INLINE void svst1_ver_za(uint32_t slice, svbool_t p, int8_t* ptr) YNN_SME_HELPER {
+  svst1_ver_za8(tile, slice, p, ptr);
 }
 
 // Helpers to call SME2 multi-vector loads and creations via overloading.
