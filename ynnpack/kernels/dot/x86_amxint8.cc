90af29048be58512ef6bdb77b87a0c32658d117f
XNNPACK Team <xnnpack-github-robot@google.com>
2026-03-04 20:10:24 -0800

    Add 2x2 AMX BF16 and INT8 kernels
    Since we maximise the available tiles for a 2x2 output, we need to reconfigure the tile config for the tail K and N. The loops are arranged to minimize the need to reconfigure.
    
    PiperOrigin-RevId: 878797219

--- ynnpack/kernels/dot/x86_amxint8.cc
+++ ynnpack/kernels/dot/x86_amxint8.cc
@@ -29,7 +29,7 @@ void dot_int8_int8_int32_16x64x64_16x16x4_amxint8(
     size_t A_stride_k3, size_t A_stride_k2, const void* A, size_t B_stride_k3,
     size_t B_stride_k2, size_t B_stride_k1, const void* B, size_t C_in_stride_m,
     const void* C_in, size_t C_out_stride_m, void* C_out) {
-  x86_amx_dot_1x4<int8_t, int32_t, dpbssd>(
+  x86_amx_dot<int8_t, int32_t, dpbssd>(
       M, N, K3, K2, K1, A_stride_m, A_stride_k3, A_stride_k2, A, B_stride_k3,
       B_stride_k2, B_stride_k1, B, C_in_stride_m, C_in, C_out_stride_m, C_out);
 }
@@ -39,27 +39,7 @@ void dot_uint8_int8_int32_16x64x64_16x16x4_amxint8(
     size_t A_stride_k3, size_t A_stride_k2, const void* A, size_t B_stride_k3,
     size_t B_stride_k2, size_t B_stride_k1, const void* B, size_t C_in_stride_m,
     const void* C_in, size_t C_out_stride_m, void* C_out) {
-  x86_amx_dot_1x4<int8_t, int32_t, dpbusd>(
-      M, N, K3, K2, K1, A_stride_m, A_stride_k3, A_stride_k2, A, B_stride_k3,
-      B_stride_k2, B_stride_k1, B, C_in_stride_m, C_in, C_out_stride_m, C_out);
-}
-
-void dot_int8_int8_int32_32x32x64_16x16x4_amxint8(
-    size_t M, size_t N, size_t K3, size_t K2, size_t K1, size_t A_stride_m,
-    size_t A_stride_k3, size_t A_stride_k2, const void* A, size_t B_stride_k3,
-    size_t B_stride_k2, size_t B_stride_k1, const void* B, size_t C_in_stride_m,
-    const void* C_in, size_t C_out_stride_m, void* C_out) {
-  x86_amx_dot_2x2<int8_t, int32_t, dpbssd>(
-      M, N, K3, K2, K1, A_stride_m, A_stride_k3, A_stride_k2, A, B_stride_k3,
-      B_stride_k2, B_stride_k1, B, C_in_stride_m, C_in, C_out_stride_m, C_out);
-}
-
-void dot_uint8_int8_int32_32x32x64_16x16x4_amxint8(
-    size_t M, size_t N, size_t K3, size_t K2, size_t K1, size_t A_stride_m,
-    size_t A_stride_k3, size_t A_stride_k2, const void* A, size_t B_stride_k3,
-    size_t B_stride_k2, size_t B_stride_k1, const void* B, size_t C_in_stride_m,
-    const void* C_in, size_t C_out_stride_m, void* C_out) {
-  x86_amx_dot_2x2<int8_t, int32_t, dpbusd>(
+  x86_amx_dot<int8_t, int32_t, dpbusd>(
       M, N, K3, K2, K1, A_stride_m, A_stride_k3, A_stride_k2, A, B_stride_k3,
       B_stride_k2, B_stride_k1, B, C_in_stride_m, C_in, C_out_stride_m, C_out);
 }
