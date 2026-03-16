c3ea126c1a8c94bd528abc258d7ca06240c6ba73
Ken Unger <ken.j.unger@gmail.com>
2026-03-15 22:14:00 -0700

    cleanup

--- src/f32-rsum2/gen/f32-rsum2-rvv-u4v.c
+++ src/f32-rsum2/gen/f32-rsum2-rvv-u4v.c
@@ -3,7 +3,7 @@
 //   Template: src/f32-rsum2/rvv.c.in
 //   Generator: tools/xngen
 //
-// Copyright 2026 Google
+// Copyright 2026 Google LLC
 //
 // This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree.
