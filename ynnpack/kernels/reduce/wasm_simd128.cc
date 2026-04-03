b8455c9b461423eb9185c6689d484c3d4d2fafab
Volodymyr Kysenko <vksnk@google.com>
2026-04-03 13:04:25 -0700

    Add WASM SIMD128 kernels for FP32 sum and sum of squares.
    PiperOrigin-RevId: 894215128

--- ynnpack/kernels/reduce/wasm_simd128.cc
+++ ynnpack/kernels/reduce/wasm_simd128.cc
@@ -35,4 +35,35 @@ MIN_MAX_KERNEL(max_fp32_4x4_wasm_simd128, dummy_t, f32x4, float, 4);
 MIN_MAX_KERNEL(max_uint8_4x16_wasm_simd128, dummy_t, u8x16, uint8_t, 16);
 MIN_MAX_KERNEL(max_int8_4x16_wasm_simd128, dummy_t, s8x16, int8_t, 16);
 
+void sum_fp32_wasm_simd128(size_t n, size_t k3, size_t k2, size_t k1,
+                           size_t a_stride_n, size_t a_stride_k3,
+                           size_t a_stride_k2, const void* a, size_t, void* c) {
+  if (k1 == 1 && a_stride_n == sizeof(float)) {
+    stream_reduce<sum_accumulator_k1_1<f32x4>, float, float>(
+        n, k3, k2, a_stride_k3, a_stride_k2, reinterpret_cast<const float*>(a),
+        /*C_stride_m=*/0, reinterpret_cast<float*>(c));
+  } else {
+    tiled_reduce<sum_accumulator_fp32<1, Identity>, float, float>(
+        n, k3, k2, k1, a_stride_n, a_stride_k3, a_stride_k2,
+        reinterpret_cast<const float*>(a),
+        /*C_stride_m=*/0, reinterpret_cast<float*>(c));
+  }
+}
+
+void sum_squared_fp32_wasm_simd128(size_t n, size_t k3, size_t k2, size_t k1,
+                                   size_t a_stride_n, size_t a_stride_k3,
+                                   size_t a_stride_k2, const void* a, size_t,
+                                   void* c) {
+  if (k1 == 1 && a_stride_n == sizeof(float)) {
+    stream_reduce<sum_accumulator_k1_1<f32x4, Square>, float, float>(
+        n, k3, k2, a_stride_k3, a_stride_k2, reinterpret_cast<const float*>(a),
+        /*C_stride_m=*/0, reinterpret_cast<float*>(c));
+  } else {
+    tiled_reduce<sum_accumulator_fp32<1, Square>, float, float>(
+        n, k3, k2, k1, a_stride_n, a_stride_k3, a_stride_k2,
+        reinterpret_cast<const float*>(a), /*C_stride_m=*/0,
+        reinterpret_cast<float*>(c));
+  }
+}
+
 }  // namespace ynn
