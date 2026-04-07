b5bdf5973e9a830796be8e0f39337314285de323
Ken Unger <ken.j.unger@gmail.com>
2026-04-07 11:12:47 -0700

    fix comment

--- src/configs/hardware-config.c
+++ src/configs/hardware-config.c
@@ -156,7 +156,7 @@ extern long syscall(long number, ...);
 #endif
 
 #ifndef RISCV_HWPROBE_EXT_ZVFH
-    /* Bit 20 represents the Zvfh (Vector Float16) extension */
+    /* Bit 30 represents the Zvfh (Vector Float16) extension */
     #define RISCV_HWPROBE_EXT_ZVFH (1ULL << 30)
 #endif
 
