225c8e8515cda7bd4dcb0b2ce10706116b33c06a
Ken Unger <ken.j.unger@gmail.com>
2026-04-07 10:46:34 -0700

    fix RISCV_HWPROBE_EXT_ZVFH when not defined

--- src/configs/hardware-config.c
+++ src/configs/hardware-config.c
@@ -157,7 +157,7 @@ extern long syscall(long number, ...);
 
 #ifndef RISCV_HWPROBE_EXT_ZVFH
     /* Bit 20 represents the Zvfh (Vector Float16) extension */
-    #define RISCV_HWPROBE_EXT_ZVFH (1ULL << 20)
+    #define RISCV_HWPROBE_EXT_ZVFH (1ULL << 30)
 #endif
 
 struct riscv_hwprobe {
