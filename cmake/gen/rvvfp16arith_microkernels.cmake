90e94071d687c3f94a1b1b85545dde4cf17ba136
Ken Unger <ken.j.unger@gmail.com>
2026-04-17 14:45:15 -0700

    Copybara import of the project:
    --
    e36eb02164f32c5f27ec60052a73632c588a93ac by Ken Unger <ken.j.unger@gmail.com>:
    
    add fp16 rvv kernels for vsin,vcos,vexp
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/google/XNNPACK/pull/9989 from ken-unger:f16-unary-trig-rvv e36eb02164f32c5f27ec60052a73632c588a93ac
    PiperOrigin-RevId: 901479544

--- cmake/gen/rvvfp16arith_microkernels.cmake
+++ cmake/gen/rvvfp16arith_microkernels.cmake
@@ -52,6 +52,8 @@ SET(PROD_RVVFP16ARITH_MICROKERNEL_SRCS
   src/f16-vbinary/gen/f16-vsub-rvvfp16arith-u8v.c
   src/f16-vbinary/gen/f16-vsubc-rvvfp16arith-u8v.c
   src/f16-vclamp/gen/f16-vclamp-rvvfp16arith-u8v.c
+  src/f16-vcos/gen/f16-vcos-rvvfp16arith-rational-3-2-div-u8v.c
+  src/f16-vexp/gen/f16-vexp-rvvfp16arith-poly-3-u8v.c
   src/f16-vhswish/gen/f16-vhswish-rvvfp16arith-u8v.c
   src/f16-vlrelu/gen/f16-vlrelu-rvvfp16arith-u8v.c
   src/f16-vmulcaddc/gen/f16-vmulcaddc-c4v-minmax-rvvfp16arith-2x.c
@@ -60,6 +62,7 @@ SET(PROD_RVVFP16ARITH_MICROKERNEL_SRCS
   src/f16-vrnd/gen/f16-vrndu-rvvfp16arith-u8v.c
   src/f16-vrnd/gen/f16-vrndz-rvvfp16arith-u8v.c
   src/f16-vrsqrt/gen/f16-vrsqrt-rvvfp16arith-rsqrt-u4v.c
+  src/f16-vsin/gen/f16-vsin-rvvfp16arith-rational-3-2-div-u8v.c
   src/f16-vsqrt/gen/f16-vsqrt-rvvfp16arith-sqrt-u8v.c
   src/f16-vunary/gen/f16-vabs-rvvfp16arith-u8v.c
   src/f16-vunary/gen/f16-vneg-rvvfp16arith-u8v.c
@@ -153,6 +156,12 @@ SET(NON_PROD_RVVFP16ARITH_MICROKERNEL_SRCS
   src/f16-vclamp/gen/f16-vclamp-rvvfp16arith-u1v.c
   src/f16-vclamp/gen/f16-vclamp-rvvfp16arith-u2v.c
   src/f16-vclamp/gen/f16-vclamp-rvvfp16arith-u4v.c
+  src/f16-vcos/gen/f16-vcos-rvvfp16arith-rational-3-2-div-u1v.c
+  src/f16-vcos/gen/f16-vcos-rvvfp16arith-rational-3-2-div-u2v.c
+  src/f16-vcos/gen/f16-vcos-rvvfp16arith-rational-3-2-div-u4v.c
+  src/f16-vexp/gen/f16-vexp-rvvfp16arith-poly-3-u1v.c
+  src/f16-vexp/gen/f16-vexp-rvvfp16arith-poly-3-u2v.c
+  src/f16-vexp/gen/f16-vexp-rvvfp16arith-poly-3-u4v.c
   src/f16-vhswish/gen/f16-vhswish-rvvfp16arith-u4v.c
   src/f16-vlrelu/gen/f16-vlrelu-rvvfp16arith-u4v.c
   src/f16-vmulcaddc/gen/f16-vmulcaddc-c8v-minmax-rvvfp16arith-2x.c
@@ -161,6 +170,9 @@ SET(NON_PROD_RVVFP16ARITH_MICROKERNEL_SRCS
   src/f16-vrnd/gen/f16-vrndu-rvvfp16arith-u4v.c
   src/f16-vrnd/gen/f16-vrndz-rvvfp16arith-u4v.c
   src/f16-vrsqrt/gen/f16-vrsqrt-rvvfp16arith-rsqrt-u2v.c
+  src/f16-vsin/gen/f16-vsin-rvvfp16arith-rational-3-2-div-u1v.c
+  src/f16-vsin/gen/f16-vsin-rvvfp16arith-rational-3-2-div-u2v.c
+  src/f16-vsin/gen/f16-vsin-rvvfp16arith-rational-3-2-div-u4v.c
   src/f16-vsqrt/gen/f16-vsqrt-rvvfp16arith-sqrt-u4v.c
   src/f16-vunary/gen/f16-vabs-rvvfp16arith-u1v.c
   src/f16-vunary/gen/f16-vabs-rvvfp16arith-u2v.c
