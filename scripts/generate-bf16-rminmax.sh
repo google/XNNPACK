a28b56b309e7780f7ac1aaefcb21530a59b8c768
Gregory Comer <gjcomer@meta.com>
2026-04-16 17:10:05 -0700

    Add Neon bf16 minmax kernels

--- scripts/generate-bf16-rminmax.sh
+++ scripts/generate-bf16-rminmax.sh
@@ -4,6 +4,25 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+################################## ARM NEON ###################################
+tools/xngen src/bf16-rminmax/neon.c.in -D BATCH_TILE=8  -D ACCUMULATORS=1 -D OP=MINMAX -o src/bf16-rminmax/gen/bf16-rminmax-neon-u8.c &
+tools/xngen src/bf16-rminmax/neon.c.in -D BATCH_TILE=16 -D ACCUMULATORS=2 -D OP=MINMAX -o src/bf16-rminmax/gen/bf16-rminmax-neon-u16-acc2.c &
+tools/xngen src/bf16-rminmax/neon.c.in -D BATCH_TILE=24 -D ACCUMULATORS=3 -D OP=MINMAX -o src/bf16-rminmax/gen/bf16-rminmax-neon-u24-acc3.c &
+tools/xngen src/bf16-rminmax/neon.c.in -D BATCH_TILE=32 -D ACCUMULATORS=2 -D OP=MINMAX -o src/bf16-rminmax/gen/bf16-rminmax-neon-u32-acc2.c &
+tools/xngen src/bf16-rminmax/neon.c.in -D BATCH_TILE=32 -D ACCUMULATORS=4 -D OP=MINMAX -o src/bf16-rminmax/gen/bf16-rminmax-neon-u32-acc4.c &
+
+tools/xngen src/bf16-rminmax/neon.c.in -D BATCH_TILE=8  -D ACCUMULATORS=1 -D OP=MAX -o src/bf16-rminmax/gen/bf16-rmax-neon-u8.c &
+tools/xngen src/bf16-rminmax/neon.c.in -D BATCH_TILE=16 -D ACCUMULATORS=2 -D OP=MAX -o src/bf16-rminmax/gen/bf16-rmax-neon-u16-acc2.c &
+tools/xngen src/bf16-rminmax/neon.c.in -D BATCH_TILE=24 -D ACCUMULATORS=3 -D OP=MAX -o src/bf16-rminmax/gen/bf16-rmax-neon-u24-acc3.c &
+tools/xngen src/bf16-rminmax/neon.c.in -D BATCH_TILE=32 -D ACCUMULATORS=2 -D OP=MAX -o src/bf16-rminmax/gen/bf16-rmax-neon-u32-acc2.c &
+tools/xngen src/bf16-rminmax/neon.c.in -D BATCH_TILE=32 -D ACCUMULATORS=4 -D OP=MAX -o src/bf16-rminmax/gen/bf16-rmax-neon-u32-acc4.c &
+
+tools/xngen src/bf16-rminmax/neon.c.in -D BATCH_TILE=8  -D ACCUMULATORS=1 -D OP=MIN -o src/bf16-rminmax/gen/bf16-rmin-neon-u8.c &
+tools/xngen src/bf16-rminmax/neon.c.in -D BATCH_TILE=16 -D ACCUMULATORS=2 -D OP=MIN -o src/bf16-rminmax/gen/bf16-rmin-neon-u16-acc2.c &
+tools/xngen src/bf16-rminmax/neon.c.in -D BATCH_TILE=24 -D ACCUMULATORS=3 -D OP=MIN -o src/bf16-rminmax/gen/bf16-rmin-neon-u24-acc3.c &
+tools/xngen src/bf16-rminmax/neon.c.in -D BATCH_TILE=32 -D ACCUMULATORS=2 -D OP=MIN -o src/bf16-rminmax/gen/bf16-rmin-neon-u32-acc2.c &
+tools/xngen src/bf16-rminmax/neon.c.in -D BATCH_TILE=32 -D ACCUMULATORS=4 -D OP=MIN -o src/bf16-rminmax/gen/bf16-rmin-neon-u32-acc4.c &
+
 #################################### Scalar ###################################
 ### Generic C micro-kernels
 tools/xngen src/bf16-rminmax/scalar.c.in -D BATCH_TILE=1 -D ACCUMULATORS=1 -D OP=MINMAX -o src/bf16-rminmax/gen/bf16-rminmax-scalar-u1.c &
