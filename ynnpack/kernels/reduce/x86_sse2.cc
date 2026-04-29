56496fd6bf7c65f373dede7dc5705957732b33ef
Dillon Sharlet <dsharlet@google.com>
2026-04-29 09:59:17 -0700

    Add int32 sum kernels
    These will be necessary to support "factoring" larger reductions into multiple stages.
    
    Benchmarks:
    ```
    -------------------------------------------------------------------------------------------
    Benchmark                                 Time             CPU   Iterations UserCounters...
    -------------------------------------------------------------------------------------------
    bench/sum_int32_avx512/real_time       3109 ns         3108 ns        47834 OP=21.0808G/s 256x1x1x256
    bench/sum_int32_avx2/real_time         4023 ns         4021 ns        33691 OP=16.2923G/s 256x1x1x256
    bench/sum_int32_sse2/real_time         6368 ns         6368 ns        21828 OP=10.2912G/s 256x1x1x256
    bench/sum_int32/real_time             13319 ns        13316 ns        10717 OP=4.92059G/s 256x1x1x256
    ```
    PiperOrigin-RevId: 907644825

--- ynnpack/kernels/reduce/x86_sse2.cc
+++ ynnpack/kernels/reduce/x86_sse2.cc
@@ -237,6 +237,22 @@ void sum_fp32_sse2(size_t n, size_t k3, size_t k2, size_t k1,
   }
 }
 
+void sum_int32_sse2(size_t n, size_t k3, size_t k2, size_t k1,
+                    size_t a_stride_n, size_t a_stride_k3, size_t a_stride_k2,
+                    const void* a, size_t, void* c) {
+  if (k1 == 1 && a_stride_n == sizeof(int32_t)) {
+    stream_reduce<sum_accumulator_k1_1<s32x4>, int32_t, int32_t>(
+        n, k3, k2, a_stride_k3, a_stride_k2,
+        reinterpret_cast<const int32_t*>(a),
+        /*C_stride_m=*/0, reinterpret_cast<int32_t*>(c));
+  } else {
+    tiled_reduce<sum_accumulator_x32<s32x4, 4>, int32_t, int32_t>(
+        n, k3, k2, k1, a_stride_n, a_stride_k3, a_stride_k2,
+        reinterpret_cast<const int32_t*>(a), /*C_stride_m=*/0,
+        reinterpret_cast<int32_t*>(c));
+  }
+}
+
 void sum_squared_fp32_sse2(size_t n, size_t k3, size_t k2, size_t k1,
                            size_t a_stride_n, size_t a_stride_k3,
                            size_t a_stride_k2, const void* a, size_t, void* c) {
