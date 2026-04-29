11be4fec87c4197008b756cba58415de06468853
velonica0 <like@mail.nankai.edu.cn>
2026-04-29 10:05:55 -0700

    Copybara import of the project:
    --
    eeec34e608b63204cc6566ebe7541ada101ce07e by velonica0 <like@mail.nankai.edu.cn>:
    
    [RVV] add rvv f32 kernel for ibilinear
    
    Add RVV kernel for f32-ibilinear (bilinear interpolation) with LMUL={1,2,4}.
    Tested on SpacemiT K1 CPU and K3 CPU, both VLEN=256.
    
    --
    95ee84226f62b4b064ae052cf417162d9f1344cd by velonica0 <like@mail.nankai.edu.cn>:
    
    Alphabetize f32-ibilinear entries in cmake/bzl lists
    
    Move f32-ibilinear entries from end of PROD/NON_PROD lists to
    alphabetical position (before f32-igemm) to avoid merge conflicts
    with other RVV PRs.
    
    --
    194cff63fd0e736011e68ea96edaf48bf750ef98 by velonica0 <like@mail.nankai.edu.cn>:
    
    fix cmake bug
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/google/XNNPACK/pull/9964 from velonica0:rvv-ibilinear 194cff63fd0e736011e68ea96edaf48bf750ef98
    PiperOrigin-RevId: 907648606

---
+++ src/f32-ibilinear/gen/f32-ibilinear-rvv-u4v.c
@@ -0,0 +1,75 @@
+// clang-format off
+// Auto-generated file. Do not edit!
+//   Template: src/f32-ibilinear/rvv.c.in
+//   Generator: tools/xngen
+//
+// Copyright 2024 Google LLC
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include <riscv_vector.h>
+
+#include "src/xnnpack/common.h"
+#include "src/xnnpack/ibilinear.h"
+
+
+void xnn_f32_ibilinear_ukernel__rvv_u4v(
+    size_t output_pixels,
+    size_t channels,
+    const float** restrict input,
+    size_t input_offset,
+    const float* restrict weights,
+    float* restrict output,
+    size_t output_increment)
+{
+  assert(output_pixels != 0);
+  assert(channels != 0);
+  assert(channels % sizeof(float) == 0);
+
+  do {
+    const float* i0 = (const float*) ((uintptr_t) input[0] + input_offset);
+    const float* i1 = (const float*) ((uintptr_t) input[1] + input_offset);
+    const float* i2 = (const float*) ((uintptr_t) input[2] + input_offset);
+    const float* i3 = (const float*) ((uintptr_t) input[3] + input_offset);
+    input += 4;
+
+    const float valphah = weights[0];
+    const float valphav = weights[1];
+    weights += 2;
+
+    size_t c = channels >> XNN_LOG2_SIZEOF_FLOAT;
+    do {
+      const size_t n = __riscv_vsetvl_e32m4(c);
+
+      // Load top-left, top-right, bottom-left, bottom-right.
+      vfloat32m4_t vtl = __riscv_vle32_v_f32m4(i0, n); i0 += n;
+      vfloat32m4_t vtr = __riscv_vle32_v_f32m4(i1, n); i1 += n;
+      vfloat32m4_t vbl = __riscv_vle32_v_f32m4(i2, n); i2 += n;
+      vfloat32m4_t vbr = __riscv_vle32_v_f32m4(i3, n); i3 += n;
+
+      // Horizontal interpolation differences.
+      vfloat32m4_t vtd = __riscv_vfsub_vv_f32m4(vtr, vtl, n);
+      vfloat32m4_t vbd = __riscv_vfsub_vv_f32m4(vbr, vbl, n);
+
+      // Horizontal interpolation: top = tl + (tr - tl) * alphah.
+      vfloat32m4_t vt = __riscv_vfmacc_vf_f32m4(vtl, valphah, vtd, n);
+      vfloat32m4_t vb = __riscv_vfmacc_vf_f32m4(vbl, valphah, vbd, n);
+
+      // Vertical interpolation: output = top + (bottom - top) * alphav.
+      vfloat32m4_t vd = __riscv_vfsub_vv_f32m4(vb, vt, n);
+      vfloat32m4_t vo = __riscv_vfmacc_vf_f32m4(vt, valphav, vd, n);
+
+      __riscv_vse32_v_f32m4(output, vo, n);
+      output += n;
+
+      c -= n;
+    } while (c != 0);
+
+    output = (float*) ((uintptr_t) output + output_increment);
+  } while (--output_pixels != 0);
+}
