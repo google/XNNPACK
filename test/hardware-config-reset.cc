4bc0e6ab80e6f5172a32c9a3551e96467d1e2e0a
Quentin Khan <qkhan@google.com>
2026-06-03 04:19:54 -0700

    Remove useless test main function.
    PiperOrigin-RevId: 925931669

--- test/hardware-config-reset.cc
+++ test/hardware-config-reset.cc
@@ -47,8 +47,3 @@ TEST(HardwareConfigResetTest, HardwareConfigResetAndInitialization) {
 }
 
 }  // namespace
-
-int main(int argc, char** argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
