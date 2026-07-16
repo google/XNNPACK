2e79a4055588d9de27e491da9bfe5679336c8f28
Colm Donelan <colm.donelan@arm.com>
2026-07-16 10:15:14 +0100

    Backing out change to XNN_SIMD_HAS_NATIVE_FMA in simd/f16-scalar.h
    * Reverting an earlier change to src/xnnpack/simd/f16-scalar.h as it
      appears to be as a result of CI artifact.
    * Fixing an assert format in qp8-f16-qc8w-gemm-minmax-16x64c4-neonsme2.c
    
    Signed-off-by: Colm Donelan <colm.donelan@arm.com>

--- src/xnnpack/simd/f16-scalar.h
+++ src/xnnpack/simd/f16-scalar.h
@@ -53,11 +53,18 @@ static XNN_INLINE xnn_simd_f16_t xnn_mul_f16(xnn_simd_f16_t a,
 #endif  // XNN_HAVE_FLOAT16
 }
 
-// Plain scalar f16 expressions do not explicitly use fused operations.
+// If we're computing the fused ops in `float`, act as if we're going to
+// round like native FMA.
 #if XNN_HAVE_FLOAT16
-#define XNN_SIMD_HAS_NATIVE_FMA 0
+#if ((XNN_ARCH_X86 || XNN_ARCH_X86_64) && defined(__FMA__)) || \
+    (XNN_ARCH_ARM64 && __ARM_FEATURE_FMA && defined(__ARM_FEATURE_FP16_FML))
+#define XNN_SIMD_HAS_NATIVE_FMA 1
 #else
-// Computing f16 fused ops via float rounds once at the final f16 conversion.
+#define XNN_SIMD_HAS_NATIVE_FMA 0
+#endif  // ((XNN_ARCH_X86 || XNN_ARCH_X86_64) && defined(__FMA__)) ||
+        // (XNN_ARCH_ARM64 && __ARM_FEATURE_FMA &&
+        // defined(__ARM_FEATURE_FP16_FML))
+#else
 #define XNN_SIMD_HAS_NATIVE_FMA 1
 #endif  // XNN_HAVE_FLOAT16
 
