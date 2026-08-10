e2c2333c78c977cf259e9ed8a7aa0a041f5dd8f0
Misha Gutman <aelphy@google.com>
2026-08-10 03:03:32 -0700

    Expose define_pack_b in YNNPACK dot subgraph.
    PiperOrigin-RevId: 962040411

--- ynnpack/subgraph/dot.h
+++ ynnpack/subgraph/dot.h
@@ -7,8 +7,10 @@
 #define XNNPACK_YNNPACK_SUBGRAPH_DOT_H_
 
 #include <cstdint>
+#include <cstddef>
 
 #include "ynnpack/include/ynnpack.h"
+#include "ynnpack/kernels/dot/dot.h"
 #include "ynnpack/subgraph/subgraph.h"
 #include "slinky/runtime/buffer.h"
 
@@ -21,6 +23,10 @@ void define_transpose_a(ynn_subgraph& subgraph, ynn_node& node,
 // Returns true if dots of type uint8 x `b_type` are faster than dots of type
 // int8 x `b_type`.
 bool prefer_uint8_dot(ynn_type b_type);
+
+uint32_t define_pack_b(ynn_subgraph& subgraph, const dot_type& type,
+                       const dot_kernel& kernel, size_t num_k_dims,
+                       bool consistent_arithmetic, uint32_t input_b_id);
 
 }  // namespace ynn
 
