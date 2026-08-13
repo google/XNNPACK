95bbc553928c74318854d788cac14c8b8820e7ad
Damien Dooley <damien.dooley@arm.com>
2026-08-13 16:39:23 +0100

    Add Arm copyright notices
    Add Arm copyright notices to modified upstream files and use Arm/MIT headers for newly added files.
    
    Signed-off-by: Damien Dooley <damien.dooley@arm.com>

--- test/pqs8-qc4w-gemm-minmax.cc
+++ test/pqs8-qc4w-gemm-minmax.cc
@@ -2,10 +2,9 @@
 // Copyright (c) Facebook, Inc. and its affiliates.
 // All rights reserved.
 //
-// Copyright 2019 Google LLC
+// SPDX-FileCopyrightText: Copyright 2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
 //
-// This source code is licensed under the BSD-style license found in the
-// LICENSE file in the root directory of this source tree.
+// SPDX-License-Identifier: MIT
 //
 // Auto-generated file. Do not edit!
 //   Specification: test/pqs8-qc4w-gemm-minmax.yaml
