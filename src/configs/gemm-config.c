e5a214c500a84579989f475b506db4d57361b38d
Damien Dooley <damien.dooley@arm.com>
2026-08-14 10:27:56 +0100

    Use Arm copyright notice format 2
    Follow the existing-project convention by using Arm copyright notice Format-2 without adding a license identifier.
    
    Signed-off-by: Damien Dooley <damien.dooley@arm.com>

--- src/configs/gemm-config.c
+++ src/configs/gemm-config.c
@@ -1,6 +1,6 @@
 // Copyright 2023 Google LLC
 //
-// SPDX-FileCopyrightText: Copyright 2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
+// Copyright 2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
 //
 // This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree.
--- test/gemm-microkernel-tester.cc
+++ test/gemm-microkernel-tester.cc
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright 2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
+// Copyright 2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
 //
 #include "test/gemm-microkernel-tester.h"
 
