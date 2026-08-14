d612d250006fb01dbabf562e4e4b472207855296
Damien Dooley <damien.dooley@arm.com>
2026-08-14 09:50:10 +0100

    Use XNNPACK license for added files
    Replace the incorrect MIT identifiers with XNNPACK BSD license references while retaining the Arm copyright notices.
    
    Signed-off-by: Damien Dooley <damien.dooley@arm.com>

--- test/qp8-f32-qc2w-gemm-minmax.cc
+++ test/qp8-f32-qc2w-gemm-minmax.cc
@@ -4,7 +4,8 @@
 //
 // SPDX-FileCopyrightText: Copyright 2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
 //
-// SPDX-License-Identifier: MIT
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
 //
 // Auto-generated file. Do not edit!
 //   Specification: test/qp8-f32-qc2w-gemm-minmax.yaml
