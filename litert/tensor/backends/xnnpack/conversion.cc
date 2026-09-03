049a93a1f0b5da383e06984a30f3e91f1709f562
destro4evr-rgb <destro4evr@proton.me>
2026-09-03 11:28:52 -0400

    conversion: add missing absl/algorithm:container and absl/strings:str_format deps
    absl::c_any_of requires absl/algorithm/container.h and
    absl::StrFormat requires absl/strings/str_format.h; both were
    missing from the include list and the BUILD deps for the
    conversion cc_library target.

--- litert/tensor/backends/xnnpack/conversion.cc
+++ litert/tensor/backends/xnnpack/conversion.cc
@@ -27,6 +27,7 @@ limitations under the License.
 #include <vector>
 
 #include "include/xnnpack.h"
+#include "absl/algorithm/container.h"
 #include "absl/base/call_once.h"
 #include "absl/base/no_destructor.h"
 #include "absl/container/flat_hash_map.h"
@@ -34,6 +35,7 @@ limitations under the License.
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
+#include "absl/strings/str_format.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
 #include "litert/tensor/backends/xnnpack/arithmetic.h"
