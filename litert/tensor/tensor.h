ddf31c07d2b07c7ddf453d3da084424d7b4bc854
Quentin Khan <qkhan@google.com>
2026-09-23 07:36:52 -0700

    Add int2 quantized embeddings lookup.
    PiperOrigin-RevId: 986746946

--- litert/tensor/tensor.h
+++ litert/tensor/tensor.h
@@ -45,7 +45,7 @@ struct TensorInit {
   Shape shape;
   std::variant<std::shared_ptr<Buffer>, std::vector<float>,
                std::vector<int32_t>, std::vector<int8_t>, std::vector<int4_t>,
-               float, double, int8_t, int32_t, int64_t>
+               std::vector<int2_t>, float, double, int8_t, int32_t, int64_t>
       buffer;
   std::shared_ptr<Quantization> quantization;
 };
